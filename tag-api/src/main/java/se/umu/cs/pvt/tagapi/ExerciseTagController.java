47ce285e970feef48d6f273a9dcb62ebf2462aba
Julia Johansson <dv21jjn@cs.umu.se>
2023-04-24 07:18:18 +0000

    45_griffin
    Merge branch '45_griffin' into 'main'
    
    Reviewed by 
    Approved by Approved-by: André Byström <c20abm@cs.umu.se>
    
    Description
    https://trello.com/c/kRNCO7av/45-java-tag-api-unused-import
    
    Removed unused imports from all files within the tag-api.
    Resulted in one line being removed.
    
    Branch commits
    * Removed unused imports from all files within the tag-api

--- tag-api/src/main/java/se/umu/cs/pvt/tagapi/ExerciseTagController.java
+++ tag-api/src/main/java/se/umu/cs/pvt/tagapi/ExerciseTagController.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
