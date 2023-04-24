f26a0c9ca9fe34e00a2e195333eb9a281ecf1a45
Jaskaran Heldestad <dv19jhd@cs.umu.se>
2023-04-24 11:02:43 +0000

    https://trello.com/c/eSkdRXh0/29-textarea-standardkomponent
    Merge branch '29_chimera' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Christoffer Billman <id19cbn@cs.umu.se>
    
    Description
    this is a standard textarea implementation #29
    
    Branch commits
    * Fixed the css and jsx
    
    * Fixed the uncontrolled component and removed the margin on the textare
    
    * Added the changes to both the css and jsx. Such as using className, a description on props, changing the name of StandArea and marginArea also fixed the jsx function to default export
    
    * Moved components into a separate folder
    
    * Standard Textarea
    
    * Moved components into a separate folder
    
    * Standard Textarea
    
    * https://trello.com/c/GxHZ8OZI/44-ta-bort-landningssidan-hem
    
    Merge branch '44_chimera' into 'main'
    
    Reviewed by
    Approved by Approved-by: Emmy Lindgren <id19eln@cs.umu.se>
    
    Description
    Changed the home page and the menu bar home button to redirect to the planning page also removed the plan button from the menu bar the ticket number is 44.
    
    Branch commits
    * Fixed the name to Grupp instead of Hem
    
    * Changed login and Home menu bar to redirect the plan page, also removed the plan button from the menubar
    
    * Moved components into a separate folder
    
    * Standard Textarea
    
    * Fixed the name to Grupp instead of Hem
    
    * Changed login and Home menu bar to redirect the plan page, also removed the plan button from the menubar
    
    * https://trello.com/c/h26L5Nto/23-button-standardkomponent
    
    Merge branch '23_chimera' into 'main'
    
    Reviewed by
    Approved by Approved-by: Alexander Arvidsson Örnberg <dv21aag@cs.umu.se>
    
    Description
    Implemented standard button from ticket [#23](https://trello.com/c/h26L5Nto/23-button-standardkomponent).
    
    Branch commits
    * styling fixes
    
    * Move component and change extenion
    
    * Implement new standard button component
    
    * Moved components into a separate folder
    
    * Standard Textarea
    
    * Standard Textarea
    
    * https://trello.com/c/jJPneUk9/50-favorit-knapp
    
    Merge branch '50_chimera' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Alexander Arvidsson Örnberg <dv21aag@cs.umu.se>
    
    Description
    Implemented the star button component, with ticket [50](https://trello.com/c/jJPneUk9/50-favorit-knapp)
    
    Branch commits
    * Update docs and css
    
    * Update comment
    
    * Implement star button
    
    * fix: now using css root var in border color.
    
    * Fixed the css and jsx
    
    * Fixed the uncontrolled component and removed the margin on the textare
    
    * Added the changes to both the css and jsx. Such as using className, a description on props, changing the name of StandArea and marginArea also fixed the jsx function to default export
    
    * Moved components into a separate folder
    
    * Standard Textarea
    
    * Moved components into a separate folder
    
    * Standard Textarea
    
    * Standard Textarea

--- src/components/Common/TextArea/TextArea.jsx
+++ src/components/Common/TextArea/TextArea.jsx
@@ -4,16 +4,25 @@ import './TextArea.css';
 /**
  * This is the standard textarea page, used for using standard textareas.
  * 
+ * The props object is used for holding the written text
+ * from and display it on the textArea. 
+ * props = {
+ *    placeholder: string
+ *    text: string
+ *    onChange: function
+ * }
+ * 
  * @author Team Chimera
- * @version 1.0
- * @since 2023-04-21
+ * @version 2.0
+ * @since 2023-04-24
  */
-
-export function TextArea(props) {
-
+export default function TextArea({ placeholder, text, onChange }) {
     return(
-        <div class="marginArea">
-            <textarea placeholder={props.placeholder || "Write something..."} class="StandArea"></textarea> 
-        </div>
+        <textarea
+            className="stand-area"
+            placeholder={placeholder}
+            value={text}
+            onChange={e => onChange(e.target.value)}
+        />
     )
 }
