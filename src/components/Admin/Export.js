d3fa9bb5dcc3c2e89cecdbfc237bdda55f3cb71d
André Byström <c20abm@cs.umu.se>
2023-04-25 10:58:45 +0000

    https://trello.com/c/rXR7zIyv/53-misslyckades-med-att-exportera-tekniker
    Merge branch '53_cyclops' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Filip Naudot <ens19fpt@cs.umu.se>
    
    Description
    
    Branch commits
    * Changed export component to use new new export api endpoint

--- src/components/Admin/Export.js
+++ src/components/Admin/Export.js
@@ -70,9 +70,8 @@ class Export extends React.Component {
      * Exports Exercise data to .json file.
      * @see exportData
      */
-    exportExercises() {
-        const type = "exercises"
-        this.exportData(type)
+    async exportExercises() {
+        await this.export('/api/export/exercises', 'exercises')
     }
 
 
@@ -81,59 +80,30 @@ class Export extends React.Component {
      * Exports Technique data to .json file.
      * @see exportData
      */
-    exportTechniques() {
-        const type = "techniques"
-        this.exportData(type)
+    async exportTechniques() {
+        await this.export('/api/export/techniques', 'techniques')
     }
 
-    /**
-     * Manages export data from back-end.
-     * @param {String} type either exercise or technique,
-     *                      identifies what will be exported.
-     */
-    async exportData(type) {
-        const activityPath = `/api/${type}/all`
-        const activityRequestOptions = {
+    async export(url, name) {
+        const requestOptions = {
             method: 'GET',
-            headers:{token: this.context.token}
+            headers: {token: this.context.token}
         }
-        var tagPath = `/api/tags/export/${type}?${type === 'exercises' ? 'exerciseIds=' : 'techniqueIds='}`
-        const tagRequestOptions = {
-            method: 'GET',
-            headers:{token: this.context.token}
-        }
-        this.sendData(activityPath, activityRequestOptions, tagPath, tagRequestOptions, type)
-    };
+        const statusLabel = this.chooseStatusLabel(name === 'exercises')
 
-    /**
-     * Sends data to the back-end.
-     * @param {String} activityPath back-end destination for activity
-     * @param {JSON} activityRequestOptions options for the export request of activities
-     * @param {String} tagPath part of destination for tags
-     * @param {JSON} tagRequestOptions options for the export request of tags
-     * @param {String} type either exercise or technique,
-     *                      identifies type of activity will be exported.
-     */
-     async sendData(activityPath, activityRequestOptions, tagPath, tagRequestOptions, type) {
-        const statusLabel = this.chooseStatusLabel(type === "exercises")
-        const activityResponse = await fetch(activityPath, activityRequestOptions)
-        if (activityResponse.ok === false) {
-            this.responseUpdateStatus(activityResponse, statusLabel)
-            return;
-        }
-        const activityData = await activityResponse.json()
-        const tagRequestParams = await this.createTagRequestParams(activityData)
-        const tagResponse = await fetch(`${tagPath}${tagRequestParams}`, tagRequestOptions)
-        if (tagResponse.ok === false) {
-            this.responseUpdateStatus(tagResponse, statusLabel)
+        const response = await fetch(url, requestOptions)
+        if(response.ok === false){
+            this.responseUpdateStatus(response, statusLabel)
             return
         }
-        const tagData = await tagResponse.json()
-        const data = this.compileExportData(activityData, tagData)
-        if (activityResponse.ok && tagResponse.ok) {
-            this.responseUpdateStatus(activityResponse, statusLabel)
-            this.downloadData(data, type)
-        }
+
+        const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+            JSON.stringify(await response.json(), null, 4)
+        )}`;
+        const link = document.createElement("a");
+        link.href = jsonString;
+        link.download = name + ".json";
+        link.click();
     }
 
     /**
@@ -143,55 +113,6 @@ class Export extends React.Component {
      */
     chooseStatusLabel(activityIsExercises) {
         return activityIsExercises ? this.statusExportExercise : this.statusExportTechnique
-    }
-
-    /**
-     * Creates the parameters for the request for exporting tags.
-     * @param {JSON} activityData the fetched activity data.
-     * @returns list of ids of activities which tags is needed. 
-     *          Should be used as parameter for the export call for tags.
-     */
-    async createTagRequestParams(activityData) {
-        var ids = []
-        for (let index = 0; index < Object.keys(activityData).length; index++) {
-            ids.push(activityData[index].id);
-            delete activityData[index].id
-        }
-        return ids
-    }
-
-    /**
-     * Compiles the fetched activity data and tag data to one JSON
-     * object of export data.
-     * @param {JSON} activityData the fetched activity data
-     * @param {JSON} tagData the fetched tag data
-     * @returns Compiled export data.
-     */
-    compileExportData(activityData, tagData) {
-        var data = activityData
-        for (let index = 0; index < Object.keys(data).length; index++) {
-            data[index].tags = tagData[index]
-        }
-        return data
-    }
-
-    /**
-     * Manages downloading of fetched data.
-     * @param {JSON} data fetched data.
-     * @param {String} type either exercise or technique,
-     *                      identifies what has been exported.
-     */
-    async downloadData(data, type) {
-        const formatedData = {
-            [type]:data
-        };
-        const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
-            JSON.stringify(formatedData, null, 4)
-        )}`;
-        const link = document.createElement("a");
-        link.href = jsonString;
-        link.download = type + ".json";
-        link.click();
     }
 
     /**
