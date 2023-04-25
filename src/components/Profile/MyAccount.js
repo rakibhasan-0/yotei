f0bca38c552d24f46b0ed3ac4a3a9c18b28b305c
Alexander Lindblom <c20alm@cs.umu.se>
2023-04-25 10:50:16 +0000

    fixed 'DONT REMOVE THE UNUSED VARIABLE 'cookie''
    Merge branch '23_medusa' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Olle Lögdahl <dv20oll@cs.umu.se>
    
    Description
    https://trello.com/c/kyEuEksq/23-fix-f%C3%B6r-dont-remove-the-unused-variable-cookie-it-is-essential-why-should-be-a-ticket
    
    Fixed an unused variable.
    
    Branch commits
    * fixed 'DONT REMOVE THE UNUSED VARIABLE 'cookie''

--- src/components/Profile/MyAccount.js
+++ src/components/Profile/MyAccount.js
@@ -1,5 +1,5 @@
 import Button from 'react-bootstrap/Button';
-import { useCookies } from 'react-cookie';
+import { Cookies } from 'react-cookie';
 import ChangePasswordForm from '../../components/Forms/ChangePasswordForm';
 import ChangeUsernameForm from '../../components/Forms/ChangeUsernameForm';
 import './MyAccount.css';
@@ -10,15 +10,14 @@ import {useNavigate} from "react-router-dom";
  * @author Team Quattro Formaggio (Group 1) 
  */
 function MyAccount() {
-    //DONT REMOVE THE UNUSED VARIABLE 'cookie', it is essential. Why? Should be a ticket
-    const [cookie, setCookie, removeCookie] = useCookies(['token']); // eslint-disable-line no-unused-vars
+    const cookies = new Cookies();
     const navigate = useNavigate();
 
     /**
      * Function to log out the user.
      */
     function logout() {
-        removeCookie('token', {path: "/"});
+        cookies.remove('token')
         navigate("/");
     }
 
@@ -43,5 +42,3 @@ function MyAccount() {
 }
 
 export default MyAccount;
-
-
