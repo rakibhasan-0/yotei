c15611bf5b8d525f885af0fc2dc63a798ab02baf
Erik Flygare <tfy17efe@cs.umu.se>
2023-04-25 09:21:59 +0000

    https://trello.com/c/EvEnsbIs/14-implementera-verifiera-en-checkbox
    Merge branch '14_chimera' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Emmy Lindgren <id19eln@cs.umu.se>
    
    Description
    Implementerade och verifierade en CheckBox i enlighet med ticket 14 a.k.a implementera och verifiera att en checkbox fungerar.
    
    Branch commits
    * Finalized CheckBox and refactorized it
    
    * Updated CheckBox, continued work on textbox for it
    
    * CheckBox almost done! Reset most testfiles. Still need to add text functionality
    
    * Fix merge conflicts
    
    * Added folder and .css for CheckBox
    
    * Added a CheckBox, still need to add more functionality and customizability.

---
+++ src/components/Common/CheckBox/CheckBox.jsx
@@ -0,0 +1,34 @@
+import './CheckBox.css'
+import {Square , CheckSquareFill} from 'react-bootstrap-icons'
+
+/** 
+ * A default checkbox that should be used throughout the project.
+ * 
+ * 
+ * 
+ * Show the properties that can be set in the props object below:
+ * props = {
+ *     checked: boolean
+ *     onClick: function,
+ * }
+ * 
+ * example:
+ * const [state, setChecked] = useState(false)
+ * <CheckBox checked=state onClick={()=>setChecked(!checked)}></CheckBox>
+ * 
+ *   
+ * @author Chimera (2023-04-21) 
+ * @version 1.0  
+ */
+export default function CheckBox({checked, onClick}) {
+  return (
+    <div className="checkbox-container" onClick={onClick}>
+      {checked
+          ? <>
+            <CheckSquareFill className='check'/>
+            </>
+          : <Square className='square' />
+      }  
+    </div>  
+  );
+}
