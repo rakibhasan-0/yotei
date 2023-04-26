389813a81a9879dace2504513b25b31215a577bd
Alexander Arvidsson Örnberg <dv21aag@cs.umu.se>
2023-04-26 07:45:06 +0000

    25_chimera: Add list component item
    Merge branch '25_chimera' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Emmy Lindgren <id19eln@cs.umu.se>
    
    Description
    Implemented a generic list view item from ticket [#25](https://trello.com/c/I9zwWKoH/25-list-standardkomponent).
    
    Standard view:
    ![image](/uploads/c73a3ff9b3aa150ba4e95e37d9679ef7/image.png)
    
    And the expanded view:
    
    ![image](/uploads/0376ca8496c01ed8ca2e599b8fe29cf8/image.png)
    
    Branch commits
    * Rotate arrow properly
    
    * Add list component item

---
+++ src/components/Common/List/Component.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { ChevronDown } from 'react-bootstrap-icons'
+import './Component.css'
+
+/**
+ * A generic list item that can be used in a list view,
+ * with a customizable optional item on the left of the
+ * component text. The component can be expanded, and
+ * the children will be displayed.
+ * 
+ * The width of the component will be set by the parent.
+ * 
+ * props = {
+ *     item: Optional JSX element which will be displayed on the left of the text,
+ *     text: Text of the component,
+ *     children: JSX element which will be displayed when the component is expanded
+ * }
+ * 
+ * The parent container using this button must constrain its width.
+ * 
+ * @author Chimera dv21aag, dv21oby
+ * @since 2023-04-25
+ * @version 1.0
+ */
+export default function Component({ item, text, children }) {
+    const [toggled, setToggled] = useState(false)
+    return (
+        <div className="list-container">
+            <div className='list-header'>
+                <div className="list-item">
+                    {item}
+                </div>
+                <p>{text}</p>
+                <div className={["list-toggle", toggled ? "list-rotate" : ""].join(" ")}>
+                    <ChevronDown size={28} onClick={() => setToggled(!toggled)} />
+                </div>
+            </div>
+            { toggled && 
+                <div className="list-child">
+                    {children}
+                </div> 
+            }
+        </div>
+    )
+}
