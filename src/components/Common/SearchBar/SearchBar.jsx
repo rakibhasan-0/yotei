1fab2663982a611c38da5547a2feb8d682bb90d5
Emmy Lindgren <id19eln@cs.umu.se>
2023-04-26 09:51:41 +0000

    34 krakotaur : implemented searchbar component
    Merge branch '34_krakotaur' into 'main'
    
    Reviewed by Reviewed-by: Daniel Vesterlund <id19dvd@cs.umu.se>
    Approved by Approved-by: Daniel Vesterlund <id19dvd@cs.umu.se>
    
    Description
    https://trello.com/c/fItnQKAm/34-s%C3%B6kruta-standardkomponent
    Added searchbar component.
    
    Branch commits
    * Added searchbar component.
    
    * Added comments
    
    * Added searchbar component.

---
+++ src/components/Common/SearchBar/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import SearchIcon from '../../../../public/search.svg'
+import './SearchBar.css';
+
+/**
+ * Creates the UI for the searchbar. Props: placeholder (placeholdertext in searchbar),
+ * text (optional value (text) for the searchbar),onChange (for when searchtext is entered). 
+ *
+ * @author Team Minotaur Mavericks & Team Kraken (Group 8,Group 7)
+ * @version 1.0
+ */
+export default function SearchBar({placeholder,text,onChange}) {
+  return (
+    <>
+    <div className='search-bar'>
+       <input
+            className="input-area"
+            placeholder={placeholder}
+            value={text}
+            onChange={e => onChange?.(e.target.value)}></input>
+        <i><img className= "search-icon" src={SearchIcon} alt='Search icon'/></i>
+    </div>
+    </>
+  )
+}
