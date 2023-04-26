a4cccbed2a9c9377f3dc4f7a98c83107ea82d282
Carl Gulliksson <dv21cgn@cs.umu.se>
2023-04-26 12:07:52 +0000

    USE_IMP_SERVER now works properly
    Merge branch '75_medusa' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Anton Degerfeldt <dv21adt@cs.umu.se>
    
    Description
    https://trello.com/c/cuDSWC83/75-useimpserver-fungerar-inte
    Before the frontend would always connect to the dev server, now it is possible to connect to a locally running instance of the backend.
    
    Branch commits
    * Update .env
    
    * refactoring stuff
    
    * added ternary operators in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,51 @@
-import { defineConfig } from "vite"
-import fs from "fs/promises"
-import react from "@vitejs/plugin-react"
-import eslint from "vite-plugin-eslint"
+import { defineConfig, loadEnv } from 'vite'
+import fs from 'fs/promises'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
 
-export default defineConfig(() => ({
-	plugins: [react(), eslint()],
-	esbuild: {
-		loader: "jsx",
-		include: /src\/.*\.jsx?$/,
-		// loader: "tsx",
-		// include: /src\/.*\.[tj]sx?$/,
-		exclude: [],
-	},
-	build: {
-		outDir: "build",
-	},
-	define: {
-		"process.env": {}
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			loader: {".js" : "jsx"},
-			plugins: [
-				{
-					name: "load-js-files-as-jsx",
-					setup(build) {
-						build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
-							loader: "jsx",
-							contents: await fs.readFile(args.path, "utf8"),
-						}))
-					},
-				},
-			],
-		},
-	},
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://imp.cs.umu.se:80"
-			},
-			"/user": {
-				target: "http://imp.cs.umu.se:80"
-			},
-		},
-		port: 3000,
-	},
-
-}))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react(), eslint()],
+    esbuild: {
+      loader: 'jsx',
+      include: /src\/.*\.jsx?$/,
+      // loader: "tsx",
+      // include: /src\/.*\.[tj]sx?$/,
+      exclude: [],
+    },
+    build: {
+      outDir: 'build',
+    },
+    define: {
+      'process.env': {},
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: { '.js': 'jsx' },
+        plugins: [
+          {
+            name: 'load-js-files-as-jsx',
+            setup(build) {
+              build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+                loader: 'jsx',
+                contents: await fs.readFile(args.path, 'utf8'),
+              }))
+            },
+          },
+        ],
+      },
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.USE_IMP_SERVER === "true" ? 'http://imp.cs.umu.se:80' : 'http://localhost:80',
+        },
+        '/user': {
+          target: env.USE_IMP_SERVER === "true" ? 'http://imp.cs.umu.se:80' : 'http://localhost:80',
+        },
+      },
+      port: 3000,
+    },
+  }
+})
