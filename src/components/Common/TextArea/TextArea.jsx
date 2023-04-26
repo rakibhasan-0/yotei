f15cc958a8019c0e147487275d540e5223df975b
Jaskaran Heldestad <dv19jhd@cs.umu.se>
2023-04-26 11:00:53 +0000

    29_chimera: Updated textarea
    Merge branch '29_chimera' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Christoffer Billman <id19cbn@cs.umu.se>
    
    Description
    This is a fix in regards to ticket [#29](https://trello.com/c/eSkdRXh0/29-textarea-standardkomponent)
    
    Branch commits
    * Update textarea

--- src/components/Common/TextArea/TextArea.jsx
+++ src/components/Common/TextArea/TextArea.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import "./TextArea.css"
 
 /**
- * This is the standard textarea page, used for using standard textareas.
+ * This is the standard textarea page, used for all textareas.
  * 
  * The props object is used for holding the written text
  * from and display it on the textArea. 
@@ -10,19 +10,33 @@ import "./TextArea.css"
  *    placeholder: string
  *    text: string
  *    onChange: function
+ *    required: function
+ *    onBlur: function
+ *    onInput: function
+ *    readOnly: boolean
+ *    id: string
+ *    defVal: string
+ *    type: string
  * }
  * 
  * @author Team Chimera
- * @version 2.0
+ * @version 3.0
  * @since 2023-04-24
  */
-export default function TextArea({ placeholder, text, onChange }) {
-	return(
-		<textarea
-			className="stand-area"
-			placeholder={placeholder}
-			value={text}
-			onChange={e => onChange(e.target.value)}
-		/>
-	)
+export default function TextArea({ placeholder, text, onChange, required, onBlur, onInput, readOnly, id,defVal,type}) {
+    return(
+        <textarea
+            className="stand-area"
+            placeholder={placeholder}
+            value={text}
+            defaultValue={defVal}
+            onChange={onChange}
+            onBlur={onBlur}
+            onInput={onInput}
+            required={required}
+            readOnly={readOnly}
+            id={id}
+            type={type}
+        />
+    ); 
 }
