99dbb90b470fda94fd0bf28eeb7bd9a3de5b7b11
Alexander Arvidsson Örnberg <dv21aag@cs.umu.se>
2023-05-03 12:44:39 +0000

    undo revert of mr
    Merge branch '91_medusa' into 'main'
    
    Reviewed by 
    Approved by 
    
    Description
    Readd mr
    
    Branch commits
    * test: made Navbar.test file.
    
    * deps: added user-event dependency.
    Did this because without it unit testing with react testing library is impossible.
    
    * fix: fix font and sizing.
    
    * replaced old navbar with new base layout.
    
    * feat: implemented new base layout.
    
    * feat: implemented new sidebar and navbar.

---
+++ src/components/Common/Navbar/Navbar.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react"
+
+import Button from "../Button/Button"
+
+import { List as HamburgerIcon, X as CloseIcon } from "react-bootstrap-icons"
+
+import "./Navbar.css"
+import { useNavigate } from "react-router"
+/**
+ * The navbar for the entire page.
+ * 
+ * Props:
+ * 		testId @type {string}  - An id for its outermost div.
+ *
+ * Example usage:
+ * 		<Navbar testId="test-id"/>
+ *
+ * @author Team Medusa
+ * @version 1.0
+ * @since 2023-05-02
+ */
+function Navbar({ testId }) {
+
+	// Controls whether or not the menu is open. 
+	const [open, setOpen] = useState(false)
+
+	const navigate = useNavigate()
+
+	const navigateAndClose = path => {
+		navigate(path)
+		setOpen(false)
+	}
+
+	return (
+		<nav data-testid={testId} className={"common-navbar"}>
+			<HamburgerIcon role="button" className="common-navbar-icon" size="48px" onClick={() => setOpen(true)}/>
+			<img src="ubk-logga.jpg" className="budo-logo"/>
+
+			<div className={`common-navbar-sidebar p-4 ${open ? "open" : ""}`}>
+
+				<CloseIcon role="button" className="common-navbar-icon" size="48px" onClick={() => setOpen(false)}/>
+
+				<Button onClick={() => navigateAndClose("/plan")}>
+					<h1 style={{fontWeight: 500, fontSize: "32px"}}>Grupper</h1>
+				</Button>
+
+				<Button onClick={() => navigateAndClose("/workout")}>
+					<h1 style={{fontWeight: 500, fontSize: "32px"}}>Pass</h1>
+				</Button>
+
+				<Button onClick={() => navigateAndClose("/exercise")}>
+					<h1 style={{fontWeight: 500, fontSize: "32px"}}>Övningar</h1>
+				</Button>
+
+				<Button onClick={() => navigateAndClose("/technique")}>
+					<h1 style={{fontWeight: 500, fontSize: "32px"}}>Tekniker</h1>
+				</Button>
+
+				<Button onClick={() => navigateAndClose("/admin")}>
+					<h1 style={{fontWeight: 500, fontSize: "32px"}}>Admin</h1>
+				</Button>
+
+				<Button onClick={() => navigateAndClose("/profile")}>
+					<h1 style={{fontWeight: 500, fontSize: "32px"}}>Min sida</h1>
+				</Button>
+
+			</div>
+		</nav>
+	)
+
+}
+export default Navbar
