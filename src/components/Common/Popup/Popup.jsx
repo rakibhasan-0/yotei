3040c539d433122b236e8b0c6eb38a9fa0e60eae
Alexander Lindblom <c20alm@cs.umu.se>
2023-05-03 11:54:39 +0000

    79_medusa: Added standard component for popup window
    Merge branch '79_medusa' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Emmy Lindgren <id19eln@cs.umu.se>
    
    Description
    https://trello.com/c/YGmd36vo/79-allm%C3%A4n-popup-med-styling
    
    Added standard component for popup window. The popup has an optional title prop.
    
    Branch commits
    * Update InputTextField.css
    
    * Removed unnecessary font import/font-family
    
    * Removed unnecessary import
    
    * Finished writing tests for Popup
    
    * Added role to title and docs
    
    * Moved popup to common and created first test
    
    * Thin line only visible with a title
    
    * Added pointer when hovering over the close button
    
    * Increased the close button size
    
    * added open-sans to index.css
    
    * Moved the scroll bar to the right
    
    * changed to icon component instead of svg
    
    * css refactor needed
    
    * final stages of styling
    
    * open and closing now works
    
    * added background and popup window

---
+++ src/components/Common/Popup/Popup.jsx
@@ -0,0 +1,52 @@
+import { X } from "react-bootstrap-icons"
+import "./Popup.css"
+import React from "react"
+
+/**
+ * Popup is a component that creates a popup window with a title and a close button.
+ * 
+ * 
+ * Props:
+ *     title @type {string}  - Sets the title of the popup window
+ *     id @type {string} - Sets the id of the popup window
+ * 	   isOpen @type {boolean} - Sets if popup should be visible or not
+ *     setIsOpen @type {useState} - Sets the state of the popup window
+ *
+ * Example usage:
+ *  
+ *	const [showPopup, setShowPopup] = useState(false)
+ *		return (
+ *			<div>
+ *				<button onClick={() => setShowPopup(true)}>Skapa teknik</button>
+ *				<Popup isOpen={showPopup} setIsOpen={setShowPopup} title="Skapa teknik">
+ *					<InputTextField placeholder="Namn"></InputTextField>
+ *				</Popup>
+ *			</div>
+ *		)
+ *
+ * @author Team Medusa
+ * @version 1.0
+ * @since 2023-05-02
+ */
+export default function Popup({ title, id, isOpen, setIsOpen, children }) {
+	if (!isOpen)
+		return null
+
+	return (
+		<>
+			<div className="popup-bg" onClick={() => setIsOpen(false)} />
+			<div className="popup" id={id}>
+				<div className="topbar">
+					{title && <h1 className="title" role="title">{title}</h1>}
+					<button className="closebutton" onClick={() => setIsOpen(false)}>
+						<X width={44} height={44} />
+					</button>
+					{title && <div className="horizontal-line" />}
+				</div>
+				<div className="content">
+					{children}
+				</div>
+			</div>
+		</>
+	)
+}
