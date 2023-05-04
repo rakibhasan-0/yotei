ac393add253a821d91a6e2e66ddd5738e1c01b14
Olle Lögdahl <dv20oll@cs.umu.se>
2023-05-04 10:36:49 +0000

    Added configs for gateway ports
    Merge branch '88_minotaur' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Carl Gulliksson <dv21cgn@cs.umu.se>
    
    Description
    The gateways forwarding ports are now configurable.
    This only accounts for the docker target.
    
    Related to:
    https://git.cs.umu.se/courses-project/5dv214vt23/infra/-/merge_requests/17
    
    Branch commits
    * Added configs for gateway ports
    
    The gateways forwarding ports are now configurable.
    This only accounts for the docker target.
    
    Related to:

--- gateway/src/test/java/se/umu/cs/pvt/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/se/umu/cs/pvt/gateway/GatewayApplicationTests.java
@@ -7,6 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes = GatewayApplication.class)
 class GatewayApplicationTests {
 
+	static {
+		System.setProperty("BACKEND_HOST", "http://localhost:80");
+		System.setProperty("WEBSERVER_HOST", "http://localhost:80");
+	}
+
 	@Test
 	void contextLoads() {
 	}
