bbabd2c517c2715d622c961d00fa7afb2d865757
Daniel Vesterlund <id19dvd@cs.umu.se>
2023-05-04 12:39:51 +0000

    40_kraken: Added id and comments to Tag component
    Merge branch '40_kraken' into 'main'
    
    Reviewed by 
    Approved by 
    
    Description
    [](https://trello.com/c/6CQy2BVC/40-ge-komponenten-tag-ett-id)
    
    Added id and comments to Tag component
    
    Branch commits
    * Added id and comments to Tag component

--- src/components/Common/Tag/Tag.jsx
+++ src/components/Common/Tag/Tag.jsx
@@ -5,41 +5,50 @@ import { PlusCircle, XCircle } from "react-bootstrap-icons"
 /**
  * Creates the UI for the Tags
  *  Props:
- *  tagType (see below)
- *  onClick, 
- *  text (the text in the tag).
+ * 		id @type {string} - id for the component
+ *  	tagType @type {string} - (see below)
+ *  	onClick @type {function} - Called when the component is clicked
+ *  	text @type {string} - Text in the component
  * 
  * tagType must be one of three
  * 1. "default": pink tag without icon.
  * 2. "suggest": gray tag with plus icon.
  * 3. "added": pink tag with x icon.
  * 
+ * Example usage: 
+ * 		<Tag 
+ * 			id="tag-searchbar"
+ * 			tagType="suggest"
+ * 			onClick={onClick}
+ * 			text="Spark"
+ * 		/>
+ * 
  * @author: Team Kraken (Group 7)
- * @version: 1.0
+ * @version: 2.0
  */
 
-export default function Tag({ tagType, onClick, text }) {
+export default function Tag({ id, tagType, onClick, text }) {
 	if (!((tagType === "default") || (tagType === "suggest") || (tagType === "added"))) {
 		throw new Error("Tag must have type of default, suggest or added")
 	}
 
 	if (tagType === "default") {
 		return (
-			<button className="base-tag" onClick={onClick}>
+			<button id={id} className="base-tag" onClick={onClick}>
 				<p className="no-margin">{text}</p>
 			</button>
 
 		)
 	} else if (tagType === "suggest") {
 		return (
-			<button className="base-tag suggest" onClick={onClick}>
+			<button id={id} className="base-tag suggest" onClick={onClick}>
 				<p className="no-margin">{text}</p>
 				<PlusCircle className="ml-2" size={18} />
 			</button>
 		)
 	} else {
 		return (
-			<button className="base-tag" onClick={onClick}>
+			<button id={id} className="base-tag" onClick={onClick}>
 				<p className="no-margin">{text}</p>
 				<XCircle className="ml-2" size={18} />
 			</button>
