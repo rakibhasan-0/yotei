ccd7461853d82e041ca1799b166ef79bc92609cc
Pontus Lövlund <ens20pld@cs.umu.se>
2023-05-08 13:39:58 +0000

    37 minotaur: Implemented standard components for tags and created tag popup
    Merge branch '37_minotaur' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Christoffer Billman <id19cbn@cs.umu.se>
    
    Description
    https://trello.com/c/aSMYCgtM
    
    Implemented standard components for choosing tags and the popup for it.
    
    Branch commits
    * Changes to tests in AddTagPopup
    
    * WIP: updates to tagchosing-components.
    
    * Added popup for adding tags. WIP.
    
    * Added taginput-standard component. WIP.
    
    * Worked on adding tags-components.
    
    * Styled component some more, WIP.
    
    * WIP: updates to tagchosing-components.
    
    * Added popup for adding tags. WIP.
    
    * Created more tests for AddTagPopup, Tag and TagInput components.
    
    * Created some tests for tags.
    
    * Component for adding tags done!
    
    * Tests for tag-component done.
    
    * WIP: updates to tagchosing-components.
    
    * Added popup for adding tags. WIP.
    
    * Added taginput-standard component. WIP.
    
    * Started working on tests for tag-component.
    
    * Worked on adding tags-components.
    
    * Added max-width.
    
    * Styled component some more, WIP.
    
    * Added functionality for creating a new tag. WIP.
    
    * WIP: updates to tagchosing-components.
    
    * Added taginput-standard component. WIP.
    
    * WIP. Added tagadding to popup.
    
    * Added popup for adding tags. WIP.
    
    * Added taginput-standard component. WIP.

---
+++ src/components/Common/Tag/TagInput.jsx
@@ -0,0 +1,57 @@
+import React from "react"
+import { useState} from "react"
+import Tag from "./Tag"
+import "./TagInput.css"
+import Popup from "../Popup/Popup"
+import AddTagPopup from "./AddTagPopup"
+/**
+ * Taginput is a component to use for choosing tags and displaying chosen tags. 
+ * 
+ * Props:
+ *     id @type {string} - Sets the id of the taginput. 
+ * 	   addedTags @type {List of tags} - The list of choosen tags.
+ *     setAddedTags @type {useState} - Sets the list of chosen tags. 
+ *
+ * Example usage:
+ *  
+ *	const [addedTags, setAddedTags] = useState([])
+ *		return (
+ *			<div>
+ *				<TagInput id ="tagChooser" addedTags={addedTags} setAddedTags={setAddedTags}/>
+ *			</div>
+ *		)
+ *
+ * @author Team Minotaur
+ * @version 1.0
+ * @since 2023-05-04
+ */
+export default function TagInput({id, addedTags,setAddedTags}) {
+	const [showPopup, setShowPopup] = useState(false)
+	const handleRemoveTag = (tag) => {
+		const copy = [...addedTags]
+		const newAdded = copy.filter(tagInCopy => tagInCopy.id !== tag.id)
+		setAddedTags(newAdded)
+	}
+
+	return (
+		<div id = {id} className="add-tag-container">
+			<Tag 
+				tagType="suggest"
+				key ="addMoreTags"
+				text ="Lägg till tagg"
+				onClick={() => setShowPopup(true)}
+			/>
+			<div className="added-tag-container">
+				{addedTags.map(tag => <Tag
+					tagType="added"
+					key={tag.id}
+					text={tag.name}
+					onClick={() => handleRemoveTag(tag)}
+				/>)}
+			</div>
+			<Popup title="Lägg till tagg" id= "addTagPopUp" isOpen={showPopup} setIsOpen={setShowPopup}>
+				<AddTagPopup id ="addTagPopupDiv" addedTags={addedTags} setAddedTags={setAddedTags} setIsOpen={setShowPopup}/>
+			</Popup>
+		</div>
+	)
+}
