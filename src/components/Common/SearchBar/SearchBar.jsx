358d9b23e83b10d6910ea157f9363d518d41c185
Daniel Vesterlund <id19dvd@cs.umu.se>
2023-05-08 12:08:42 +0000

    43 kraken: Updated searchbar with tags and created tests for the component.
    Merge branch '43_kraken' into 'main'
    
    Reviewed by 
    Approved by 
    
    Description
    [](https://trello.com/c/xHZlyLO7/43-ut%C3%B6ka-standardkomponent-f%C3%B6r-s%C3%B6kruta)
    
    The searchbar has been updated so that it contains tags that can be added and removed. Tests for the SearchBar component has also been created.
    
    Branch commits
    * Updated searchbar with tags and created tests for the component
    
    * Updated Tag component with id and comments
    
    * Added height to tag
    
    * WIP: searchbar updated with tags

--- src/components/Common/SearchBar/SearchBar.jsx
+++ src/components/Common/SearchBar/SearchBar.jsx
@@ -1,26 +1,89 @@
-import React from "react"
-import SearchIcon from "../../../../public/search.svg"
+import React, { useState } from "react"
 import "./SearchBar.css"
-
+import { Search } from "react-bootstrap-icons"
+import Tag from "../Tag/Tag"
 /**
- * Creates the UI for the searchbar. Props: placeholder (placeholdertext in searchbar),
- * text (optional value (text) for the searchbar),onChange (for when searchtext is entered). 
+ * Creates the UI for the searchbar.
+ * 
+ * Props:
+ * 	   id @type {string} - id for the component
+ *     placeholder @type {string}  - The placeholdertext in searchbar
+ *     text @type {string} - The value in the searchbar
+ *     onChange @type {function} - Called when the value is changed
+ * 	   addedTags @type {Array} - List of added tags
+ *	   setAddedTags @type {function} - Setter for addedTags
+ *     suggestedTags @type {Array} - List of suggested tags
+ *     setSuggestedTags @type {function} - Setter for suggestedTags
+ *
+ * Example usage:
+ *     <SearchBar 
+ * 			id="searchbar-techniques" 
+ * 			placeholder="Sök" 
+ * 			text={query} 
+ * 			onChange={setQuery}
+ * 			addedTags={tags}
+ * 			setAddedTags={setTags}
+ * 			suggestedTags={suggestedTags}
+ * 			setSuggestedTags={setSuggestedTags}
+ * 		/>
  *
  * @author Team Minotaur Mavericks & Team Kraken (Group 8,Group 7)
  * @since 2023-05-02
  * @version 2.0 
  */
-export default function SearchBar({placeholder,text,onChange, id}) {
+
+export default function SearchBar({ id, placeholder, text, onChange, addedTags, setAddedTags, suggestedTags, setSuggestedTags }) {
+
+	const [focused, setFocused] = useState(false)
+
+	const handleAddTag = (tag) => {
+		// if tag is already added, don't add it
+		for (const tagInList of addedTags) {
+			if (tag === tagInList) return
+		}
+		setAddedTags([...addedTags, tag])
+		// removes the added tag from suggestions
+		const copy = [...suggestedTags]
+		const newSuggested = copy.filter(tagInCopy => tagInCopy !== tag)
+		setSuggestedTags(newSuggested)
+	}
+
+	const handleRemoveTag = (tag) => {
+		const copy = [...addedTags]
+		const newAdded = copy.filter(tagInCopy => tagInCopy !== tag)
+		setAddedTags(newAdded)
+		setSuggestedTags([...suggestedTags, tag])
+	}
+
 	return (
-		<>
-			<div id={id} className='search-bar'>
-				<input
-					className="input-area"
-					placeholder={placeholder}
-					value={text}
-					onChange={e => onChange?.(e.target.value)}></input>
-				<i><img className= "search-icon" src={SearchIcon} alt='Search icon'/></i>
+		<div id={id} className={focused ? "search-bar open" : "search-bar"}>
+			<input
+				className="input-area"
+				placeholder={placeholder}
+				value={text}
+				onChange={e => {
+					return onChange?.(e.target.value)
+				}}
+				onFocus={() => setFocused(true)}
+				onBlur={() => setFocused(false)}
+			>
+			</input>
+			<i className="search-icon"><Search /></i>
+			<div className={focused || (addedTags.length > 0 || suggestedTags.length > 0) ? "tag-container" : "tag-container closed"}>
+				{addedTags.map(tag => <Tag
+					tagType="added"
+					key={tag}
+					text={tag}
+					onClick={() => handleRemoveTag(tag)}
+				/>)}
+				{suggestedTags.map(tag => <Tag
+					id={"tag-in-searchbar"}
+					tagType="suggest"
+					key={tag}
+					text={tag}
+					onClick={() => handleAddTag(tag)}
+				/>)}
 			</div>
-		</>
+		</div>
 	)
 }
--- src/components/Common/Tag/Tag.jsx
+++ src/components/Common/Tag/Tag.jsx
@@ -5,23 +5,24 @@ import { PlusCircle, XCircle } from "react-bootstrap-icons"
 /**
  * Creates the UI for the Tags
  *  Props:
- * 		id @type {string} - id for the component
- *  	tagType @type {string} - (see below)
- *  	onClick @type {function} - Called when the component is clicked
- *  	text @type {string} - Text in the component
+*  	    id @type {string} - id for the component
+*	    tagType @type {string} - (see below)
+*       onClick @type {function} - called when tag is clicked
+*       text @type {string} - the text in the tag
  * 
- * tagType must be one of three
- * 1. "default": pink tag without icon.
- * 2. "suggest": gray tag with plus icon.
- * 3. "added": pink tag with x icon.
+ * tagType must be one of three:
+ * 		1. "default": pink tag without icon.
+ * 		2. "suggest": gray tag with plus icon.
+ * 		3. "added": pink tag with x icon.
  * 
- * Example usage: 
- * 		<Tag 
- * 			id="tag-searchbar"
- * 			tagType="suggest"
+ * Example usage:
+ * 		<Tag
+ * 			id="tag-for-exercise"
+ * 			tagType="default"
  * 			onClick={onClick}
  * 			text="Spark"
  * 		/>
+ * 
  * 
  * @author: Team Kraken (Group 7)
  * @version: 2.0
