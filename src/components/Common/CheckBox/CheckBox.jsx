b5e47164938379baf01a92dfc34fe55c5fb7f849
Christoffer Billman <id19cbn@cs.umu.se>
2023-05-08 09:08:43 +0000

    refactor: rewrote checkbox component.
    Merge branch '111_medusa' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Carl Gulliksson <dv21cgn@cs.umu.se>
    
    Description
    [#111](https://trello.com/c/Hj0OaY1P/111-fixa-standard-komponenten-checkbox-så-att-den-kan-stängas-av)
    
    The checkbox component has been rewritten such that it uses a native HTML input-element.
    
    It also has added features such as having a disabled state and being able to set a label.
    
    Branch commits
    * refactor: rewrote checkbox component.

--- src/components/Common/CheckBox/CheckBox.jsx
+++ src/components/Common/CheckBox/CheckBox.jsx
@@ -1,37 +1,34 @@
-import React from "react"
 import "./CheckBox.css"
-import {Square , CheckSquareFill} from "react-bootstrap-icons"
+import { Check, Dash } from "react-bootstrap-icons"
 
 /** 
  * A default checkbox that should be used throughout the project.
  * 
+ * Example usage:
+ * 		<CheckBox
+ * 			checked={checked}
+ * 			onClick={setChecked}
+ * 			label="Hello"
+ * 			disabled
+ * 			id="test-id"
+ * 		/>
  * 
- * 
- * Show the properties that can be set in the props object below:
- * props = {
- *     checked: boolean
- *     onClick: function,
- *     id: string
- * }
- * 
- * example:
- * const [state, setChecked] = useState(false)
- * <CheckBox checked=state onClick={()=>setChecked(!checked)}></CheckBox>
- * 
- *   
- * @author Chimera
+ * @author Medusa
  * @since 2023-05-02
- * @version 2.0 
+ * @version 3.0 
  */
-export default function CheckBox({checked, onClick, id}) {
+export default function CheckBox({checked, onClick, label, disabled, id}) {
 	return (
-		<div id={id} className="checkbox-container" onClick={onClick}>
-			{checked
-				? <>
-					<CheckSquareFill className='check'/>
-				</>
-				: <Square className='square' />
-			}  
-		</div>  
+		<label className="checkbox-label" id={id}>
+			{disabled && <Dash className="checkbox-icon"/>}
+			{checked && <Check className="checkbox-icon"/>}
+			<input
+				type="checkbox"
+				value={checked}
+				onChange={() => onClick(!checked)}
+				disabled={disabled}
+			/>
+			{label}
+		</label>
 	)
 }
