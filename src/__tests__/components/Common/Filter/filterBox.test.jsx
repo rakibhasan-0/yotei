9b7357f166d36972cb544e7079bd329f1d56d065
Fardis Nazemroaya Sedeh <dv20fnh@cs.umu.se>
2023-05-08 09:25:18 +0000

    47 kraken: Finished writing tests for FilterBox component
    Merge branch '47_kraken' into 'main'
    
    Reviewed by 
    Approved by 
    
    Description
    https://trello.com/c/alvj5p2a 
    
    Added additional tests that check for undefined behavior and different props config.
    
    Branch commits
    * fixed indentation for lint
    
    * added tests for filterBox
    
    * Edited filename
    
    * added tests for filterBox
    
    * Edited filename

--- src/__tests__/components/Common/Filter/filterBox.test.jsx
+++ src/__tests__/components/Common/Filter/filterBox.test.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import {render, screen, configure} from "@testing-library/react"
 import "@testing-library/jest-dom"
 import FilterBox from "../../../../components/Common/Filter/FilterBox/FilterBox"
+import CheckBox from "../../../../components/Common/CheckBox/CheckBox"
 
 configure({testIdAttribute: "id"})
 
@@ -25,3 +26,31 @@ test("Filter hidden when status is false.", async() => {
 
 	expect(className).toBe("filterBox-pressed") 
 })
+
+
+test("FilterBox: Filter has children", async() => {
+	render(
+        
+		<FilterBox id={"filterBox"} status={true}>
+			<p id={"p"}className="kihon-text">Kihon</p>
+			<CheckBox id={"check"} />
+		</FilterBox>
+	)
+
+	const filterBox = screen.getByTestId("filterBox")
+
+	expect(filterBox.contains(screen.getByTestId("p")) && filterBox.contains(screen.getByTestId("check"))).toBeTruthy()
+
+})
+
+test("FilterBox: works with no props", async() => {
+	render(
+		<FilterBox id={"filterBox"}/>	
+	)
+
+	const filterBox = screen.getByTestId("filterBox")
+
+	expect(filterBox).not.toBeUndefined()
+
+})
+
