cdbad8f52c4beba2c2fd9f064e45dadfa5342f47
Julia Johansson <dv21jjn@cs.umu.se>
2023-05-15 15:56:11 +0000

    110_griffin: Adjusted the SessionHeader to validate input and handle render errors
    Merge branch '110_griffin' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Hanna Littorin <c19hln@cs.umu.se>
    
    Description
    Merge branch '110_griffin' into 'main'
    
    Reviewed by 
    Approved by
    
    
    Description Fixes ticket [#110](https://trello.com/c/2iNoWcbW/110-group-plan-view-components-sessionheader)
    
    ** Branch commits **
    * implemented error handling during rendering to not cause crashes
    * implemented tests to verify error handling
    
    # Previous Commits
    ** Branch commits **
    * adapted CSS classnames to be more specific
    
    
    ** Branch commits **
    * Implemented component
    * Documented component, functionality and properties
    
    ** Testing **
    * Input validation: invalid ID renders placeholder component
    * Input validation: invalid title renders with title = unnamed
    
    Needs to be done but hasn't because of time-limit
    
    * Input validation: invalid format day
    * Input validation: invalid format date
    * Input validation: invalid format time
    
    Branch commits
    * Adjusted the SessionHeader to validate input and handle render errors

--- src/__tests__/components/Plan/SessionHeader.test.jsx
+++ src/__tests__/components/Plan/SessionHeader.test.jsx
@@ -13,34 +13,131 @@ describe("SessionHeader", () => {
 
 	describe("When initialized", () => {
 
-		test("should render component with given ID -> testSessionHeader", () => {
+		test("should render component with given ID -> testSessionHeader", async() => {
 			render(<SessionHeader id = "testSessionHeader"/>)
 			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveAttribute("id", "testSessionHeader-session-header")
 		})
 
-		test("should render component containing given title -> testTitle", () => {
+		test("should render component containing given title -> testTitle", async() => {
 			render(<SessionHeader id = "testSessionHeader" title="testTitle"/>)
 			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("testTitle")
 		})
 
-		test("should render component containing given day Monday -> Mon", () => {
-			render(<SessionHeader id = "testSessionHeader" title="testTitle" day="Mon"/>)
-			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("Mon")
+		test("should render component containing given day Mån -> Mån", async() => {
+			render(<SessionHeader id = "testSessionHeader" title="testTitle" day="Mån"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("Mån")
+		})
+
+		test("should render component containing given day Tis -> Tis", async() => {
+			render(<SessionHeader id = "testSessionHeader" title="testTitle" day="Tis"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("Tis")
+		})
+
+		
+		test("should render component when given weekday Ons -> Ons", async() => {
+			render(<SessionHeader id = "testSessionHeader" title={test} day="Ons" date="01/01"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("Ons")
+		})
+
+		
+		test("should render component when given weekday Tors -> Tors", async() => {
+			render(<SessionHeader id = "testSessionHeader" title={test} day="Tors" date="01/01"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("Tors")
+		})
+		
+		
+		test("should render component when given weekday Fre -> Fre", async() => {
+			render(<SessionHeader id = "testSessionHeader" title={test} day="Fre" date="01/01"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("Fre")
+		})
+
+		
+		test("should render component when given weekday Lör->Lör", async() => {
+			render(<SessionHeader id = "testSessionHeader" title={test} day="Lör" date="01/01"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("Lör")
+		})
+
+		
+		test("should render component when given weekday Sön -> Sön", async() => {
+			render(<SessionHeader id = "testSessionHeader" title={test} day="Sön" date="01/01"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("Sön")
+		})
+
+		test("should render component containing given date 01/01", async() => {
+			render(<SessionHeader id = "testSessionHeader" title={test} day="Sön" date="01/01"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("01/01")
+		})
+		
+		test("should render component containing given time 00:00", async() => {
+			render(<SessionHeader id = "testSessionHeader" title={test} day="Sön" date="01/01" time="00:00"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("00:00")
+		})
+
+		test("should render component containing given time 12:30", async() => {
+			render(<SessionHeader id = "testSessionHeader" title={test} day="Sön" date="01/01" time="12:30"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("12:30")
+		})
+
+		test("should render component containing given time 23:59", async() => {
+			render(<SessionHeader id = "testSessionHeader" title={test} day="Sön" date="01/01" time="23:59"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("23:59")
 		})
 	})
 
 	describe("When given invalid input", () => {
 
-		test("should not render component with invalid ID -> undefined", () => {
+		test("should not render component with invalid ID -> undefined", async() => {
 			const { container } = render(<SessionHeader/>)
 			const innerHtml = container.innerHTML
 
 			expect(innerHtml).toEqual(expect.stringContaining("Error loading component"))
 		})
 
-		test("should render component with title \"unamed\" if title is invalid", () => {
+		test("should render component with title \"unamed\" if title is invalid", async() => {
 			render(<SessionHeader id = "testSessionHeader" title={null}/>)
 			expect(screen.getByTestId("testSessionHeader-session-header")).toHaveTextContent("unnamed")
 		})
+
+		test("should render component when given invalid day input but without day-title", async() => {
+			render(<SessionHeader id = "testSessionHeader" title="testTitle" day="invalid"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).not.toHaveTextContent("invalid")
+		})
+
+		test("should render component when given invalid date input but without date-title 00/01", async() => {
+			render(<SessionHeader id = "testSessionHeader" title="testTitle" time="13:37" day="mon" date="00/01"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).not.toHaveTextContent("00/01")
+		})
+
+		test("should render component when given invalid date input but without date-title 32/01", async() => {
+			render(<SessionHeader id = "testSessionHeader" title="testTitle" time="13:37" day="mon" date="32/01"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).not.toHaveTextContent("32/01")
+		})
+
+		test("should render component when given invalid date input but without date-title 01/13", async() => {
+			render(<SessionHeader id = "testSessionHeader" title="testTitle" time="13:37" day="mon" date="01/13"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).not.toHaveTextContent("01/13")
+		})
+
+		test("should render component when given invalid date input but without date-title 1/01", async() => {
+			render(<SessionHeader id = "testSessionHeader" title="testTitle" time="13:37" day="mon" date="1/01"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).not.toHaveTextContent("1/01")
+		})
+
+		test("should render component when given invalid date input but without date-title 01/1", async() => {
+			render(<SessionHeader id = "testSessionHeader" title="testTitle" time="13:37" day="mon" date="01/1"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).not.toHaveTextContent("01/1")
+		})
+
+		test("should render component when given invalid time input but without time-title 24:00", async() => {
+			render(<SessionHeader id = "testSessionHeader" title="testTitle" day="mon" date="01/1" time="24:00"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).not.toHaveTextContent("24:00")
+		})
+		
+		test("should render component when given invalid time input but without time-title 23:60", async() => {
+			render(<SessionHeader id = "testSessionHeader" title="testTitle" day="mon" date="01/1" time="23:60"/>)
+			expect(screen.getByTestId("testSessionHeader-session-header")).not.toHaveTextContent("24:00")
+		})
+
+		
 	})
 })
--- src/components/Plan/SessionHeader.jsx
+++ src/components/Plan/SessionHeader.jsx
@@ -4,8 +4,9 @@ import React from "react"
 
 /**
  * 	The SessionHeader is a component which displays a Sessions information.
- * 	The properties of the component should match the given format. It is up
- * 	to the parent component to constrain the SessionHeader.
+ * 	The properties of the component should match the given format. The component
+ * 	has been implemented to validate the format of the given input and will not 
+ * 	render invalid input. It is up to the parent component to constrain the SessionHeader.
  * 
  * 	@param 		id 		@type { string }  		- ID of the component
  * 	@param 		title	@type { string }		- title of the session (centered text)
@@ -20,10 +21,13 @@ import React from "react"
  * 	@returns A SessionHeader
  * 
  * 
- * 	@author		Griffin DV21JJN
+ * 	@author		Griffin DV21JJN C19HLN
  */
 
 function SessionHeader ({ id, title, day, date, time }) {
+	let dayRegex = new RegExp("Mån|Tis|Ons|Tors|Fre|Lör|Sön")
+	let dateRegex = new RegExp("(([0][1-9])|([1-2][0-9])|(3[01]))[/]([0][1-9]|[1][0-2])")
+	let timeRegex = new RegExp("(([0][0-9]|[1][0-9]|[2][0-3])[:]([0][0-9]|[1-5][0-9]))")
 
 	function checkID () {
 		if (id === null || id === undefined) {
@@ -43,23 +47,74 @@ function SessionHeader ({ id, title, day, date, time }) {
 		return true
 	}
 
+	function checkDay () {
+		if (!dayRegex.test(day)) {
+			console.error("Invalid day-format")
+			return false
+		}
+
+		return true
+	}
+
+
+	function checkDate () {
+		if (!dateRegex.test(date)) {
+			console.error("Invalid date format")
+			return false
+		}
+
+		return true
+	}
+
+
+	function checkTime () {
+		if (!timeRegex.test(time)){
+			console.error("Invalid time format")
+			return false
+		}
+
+		return true
+	}
+
+
 	return (
 		checkID() ?
-			<div id = {`${id}-session-header`} className = "sc23-session-header d-flex justify-content-evenly">
+			(
+				<div id = {`${id}-session-header`} className = "sc23-session-header d-flex justify-content-evenly">
+					<div id = {`${id}-date`} className = "sc23-session-header-day-date sc23-session-header-item">
+						{
+						
+							checkDate() ?
+								<h2 className = "sc23-session-header-text">{date}</h2>
+								:
+								<h2 className = "sc23-session-header-text">{"     "}</h2>
+						}
+						{
 
+							checkDay() ?
+								<h2 className = "sc23-session-header-text testanuda">{day}</h2>
+								:
+								<h2 className = "sc23-session-header-text testanuda">{"   "}</h2>
+						}
+					</div>
+					{
+						checkTitle() ?
+							<div id = {`${id}-session-header-title`} className = "sc23-session-header-title sc23-session-header-item"><h2 className = "sc23-session-header-text">{title}</h2></div>
+							:
+							<div id = {`${id}-session-header-title-error`} className = "sc23-session-header-title sc23-session-header-item"><h2 className = "sc23-session-header-text">unnamed</h2></div>
+					}
 
-				<div id = {`${id}-date`} className = "sc23-session-header-day-date sc23-session-header-item"><h2>{date + "  " + day}</h2></div>
-
-				{
-					checkTitle() ?
-						<div id = {`${id}-session-header-title`} className = "sc23-session-header-title sc23-session-header-item"><h2>{title}</h2></div>
-						:
-						<div id = {`${id}-session-header-title-error`} className = "sc23-session-header-title sc23-session-header-item"><h2>unnamed</h2></div>
-				}
-
-				<div id = {`${id}-time`} className = "sc23-session-header-time sc23-session-header-item"><h2>{time}</h2></div> 
+					<div id = {`${id}-time`} className = "sc23-session-header-time sc23-session-header-item">
+						{
+							checkTime() ?
+								<h2 className = "sc23-session-header-text">{time}</h2>
+								:
+								<h2 className = "sc23-session-header-text">{"   "}</h2>
+						}
+					</div> 
 					
-			</div>
+				</div>
+			)
 			:
 			<div id = "error-session-header" className = "sc23-session-header-session-header">Error loading component</div>
 	)
