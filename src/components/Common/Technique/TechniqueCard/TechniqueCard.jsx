c7675c8bd321f8a77b90cd7ff03effa1d4186f40
Oscar Bohlin <oscarb@accum.se>
2023-05-16 13:38:28 +0000

    133_medusa: Technique card color on side instead of top
    Merge branch '133_medusa' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Oscar Bohlin <oscarb@accum.se>
    Approved-by: Christoffer Billman <id19cbn@cs.umu.se>
    
    Description
    Trello: https://trello.com/c/DypJatvX/133-teknikkort-med-f%C3%A4rg-p%C3%A5-kortsida
    
    Put the color in the side instead of the top
    
    New design in figma has been created and approved.
    
    ![Screenshot_from_2023-05-16_10-19-55](/uploads/b0be3a7a3e8a58e1a6ba6f8bdfbf2ceb/Screenshot_from_2023-05-16_10-19-55.png)
    ![Screenshot_from_2023-05-16_10-19-49](/uploads/44947c8d3223e5390101f5627d5c38ce/Screenshot_from_2023-05-16_10-19-49.png)
    ![Screenshot_from_2023-05-16_10-22-20](/uploads/e977dbf769112c5ed436e33c6f5d72cb/Screenshot_from_2023-05-16_10-22-20.png)
    
    Branch commits
    * fix lint for ReviewFormComponent.jsx
    
    * remove unused code
    
    * fix lint for TechniqueCard.jsx
    
    * Revert "lint fix"
    
    This reverts commit 7664c4d04ad64e5769e42ac71b4873c413ff7bfc.
    
    * lint fix
    
    * Färgen på teknikkort är nu på sidan av tekniken och inte ovanpå

--- src/components/Common/Technique/TechniqueCard/TechniqueCard.jsx
+++ src/components/Common/Technique/TechniqueCard/TechniqueCard.jsx
@@ -18,26 +18,10 @@ function TechniqueCard({ technique, checkBox, id }) {
 	
 	return (
 		<div className="technique-card" id={id}>
-			<div className="technique-card-belt-color-container">
-				{
-					technique.beltColors !== undefined ?
-						technique.beltColors.length > 0 ?  	
-							technique.beltColors.map((belt) => {
-								return (belt !== undefined ?
-									belt.is_child ?
-										constructChildBelt(technique, belt)
-										: 
-										constructAdultBelt(technique, belt) 
-									: constructAdultBelt("13c9ed") 
-								)})
-							:	constructDefaultBelt(technique, "8e03ad") //om vi fär 0st fägerger (lila)
+			{constructColor(technique)}
 
-						: constructDefaultBelt(technique, "8e03ad") //om vi inte får färger (lila)
-					
-				}
-				
-			</div>
 			<div className="technique-info-container">
+
 				{checkBox ? 
 					<div className="technique-checkbox-container">
 						{checkBox}
@@ -45,23 +29,51 @@ function TechniqueCard({ technique, checkBox, id }) {
 					:
 					null
 				}
+
+
 				<div className="technique-name-container">
 					<a href={"/technique/technique_page/" + technique.techniqueID}>
 						<h5 className="technique-name">{technique.name}</h5>	
 					</a>
 				</div>
 
-				<a href={"/technique/technique_page/" + technique.techniqueID}>
-					<div className="technique-arrow-container">
+				<div className="technique-arrow-container">
+					<a href={"/technique/technique_page/" + technique.techniqueID}>
 						<ChevronDown />
-					</div>
-				</a>
+					</a>
+				</div>
+				
 
 			</div>
 		</div>
+
 	)
 }
 
+
+function constructColor(technique) {
+	return (
+		<div className="technique-card-belt-color-container">
+			{
+				technique.beltColors !== undefined ?
+					technique.beltColors.length > 0 ?  	
+						technique.beltColors.map((belt) => {
+							return (belt !== undefined ?
+								belt.is_child ?
+									constructChildBelt(technique, belt, technique.beltColors.length)
+									: 
+									constructAdultBelt(technique, belt, technique.beltColors.length) 
+								: constructAdultBelt("13c9ed", technique.beltColors.length) 
+							)})
+						:	constructDefaultBelt(technique, "8e03ad") //om vi fär 0st fägerger (lila)
+
+					: constructDefaultBelt(technique, "8e03ad") //om vi inte får färger (lila)
+					
+			}
+					
+		</div>
+	)
+}
 
 function constructDefaultBelt(color) {
 	return (
@@ -72,23 +84,34 @@ function constructDefaultBelt(color) {
 	)
 }
 
-function constructAdultBelt(technique, belt) {
+function constructAdultBelt(technique, belt, beltLength) {
 	return (
 		<div
 			key={technique.techniqueID + belt.belt_name + belt.is_child}
 			className={["technique-card-belt-color", belt.belt_name === "Vitt" ? "technique-card-belt-border" : ""].join(" ")}
-			style={{background: `#${belt.belt_color}`}}
+			style={
+				{
+					background: `#${belt.belt_color}`,
+					height: `${100 / beltLength}%`
+				}
+			}
 		/>
 	)
 
 }
 
-function constructChildBelt(technique, belt) {
+function constructChildBelt(technique, belt, beltLength) {
 	return (
 		<div 
 			key={technique.techniqueID  + belt.belt_name + belt.is_child}
 			className={["technique-card-belt-color", "technique-card-belt-border"].join(" ")}
-			style={{background: `linear-gradient( #fff 33%, #${belt.belt_color} 33%, #${belt.belt_color} 66%, #fff 0)`}}
+			style={
+				{
+					// background: `radial-gradient(circle, rgba(0,0,0,1) 20%, rgba(0,0,0,1) 70%, rgba(255,255,255,1) 70%`, 
+					background: `linear-gradient(90deg, #fff 25%, #${belt.belt_color} 25%, #${belt.belt_color} 75%, #fff 75%)`,	
+					height: `${100 / beltLength}%`
+				}
+			}
 		/>
 	)
 }
