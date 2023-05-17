b49ca7299b9eb7e9c26e9ab2ee41292d330bc60d
Erik Risberg <c18erg@cs.umu.se>
2023-05-17 12:29:05 +0000

    91_Phoenix: Added test for createExercise
    Merge branch '91_Phoenix' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Jacob Jerkérus <c20jjs@cs.umu.se>
    
    Description
    Added tests for CreateExercise that checks that the necessary components are present.
    
    Branch commits
    * Fixed linting
    
    * Added test for createExercise

---
+++ src/__tests__/pages/CreateExercise/CreateExercise.test.jsx
@@ -0,0 +1,52 @@
+import { render, screen, configure } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import { server } from "../../server"
+const requestSpy = jest.fn()
+server.events.on("request:start", requestSpy)
+import Popup from "../../../components/Common/Popup/Popup"
+import ExerciseCreate from "../../../pages/Exercise/ExerciseCreate"
+configure({ testIdAttribute: "id" })
+
+describe ("ExerciseCreate should render", () => {
+
+	beforeEach(() => {
+        render(//eslint-disable-line
+			<Popup title={"Skapa övning"} isOpen={true}>
+				<ExerciseCreate />
+			</Popup>
+		)
+	})
+
+	test("Title", () => {
+		expect(screen.getByText("Skapa övning")).toBeInTheDocument()
+	})
+
+	test("Name input", () => {
+		expect(screen.getByPlaceholderText("Namn")).toBeInTheDocument()
+	})
+
+	test("Description input", () => {
+		expect(screen.getByPlaceholderText("Beskrivning")).toBeInTheDocument()
+	})
+
+	test("Lägg till fler övningar checkbox", () => {
+		expect(screen.getByText("Lägg till fler övningar")).toBeInTheDocument()
+	})
+
+	test("Rensa text checkbox", () => {
+		expect(screen.getByText("Rensa text")).toBeInTheDocument()
+	})
+
+	test("Input field of minutepicker", () => {
+		expect(screen.getByTestId("minute-picker-minuteSelect")).toBeInTheDocument()
+	})
+
+	test("Back button", () => {
+		expect(screen.getByTestId("EC-BackBtn")).toBeInTheDocument()
+	})
+
+	test("Add button", () => {
+		expect(screen.getByTestId("EC-AddBtn")).toBeInTheDocument()
+	})
+
+})
