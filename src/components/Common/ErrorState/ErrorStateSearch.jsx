16dfe72e9f54008d5a35d767758713f2949fab40
Daniel Vesterlund <id19dvd@cs.umu.se>
2023-05-22 07:41:35 +0000

    75_kraken: Created the ErrorStateSearch component
    Merge branch '75_kraken' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Hamza Budeir <dv21hbr@cs.umu.se>
    
    Description
    https://trello.com/c/7l4BY2au/75-felmeddelanden-fetchning-vid-s%C3%B6kning
    
    The component ErrorStateSearch was created and can now be used to display an error message when the user searches for something that does not exist.
    
    Branch commits
    * Created the ErrorStateSearch component

---
+++ src/components/Common/ErrorState/ErrorStateSearch.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import { EmojiFrown } from "react-bootstrap-icons"
+import "./ErrorState.css"
+
+/**
+ * A placeholder for an error message when searching for something
+ * 
+ * Props:
+ * 	   id (optional) @type {string} - An id used for testing.
+ *     message @type {string}  - An error message to be displayed to the user.
+ *
+ * Example usage:
+ *     <ErrorState 
+ * 			id="some-id"
+ * 			message="Kunde inte hitta tekniker"
+ * 		/>
+ *
+ * @author Team Kraken
+ * @version 1.0
+ * @since 2023-05-22
+ */
+
+export default function ErrorStateSearch({ id, message }) {
+	return (
+		<div id={id} className={"error-state-container"} >
+			<div className={"error-state-icon-container"}>
+				<EmojiFrown size="5rem" color="var(--red-primary)" />
+			</div>
+			<h2>{message}</h2>
+		</div>
+	)
+}
