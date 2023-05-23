3c39d756fb16e2744a8e93f5a06290fd8b5bae3b
Jaskaran Heldestad <dv19jhd@cs.umu.se>
2023-05-23 06:13:52 +0000

    161_chimera: fixed error page and added .jsx to NoPage.js. Also removed the noPage.css
    Merge branch '161_chimera' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Oskar By <dv21oby@cs.umu.se>
    
    Description
    https://trello.com/c/GlQgJaR4/161-%C3%A4ndra-i-indexjs-s%C3%A5-det-blir-till-den-nya-errorstate-komponenten-ist%C3%A4llet-f%C3%B6r-nopage
    
    fixed error page and added .jsx to NoPage.js. Also removed the noPage.css since it is no longer used
    
    Branch commits
    * Remove file
    
    * fixed error page and added .jsx to NoPage.js. Also removed the noPage.css since it is no longer used
    
    * fixed error page and added .jsx to NoPage.js
    
    * Fixed the old NoPage to use the ErrorState component

--- src/pages/Misc/NoPage.js
+++
@@ -1,9 +0,0 @@
-import "./noPage.css"
-/**
- * @returns An empty page with 404 text on it.
- */
-const NoPage = () => {
-	return <img alt="noPage" id="noPageBudo" src="/sad_budo.png"></img>
-}
-
-export default NoPage
