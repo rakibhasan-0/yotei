783aa0eaf70bdae55aa1e28ebfc72b03b5d532f0
Anton Degerfeldt <dv21adt@cs.umu.se>
2023-05-25 13:55:14 +0000

    170_cyclops: Exercise page filtering, sorting and cookies
    Merge branch '170_cyclops' into 'main'
    
    Reviewed by Reviewed-by: Filip Naudot <ens19fpt@cs.umu.se>
    Approved by Approved-by: Filip Naudot <ens19fpt@cs.umu.se>
    
    Description
    [Ticket: ](https://trello.com/c/BuUn8sme) Sorting and filtering is now properly stored in the cookies and the sorting can be applied to searched results.
    
    Branch commits
    * Minor bugfixes and some stability improvements to the cached options.
    
    * Added accessibility roles to the Sorter and created some test cases.
    
    * Added some documentation and example usage.
    
    * Reworked the ExerciseIndex page to use sorting and to properly handle cookies on reload.
    
    * Changed the way that the sorter keeps track of the currently selected option.
    
    * Cookies should now be restored properly.
    
    * Reworked Sorter to take items that have labels and values instead.

--- src/components/Common/Sorting/Sorter.jsx
+++ src/components/Common/Sorting/Sorter.jsx
@@ -1,49 +1,48 @@
-import React,{ useEffect, useState } from "react"
+import React from "react"
 import Component from "../List/Dropdown"
 import style from "./Sorter.module.css"
 import "../BeltPicker/BeltPicker.module.css"
-
-function Sorter ({ id, children, defaultSort ,onSortChange }) {
-	const [selectedOption, setSelectedOption] = useState(defaultSort || "")
-    
-	const handleChildClick = (item) => {
-		setSelectedOption(item)
-	}
-
-	useEffect(() => {
-		onSortChange(selectedOption)
-	}, [selectedOption])
-
+/**
+ * A dropdown containing sorting options. The options are required to be 
+ * objects that have a "label" attribute but can otherwise contain any type 
+ * of data. The "onSortChange" function will be called whenever the currently 
+ * selected option is changed.
+ * 
+ * @usage Example:
+ * const sortOptions = [
+ *    {label: "option1", ...},
+ *    {label: "option2", ...}
+ * ]
+ * const [sort, setSort] = useState()
+ * <Sorter id="foo" onSortChange={setSort} selected={sort} options={sortOptions} />
+ * @author ?, edited by Cyclops (Group 5)
+ * @version 2.0
+ * 
+ * @param {*} param0 
+ * @returns 
+ */
+export default function Sorter ({ id, selected, onSortChange, options }) {    
 	return (
 		<Component
 			text={
 				<span style={{ display: "flex", flexDirection: "row", alignItems: "center", marginTop:"5px"}}>
                 Sortera efter:
 					<span className={style.sortText} style={{ marginLeft:"4px" }}>
-						{selectedOption ? selectedOption : children[0]}
+						{selected && selected.label}
 					</span>
 				</span>}
 			id={id}
-			onClick={() => handleChildClick(selectedOption)}
 		>
-			<div className={style.sort}>
-				{React.Children.map(children, (child, index) => {
-					const optionText = child.props.children
-
-					// Check if it's the first option when selectedOption is null or empty
-					const isCurrentOption =
-                        optionText === selectedOption ||
-                        (!selectedOption && index === 0 && !defaultSort)
-                              
+			<div role="list" className={style.sort}>
+				{ options && options.map((option, index) => {
 					return (
-						<div
-							key={index}
-							className={`${style.sortItem} ${
-								isCurrentOption ? style.currentOption : ""
-							}`}
-							onClick={() => handleChildClick(optionText)}
-						>
-							<span>{optionText}</span>
+						<div role="listitem" key={index}
+							className={`
+								${style.sortItem} 
+								${selected === option ? style.currentOption : ""}
+							`}
+							onClick={() => onSortChange(option)}>
+							<span>{option.label}</span>
 						</div>
 					)
 				})}
@@ -51,5 +50,3 @@ function Sorter ({ id, children, defaultSort ,onSortChange }) {
 		</Component>
 	)
 }
-
-export default Sorter
--- src/pages/Exercise/ExerciseIndex.jsx
+++ src/pages/Exercise/ExerciseIndex.jsx
@@ -17,16 +17,22 @@ import InfiniteScrollComponent from "../../components/Common/List/InfiniteScroll
 
 
 /**
- * Function for the Exercise-page. Creates the searchbar and the list.
+ * Displays a searchbar, a sorter and a list of exercises.
  * 
- * Displays a searchbar and a list of exercises.
- * 
- * @author Hawaii, Verona, Phoenix
+ * @author Hawaii, Verona, Phoenix, Cyclops
  * @since 2023-05-10
- * @version 1.0
+ * @version 2.0
  */
-function ExerciseIndex() {
+export default function ExerciseIndex() {
+	const sortOptions = [
+		{label: "Namn: A-Ö", cmp: (a, b) => {return a.name.localeCompare(b.name)}},
+		{label: "Namn: Ö-A", cmp: (a, b) => {return -a.name.localeCompare(b.name)}},
+		{label: "Längd: Kortast först", cmp: (a, b) => {return a.duration - b.duration}},
+		{label: "Längd: Längst först", cmp: (a, b) => {return b.duration - a.duration}}
+	]
+
 	const [cookies, setCookie] = useCookies(["exercise-filter"])
+	const [exercises, setExercises] = useState([])
 	const [visibleList, setVisibleList] = useState([])
 	const [searchText, setSearchText] = useState("")
 	const [addedTags, setAddedTags] = useState([])
@@ -35,114 +41,40 @@ function ExerciseIndex() {
 	const detailURL = "/exercise/exercise_page/"
 	const [popupVisible, setPopupVisible] = useState(false)
 	const [map, mapActions] = useMap()
-	const [sort, setSort] = useState("nameAsc")
-	const [triggerReload, setTriggerReload] = useState(false)
-
+	const [sort, setSort] = useState(sortOptions[0])
+	
 	useEffect(() => {
 		const filterCookie = cookies["exercise-filter"]
 		if(filterCookie) {
 			setAddedTags(filterCookie.tags)
-			setSort(filterCookie.sort)
+			let cachedSort = sortOptions.find(option => filterCookie.sort === option.label)
+			setSort(cachedSort ? cachedSort : sortOptions[0])
 		}
 	}, [])
 
-	const getAllExercises = async () => {
-		try {
-			const headers = { token }
-			const url = `/api/exercises/all?sort=${sort}`
-      
-			const response = await fetch(url, { headers })
-			const data = await response.json()
-      
-			setVisibleList(data)
-		} catch (error) {
-			console.error(error)
-		}
-	}
-      
-
+	useEffect(setExerciseList, [exercises, sort])
 
 	useEffect(() => {
+		setCookie("exercise-filter", {tags: addedTags, sort: sort.label}, {path: "/"})
 		const args = {
 			text: searchText,
 			selectedTags: addedTags
 		}
 
-		getExercises(args, token, map, mapActions, (result) => {
-			setVisibleList(result.results)
-			setSuggestedTags(result.tagCompletion)
-			setCookie("exercise-filter", {tags: addedTags, sort}, {path: "/"})
+		getExercises(args, token, map, mapActions, result => {
+			if(!result.error) {
+				setSuggestedTags(result.tagCompletion)
+				setExercises(result.results)
+			}
 		})
 	}, [searchText, addedTags])
 
-	useEffect(() => {
-		getAllExercises()
-	}, [sort])
-
-	const handlePageLoad = () => {
-		getAllExercises()
-	}
-      
-	// Call handlePageLoad on page load
-	useEffect(() => {
-		handlePageLoad()
-	}, [])
-      
-	// Call handlePageLoad when triggered by a state change
-	useEffect(() => {
-		if (triggerReload) {
-			handlePageLoad()
-			setTriggerReload(false) // Reset triggerReload state after reloading
+	function setExerciseList() {
+		setCookie("exercise-filter", {tags: addedTags, sort: sort.label}, {path: "/"})
+		if(exercises) {
+			const sortedList = [...exercises].sort(sort.cmp)
+			setVisibleList(sortedList)
 		}
-	}, [triggerReload])
-      
-	// Call handlePageLoad when the popup is closed
-	const handlePopupClose = () => {
-		handlePageLoad()
-		setPopupVisible(false)
-	}
-      
-
-	const handleSortChange = (selectedOption) => {
-		let newSort
-		switch (selectedOption) {
-		case "Namn: A-Ö":
-			newSort = "nameAsc"
-			break
-		case "Namn: Ö-A":
-			newSort = "nameDesc"
-			break
-		case "Längd: Kortast först":
-			newSort = "durationAsc"
-			break
-		case "Längd: Längst först":
-			newSort = "durationDesc"
-			break
-		default:
-			console.error("Invalid sort option")
-			return // Early return if an invalid sort option is selected
-		}
-		if (newSort !== sort) {
-			setSort(newSort)
-			setCookie("exercise-sort", {sort: newSort}, {path: "/"})
-		}
-	}
-    
-	function renderExercises(activities, detailURL) {
-		const exercises = activities?.map((activity, index) => {
-			return <ExerciseCard
-				item={activity.name}
-				text={activity.duration + " min"}
-				key={index}
-				id={activity.id}
-				detailURL={detailURL}
-				index={index}>
-			</ExerciseCard>
-		})
-
-		return (<InfiniteScrollComponent>
-			{exercises}
-		</InfiniteScrollComponent>)
 	}
 
 	return (
@@ -161,17 +93,23 @@ function ExerciseIndex() {
 			/>
 			
 			<FilterContainer id="ei-filter" title="Sortering">
-				<Sorter onSortChange={handleSortChange} id="ei-sort" defaultSort={"Namn: A-Ö"}>
-					<div>Namn: A-Ö</div>
-					<div>Namn: Ö-A</div>
-					<div>Längd: Kortast först</div>
-					<div>Längd: Längst först</div>
-
-					{/* Add more options as needed */}
-				</Sorter>
+				<Sorter onSortChange={setSort} id="ei-sort" selected={sort} options={sortOptions} />
 			</FilterContainer>
 		
-			{renderExercises(visibleList, detailURL)
+			{ visibleList ? 
+				<InfiniteScrollComponent>
+					{ visibleList.map((exercise, index) => {
+						return <ExerciseCard
+							item={exercise.name}
+							text={exercise.duration + " min"}
+							key={exercise.id}
+							id={exercise.id}
+							detailURL={detailURL}
+							index={index}>
+						</ExerciseCard>
+					})}
+				</InfiniteScrollComponent>
+				: <p><br/><br/>Här var det tomt!</p>
 			}
 
 			<br/><br/><br/><br/>
@@ -184,13 +122,11 @@ function ExerciseIndex() {
 				title={"Skapa övning"}
 				id={"create-exercise-popup"}
 				isOpen={popupVisible}
-				setIsOpen={handlePopupClose}
+				setIsOpen={setPopupVisible}
 				noBackground={false}
 			>
-				<ExerciseCreate setShowPopup={setPopupVisible} onClose={() => setTriggerReload(!triggerReload)}/>
+				<ExerciseCreate setShowPopup={setPopupVisible}/>
 			</Popup>
 		</>
 	)
 }
-
-export default ExerciseIndex
