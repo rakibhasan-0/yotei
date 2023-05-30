2e12c67de9ed60ed0255c022d91cd5f284115627
André Byström <c20abm@cs.umu.se>
2023-05-30 13:31:16 +0000

    152_cyclops: Restify tag controllers
    Merge branch '152_cyclops' into 'main'
    
    Reviewed by Reviewed-by: Filip Naudot <ens19fpt@cs.umu.se>
    Approved by Approved-by: Filip Naudot <ens19fpt@cs.umu.se>
    
    Description
    https://trello.com/c/oomEtEHA/152-tag-api-restify-endpoints
    
    Tagit bort oanvända controllers/endpoints. Restifyat TagController och ExerciseTagController
    
    Branch commits
    * Updated deprecated path
    
    * Removed unused tag API endpoints/controllers
    
    * Updated ExerciseTagController to be more restful. Removed unused endpoints
    
    * Restified the TagController
    
    * Removed unused tag API endpoints/controllers
    
    * Updated ExerciseTagController to be more restful. Removed unused endpoints
    
    * Restified the TagController

--- api/src/main/java/se/umu/cs/pvt/tag/ExerciseTagController.java
+++ api/src/main/java/se/umu/cs/pvt/tag/ExerciseTagController.java
@@ -40,8 +40,9 @@ public class ExerciseTagController {
      * @return HTTP response
      * A response entity containing the added Exercise/Tag pair with status OK.
      * BAD_REQUEST if that tag ID doesn't exist.
+     * @deprecated /add/exercise
      */
-    @PostMapping("/add/exercise")
+    @PostMapping(value={"/exercises","/add/exercise"})
     public ResponseEntity<ExerciseTag> postExerciseTagPair(@RequestBody ExerciseTag toAddExerciseTag,
                                                            @RequestParam(name = "tag") Long tagId) {
 
@@ -53,23 +54,6 @@ public class ExerciseTagController {
         return new ResponseEntity<>(toAddExerciseTag, HttpStatus.BAD_REQUEST);
     }
 
-
-    /**
-     * Finds all Exercise Ids that has a specific tag on it.
-     *
-     * @param tagId The tag Id of the specified tag.
-     * @return HTTP reponse
-     * A response entity with the list of Exercise ids with status OK.
-     * BAD_REQUEST if no Exercises with that tag exist.
-     */
-    @GetMapping("/get/exercise/by-tag")
-    public ResponseEntity<List<ExerciseTagShort>> getExerciseByTag(@RequestParam(name = "tag") Long tagId) {
-        if (tagRepository.findById(tagId).orElse(null) != null) {
-            return new ResponseEntity<>(exerciseTagRepository.findAllProjectedByTagId(tagId), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
-
     /**
      * Finds all Tags for a given Exercise.
      *
@@ -77,8 +61,9 @@ public class ExerciseTagController {
      * @return HTTP response
      * A response entity with the list of Tag elements with status OK.
      * BAD_REQUEST if Exercise has no tags.
+     * @deprecated /get/tag/by-exercise
      */
-    @GetMapping("/get/tag/by-exercise")
+    @GetMapping(value={"", "/get/tag/by-exercise"}, params = "exerciseId")
     public ResponseEntity<List<TagResponse>> getTagByExercises(@RequestParam(name = "exerciseId") Long exerciseId) {
         if (exerciseTagRepository.findByExerciseId(exerciseId) != null) {
             return new ResponseEntity<>(exerciseTagRepository.findAllProjectedByExerciseId(exerciseId).stream()
@@ -96,9 +81,10 @@ public class ExerciseTagController {
      * @return HTTP response
      * OK is deletion successful.
      * BAD_REQUEST if deletion pair not found.
+     * @deprecated /remove/exercise
      */
     @Transactional
-    @DeleteMapping("/remove/exercise")
+    @DeleteMapping(value={"/exercises", "/remove/exercise"})
     public ResponseEntity<ExerciseTag> deleteExerciseTagPair(@RequestBody ExerciseTag toAddExerciseTag,
                                                              @RequestParam(name = "tag") Long tagId) {
         if (tagRepository.findById(tagId).orElse(null) != null) {
@@ -113,108 +99,15 @@ public class ExerciseTagController {
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-
-    /**
-     * Gets all ExerciseIds mapped by their Tags.
-     *
-     * @return HTTP reponse
-     * OK if non-empty body is returned.
-     * NO_CONTENT if there doesn't exist any tags for exercises.
-     * BAD_REQUEST for faulty requests.
-     */
-    @GetMapping("/fetch/exercises/by-tag")
-    public ResponseEntity<Map<Long, List<Long>>> getExercisesByTags() {
-        HashMap<Long, List<Long>> exTags = new HashMap<>();
-
-        // Fetch TechniqueTags for their Id.
-        ArrayList<Tag> tags = (ArrayList<Tag>) tagRepository.findAll();
-        for (Tag tag : tags) {
-            Long tagId = tag.getId();
-            ArrayList<Long> temp = new ArrayList<>();
-            if (exerciseTagRepository.findAllProjectedByTagId(tagId) != null) {
-                ArrayList<ExerciseTagShort> exerciseTags = (ArrayList<ExerciseTagShort>) exerciseTagRepository.findAllProjectedByTagId(tagId);
-                for (ExerciseTagShort exTag : exerciseTags) {
-                    temp.add(exTag.getExerciseId());
-                }
-            }
-            // Ignore exercises with no tags.
-            if (!temp.isEmpty()) {
-                exTags.put(tagId, temp);
-            }
-        }
-
-        // Check if there doesn't exist any tags for exercises.
-        if (exTags.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-
-        // Return the final response.
-        return new ResponseEntity<>(exTags, HttpStatus.OK);
-    }
-
-
-    /**
-     * Adds ExerciseTags to a given exercise.
-     *
-     * @param exerciseId   given exercise id.
-     * @param exerciseTags ExerciseTags to be added.
-     * @return HTTP status
-     * OK if the setting of tags was successful.
-     * BAD_REQUEST if no tags was found.
-     */
-    @PostMapping("/set-tags/exercises")
-    public ResponseEntity<ExerciseTag> setExerciseTagsPost(@RequestParam(name = "exercise_id") long exerciseId,
-                                                           @RequestBody ArrayList<ExerciseTag> exerciseTags) {
-
-        return setExerciseTags(exerciseId, exerciseTags);
-    }
-
-
-    /**
-     * Updates ExerciseTags for a given exercise.
-     *
-     * @param exerciseId   given exercise id.
-     * @param exerciseTags ExerciseTags to be added.
-     * @return HTTP status
-     * OK if the setting of tags was successful.
-     * BAD_REQUEST if no tags was found.
-     */
-    @PutMapping("/set-tags/exercises")
-    public ResponseEntity<ExerciseTag> setExerciseTags(@RequestParam(name = "exercise_id") long exerciseId,
-                                                       @RequestBody ArrayList<ExerciseTag> exerciseTags) {
-        if (exerciseTags == null || exerciseTags.size() == 0) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        // Delete old exercise tags.
-        exerciseTagRepository.deleteAll(exerciseTagRepository.findByExerciseId(exerciseId));
-
-        for (ExerciseTag exerciseTag : exerciseTags) {
-
-            // Finds a matching tag or creates a tag that matches the given tag object.
-            Tag tempTag = tagRepository.getTagByName(exerciseTag.getTagObject().getName());
-            if (tempTag == null) {
-                tempTag = tagRepository.save(exerciseTag.getTagObject());
-            }
-
-            // Sets the tag to the exercise tag object and saves it.
-            exerciseTag.setTag(tempTag);
-            exerciseTag.setExerciseId(exerciseId);
-            exerciseTagRepository.save(exerciseTag);
-        }
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-
     /**
      * Removes the ExerciseTags for a given exercise.
      *
      * @param exerciseId given exercise id.
      * @return HTTP response
      * OK if successful.
+     * @deprecated /remove-tags/exercises/{exercise_id}
      */
-    @DeleteMapping("/remove-tags/exercises/{exercise_id}")
+    @DeleteMapping(value={"/exercises/{exercise_id}", "/remove-tags/exercises/{exercise_id}"})
     public ResponseEntity<ExerciseTag> deleteExerciseTagPair(@PathVariable("exercise_id") long exerciseId) {
         exerciseTagRepository.deleteAll(exerciseTagRepository.findByExerciseId(exerciseId));
         return new ResponseEntity<>(HttpStatus.OK);
--- api/src/main/java/se/umu/cs/pvt/tag/TagController.java
+++ api/src/main/java/se/umu/cs/pvt/tag/TagController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 
@@ -17,15 +18,13 @@ import java.util.List;
 @CrossOrigin
 @RequestMapping(path = "/api/tags")
 public class TagController {
-
     @Autowired
     private final TagRepository tagRepository;
-
 
     /**
      * Contructor for the TagController object.
      *
-     * @param   tagRepository   Autowired.
+     * @param tagRepository Autowired.
      */
     public TagController(TagRepository tagRepository) {
         this.tagRepository = tagRepository;
@@ -35,9 +34,10 @@ public class TagController {
     /**
      * Returns all Tags from the database.
      *
-     * @return      All tags.
+     * @return All tags.
+     * @deprecated the /all endpoint.
      */
-    @GetMapping("/all")
+    @GetMapping(value = {"", "/all"})
     public ResponseEntity<List<Tag>> getTags() {
         List<Tag> tags = tagRepository.findAll();
         if (tags.isEmpty()) {
@@ -46,11 +46,28 @@ public class TagController {
         return new ResponseEntity<>(tags, HttpStatus.OK);
     }
 
+    /**
+     * Deletes a tag by id.
+     *
+     * @param id the id of the tag.
+     * @return response, 200 OK on success.
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteTag(@PathVariable Long id) {
+        // See if a tag with the given ID exists.
+        if (tagRepository.findById(id).isEmpty()) {
+            return new ResponseEntity<>("Tag with ID: " + id + " does not exist", HttpStatus.BAD_REQUEST);
+        }
+        tagRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
     /**
      * Removes a Tag with a given ID.
      *
-     * @param   id      The IdD of the Tag to remove.
+     * @param id The IdD of the Tag to remove.
+     * @see se.umu.cs.pvt.tag.TagController#deleteTag(Long)
+     * @deprecated this entire endpoint.
      */
     @DeleteMapping("/remove")
     public ResponseEntity<Object> removeTag(@RequestParam(name = "id") Long id) {
@@ -59,26 +76,27 @@ public class TagController {
         }
         // See if a tag with the given ID exists.
         if (tagRepository.findById(id).isEmpty()) {
-            return new ResponseEntity<>("Tag with ID: " + id + " does not exist",HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("Tag with ID: " + id + " does not exist", HttpStatus.BAD_REQUEST);
         }
         tagRepository.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
-    
+
     /**
      * Adds a Tag.
      *
-     * @param   toAdd       The Tag to add.
-     * @return              The newly added Tag.
+     * @param toAdd The Tag to add.
+     * @return The newly added Tag.
+     * @deprecated the /add endpoint.
      */
-    @PostMapping("/add")
+    @PostMapping(value = {"", "/add"})
     public ResponseEntity<Tag> postTag(@RequestBody Tag toAdd) {
         if (toAdd.getName().isEmpty()) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-		
-		toAdd.nameToLowerCase();
+
+        toAdd.nameToLowerCase();
         tagRepository.save(toAdd);
         return new ResponseEntity<>(tagRepository.getTagByName(toAdd.getName()), HttpStatus.CREATED);
-    }  
+    }
 }
