2eca66b41dae048784cc5e219e245ede31b9b26c
Yun Nyan Liu <c20ylu@cs.umu.se>
2023-06-01 11:24:40 +0000

    50_dragon_description : fixed description
    Merge branch '50_dragon_description' into 'main'
    
    Reviewed by 
    Approved by Approved-by: Per Bergman Viklund <c19pbd@cs.umu.se>
    
    Description
    fixed mediaplayer description
    
    Branch commits
    * hotfix
    
    * Fixed css on admin page
    
    Merge branch 'admin_page_css' into 'main'
    
    Reviewed by
    Approved by Approved-by: Oskar By <dv21oby@cs.umu.se>
    
    Description
    No ticket :)
    
    Branch commits
    * Fixed css och admin page

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -5,7 +5,6 @@ import VideoPlayerReactPlayer from "../VideoPlayer/VideoPlayerReactPlayer"
 import Image from "../Image/Image"
 import "../Gallery/Gallery.css"
 import { AccountContext } from "../../context"
-import TextArea from "../Common/TextArea/TextArea"
 
 /**
  * A media component for displaying video or images.
@@ -56,13 +55,15 @@ export default function Gallery({ id }) {
 	/**
 	 * Used for adding description into gallery.
 	 * @param {*} mediaObject image or video that has a description.
-	 * @returns a textarea with description connected to mediaObject.
+	 * @returns a paragraph with description connected to mediaObject.
 	 */
 	function MediaDescription(mediaObject){
-		return(	
-			<TextArea id={`${mediaObject.id}-media-description`} 
-				text={mediaObject.description} 
-				readOnly={true}/>
+		return(
+			<p id={`${mediaObject.id}-media-description`} 
+				className="media-description" 
+				style={{border : mediaObject.description == "" ? "none" : "solid black 1px"}}>
+				{mediaObject.description}
+			</p>
 		)
 	
 	}
@@ -72,7 +73,7 @@ export default function Gallery({ id }) {
 		<div className="container gallery-container" id={id}>
 			<div className="row mt-2 mb-2">
 				<div className="col-sm-12 text-center ">
-					<Carousel showThumbs={false} showStatus={false} showArrows={true}>                         
+					<Carousel showThumbs={false} showStatus={false} showArrows={true} >                         
 						{pictures.map((image, index) => (
 							<div className="gallery-image-and-description" key={index}>
 								<Image path={image.url} key={index} />
