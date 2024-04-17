b973296ded04be3053d876073179e88acd158139
Lee Lannerblad <ens19lld@cs.umu.se>
2024-04-17 11:07:39 +0200

    fixed scripts for backing up and restoring db

--- infra/database/restore_db.sh
+++ infra/database/restore_db.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-ADDRESS=postgres.cs.umu.se
-USERNAME=c5dv214_vt22_dev
-DATA=c5dv214_vt22_dev
+HOST=localhost
+USERNAME=psql
+DATABASE=yotei
 INFILE="$1"                     # input file = the script argument
 
 SCRIPT_PATH=$(dirname "$0")         # Find path of this script
 OLD_PGPASS=$PGPASSFILE              # Save old path
 export PGPASSFILE=$SCRIPT_PATH/pgpass.conf # Remember to edit this file!
 
-docker -exec -t 5dv214vt22_psql_1 \
+docker exec -t infra-psql-1 \
 pg_restore \
-    --clean \
-    -d $DATA \
-    --verbose \
-    --format=c \
-    -h $ADDRESS \
-    -U $USERNAME \
-    "$INFILE"
+   --clean \
+   -d "$DATABASE" \
+   --verbose \
+   --format=c \
+   -h "$HOST" \
+   -U "$USERNAME" \
+   "$INFILE"
 
 PGPASSFILE=$OLD_PGPASS          # Restore old path
