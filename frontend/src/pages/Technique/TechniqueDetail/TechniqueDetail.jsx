bfa46b2fca7f6fb969f002f7edcd7f1674910bfc
Joakim Gladh <c21jgh@cs.umu.se>
2024-04-18 10:28:37 +0200

    Fixed sorting for belts in technique description

--- frontend/src/pages/Technique/TechniqueDetail/TechniqueDetail.jsx
+++ frontend/src/pages/Technique/TechniqueDetail/TechniqueDetail.jsx
@@ -32,7 +32,7 @@ import ActivityDelete from "../../../components/Activity/ActivityDelete/Activity
  * 
  * @author Team Medusa (Grupp 6) & Cyclops (Group 5) & Tomato (Group 6)
  * @version 3.0
- * @since 2024-04-17
+ * @since 2024-04-18
  */
 function TechniqueDetail({ id }) {
 
@@ -47,6 +47,18 @@ function TechniqueDetail({ id }) {
 	const accountRole = useContext(AccountContext)
 	const [showConfirmPopup, setShowConfirmPopup] = useState(false)
 	const [showDeletePopup, setShowDeletePopup] = useState(false)
+
+	// Belt sorting order, Adult easiest -> Adult hardest -> Child easiest -> Child hardest
+	const order = [3, 5, 7, 9, 11, 13, 14, 15, 16, 4, 6, 8, 10, 12];
+	const beltSort = (a, b) => {
+		const indexA = order.indexOf(a.id);
+		const indexB = order.indexOf(b.id);
+
+		if (indexA === -1) return 1
+		if (indexB === -1) return -1
+
+		return indexA - indexB;
+	};	
 
 	const handleGet = useCallback(() => {
 		setLoading(true)
@@ -132,7 +144,7 @@ function TechniqueDetail({ id }) {
 				}
 				<div className="technique-detail-belts-container">
 					{technique.belts ? (
-						technique.belts.map(belt => <BeltIcon key={belt.name} belt={belt} />)
+						technique.belts.sort(beltSort).map(belt => <BeltIcon key={belt.name} belt={belt} />)
 					) : (
 						<p>Inga bälten kunde hittas för denna teknik.</p>
 					)}
