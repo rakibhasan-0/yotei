648fa0c95effae548ab6e0fae69f95afb4b203cf
Lee Lannerblad <ens19lld@cs.umu.se>
2024-04-19 10:28:16 +0200

    fixed character counter in TextArea

--- frontend/src/components/Common/TextArea/TextArea.jsx
+++ frontend/src/components/Common/TextArea/TextArea.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import styles from "./TextArea.module.css"
 
 /**
@@ -37,24 +38,50 @@ import styles from "./TextArea.module.css"
 	/>
  * 
  * 
- * @author Team Chimera & Medusa
+ * @author Team Chimera & Medusa & Team Tomato
  * @since 2023-04-24
- * @updated 2023-05-30 Chimera updated documentation
+ * @updated 2024-04-19 Chimera updated documentation
  * @version 4.1
  */
-export default function TextArea({ placeholder, text, onChange, required, onBlur, onInput, readOnly, id, defVal, type, errorMessage, errorDisabled, maxLength}) {
-
+export default function TextArea({
+	placeholder,
+	text,
+	onChange,
+	required,
+	onBlur,
+	onInput,
+	readOnly,
+	id,
+	defVal,
+	type,
+	errorMessage,
+	errorDisabled,
+	maxLength,
+}) {
 	const defaultLimit = 800
 	const isErr = !(errorMessage == undefined || errorMessage == null || errorMessage == "")
-	return(
+	const [characterCount, setCharacterCount] = useState(text?.length ?? 0)
+
+	const countCharacters = (inputText) => {
+		return inputText.length
+	}
+
+	return (
 		<label className={styles.label}>
-			<p className={styles.limitText}>{text?.length || 0}/{maxLength || defaultLimit}</p>
+			<p className={styles.limitText}>
+				{characterCount}/{maxLength || defaultLimit}
+			</p>
 			<textarea
 				className={isErr ? `${styles.textarea} ${styles.textareaErr}` : `${styles.textarea}`}
 				placeholder={placeholder}
 				value={text}
 				defaultValue={defVal}
-				onChange={onChange}
+				onChange={(e) => {
+					if (onChange) {
+						onChange(e)
+					}
+					setCharacterCount(countCharacters(e.target.value))
+				}}
 				onBlur={onBlur}
 				onInput={onInput}
 				required={required}
@@ -65,5 +92,5 @@ export default function TextArea({ placeholder, text, onChange, required, onBlur
 			/>
 			{!errorDisabled && <p className={styles.err}>{errorMessage}</p>}
 		</label>
-	) 
+	)
 }
