006c90fc5a15c415c12fa21f844f215c98bf8a3f
Tobias Ljung <dv21tlg@cs.umu.se>
2024-04-19 15:14:15 +0200

    Fixed indentation issues

--- frontend/src/pages/Exercise/ExerciseCreate.jsx
+++ frontend/src/pages/Exercise/ExerciseCreate.jsx
@@ -31,6 +31,7 @@ import { unstable_useBlocker as useBlocker } from "react-router"
  *     Verona (2022-05-04)
  *     Phoenix (Group 1) (2023-05-04)
  *     Medusa (Group 6) (2023-06-01)
+ *     Coconut (Grupp 7) (2024-04-19)
  *
  * @since 2023-05-22
  * @version 2.0
@@ -107,6 +108,9 @@ export default function ExerciseCreate() {
 		setIsBlocking(name != "" || desc != "")
 	}, [name, desc])
 
+	/**
+	 * Saves an exerciseCreateInput object in local storage
+	 */
 	useEffect(() => {
 		localStorage.setItem("exerciseCreateLocalStorageKey", JSON.stringify(exerciseCreateInput))
 		return () => {
@@ -114,6 +118,9 @@ export default function ExerciseCreate() {
 		}
 	}, [exerciseCreateInput, isSubmitted])
 
+	/**
+	 * Loads an exerciseCreateInput object from local storage or initializes one if non was found.
+	 */
 	useEffect(() => {
 		const item = localStorage.getItem("exerciseCreateLocalStorageKey")
 		if (item) {
@@ -121,295 +128,310 @@ export default function ExerciseCreate() {
 		} else {
 			// Initialize state only if localStorage data is not available
 			setExerciseCreateInput({
-			name: "",
-			desc: "",
-			time: 0,
-			addBoxChecked: false,
-			eraseBoxChecked: false,
-			addedTags: []
+				name: "",
+				desc: "",
+				time: 0,
+				addBoxChecked: false,
+				eraseBoxChecked: false,
+				addedTags: []
 			})
 		}
 	}, [])
 
-	const handleInputChange = (fieldName, value) => {
+	/**
+	 * Updates the exerciseCreateInput object when new input is added
+	 * @param fieldName The exerciseCreateInput objects attribute to be changed
+	 * @param value The new value
+	 */
+	const storeInputChange = (fieldName, value) => {
 		setExerciseCreateInput(prevState => ({
 			...prevState,
 			[fieldName]: value
 		}))
 	}
-		/**
-		 * Method for API call when creating an exercise.
-		 * Also check with the database s if the exercise can be added.
-		 *
-		 * @returns the id of the exercise that has been created
-		 */
-		async function addExercise() {
-			let data = null
 
-			const item = localStorage.getItem("exerciseCreateLocalStorageKey")
-			if(item) console.log("ADD EXERCISE:", item)
+	/**
+	 * Method for API call when creating an exercise.
+	 * Also check with the database s if the exercise can be added.
+	 *
+	 * @returns the id of the exercise that has been created
+	 */
+	async function addExercise() {
+		let data = null
 
-			const requestOptions = {
-				method: "POST",
-				headers: { "Content-type": "application/json", "token": context.token },
-				body: JSON.stringify({ name: name, description: desc, duration: time })
-			}
-			try {
-				const response = await fetch("/api/exercises/add", requestOptions)
-				if (response.ok) {
-					data = await response.json()
-					setErrorMessage("")
-					setIsSubmitted(true)
-					return data.id
-				} else {
-					if (response.status === 409) {
-						setErrorMessage("En övning med detta namn existerar redan")
-					}
-					if (response.status === 400) {
-						setErrorMessage(" ")
-					}
-				}
-			} catch (error) {
-				toast.error("Övningen kunde ej läggas till")
-			}
-			return null
+		const item = localStorage.getItem("exerciseCreateLocalStorageKey")
+		if(item) console.log("ADD EXERCISE:", item)
+
+		const requestOptions = {
+			method: "POST",
+			headers: { "Content-type": "application/json", "token": context.token },
+			body: JSON.stringify({ name: name, description: desc, duration: time })
 		}
-
-
-		/**
-		 * Validates the list of tags and loops through it and tries to link it to the exercise.
-		 *
-		 * @param exId - exercise id
-		 * @returns a boolean if the tags were successfully linked to the exercise
-		 */
-		async function addTag(exId) {
-			let hasLinked = true
-			if (exId === null) {
-				return false
-			}
-			if (addedTags.length === 0 || addedTags === undefined) {
-				return true
-			}
-			for (let i = 0; i < addedTags.length; i++) {
-				let successResponse = await linkExerciseTag(exId, addedTags.at(i).id)
-				if (!successResponse) {
-					hasLinked = false
-				}
-			}
-			return hasLinked
-		}
-
-
-		/**
-		 * Method for API call when linking a tag to an exercise.
-		 *
-		 * @param exId - id of exercise
-		 * @param tagId - id of tag
-		 */
-		async function linkExerciseTag(exId, tagId) {
-			const requestOptions = {
-				method: "POST",
-				headers: { "Content-type": "application/json", "token": context.token },
-				body: JSON.stringify({ "exerciseId": exId })
-			}
-
-			try {
-				const response = await fetch("/api/tags/exercises?tag=" + tagId, requestOptions)
-				if (response.ok) {
-					return true
-				} else {
-					return false
-				}
-			} catch (error) {
-				toast.error("Taggar kunde ej kopplas till övningen")
-				return false
-			}
-		}
-
-		/**
-		 * Calls the API calls in the correct order by
-		 * first creating the exercise, then linking the exercise with the chosen tags.
-		 */
-
-		function addExerciseAndTags() {
-			if (checkInput() === true) {
-				setIsBlocking(false)
-				addExercise()
-					.then((exId) => handleExId(exId))
-					.then((exId) => handleSendData(exId))
-					.then((exId) => addTag(exId))
-					.then((linkedTags) => setTags(linkedTags))
-			}
-		}
-
-		function handleExId(exId) {
-			setTempId(exId)
-			return exId
-		}
-
-		function handleSendData(exId) {
-			setSendData(true)
-			return exId
-		}
-
-		/**
-		 * Checks if insert worked, if so redirect back to exercise.
-		 * @param linkedTags - result from linking tags
-		 */
-		function exitProdc(linkedTags) {
-			if (linkedTags) {
-				toast.success("Övningen " + name + " lades till")
-				if (addBoxChecked === false) {
-					navigate(-1)
-				} else {
-					if (eraseBoxChecked === true) {
-						setName("")
-						setTime(0)
-						setDesc("")
-						setAddedTags([])
-					}
-				}
-			}
-		}
-
-		/**
-		 * A function that will validate the users input on name.
-		 * @returns Boolean if the name is ok or not
-		 */
-		function checkInput() {
-			if (exerciseCreateInput.name !== "" && exerciseCreateInput.name !== undefined) {
+		try {
+			const response = await fetch("/api/exercises/add", requestOptions)
+			if (response.ok) {
+				data = await response.json()
 				setErrorMessage("")
-				return true
+				setIsSubmitted(true)
+				return data.id
 			} else {
-				setErrorMessage("Övningen måste ha ett namn")
+				if (response.status === 409) {
+					setErrorMessage("En övning med detta namn existerar redan")
+				}
+				if (response.status === 400) {
+					setErrorMessage(" ")
+				}
 			}
+		} catch (error) {
+			toast.error("Övningen kunde ej läggas till")
+		}
+		return null
+	}
+
+
+	/**
+	 * Validates the list of tags and loops through it and tries to link it to the exercise.
+	 *
+	 * @param exId - exercise id
+	 * @returns a boolean if the tags were successfully linked to the exercise
+	 */
+	async function addTag(exId) {
+		let hasLinked = true
+		if (exId === null) {
 			return false
 		}
+		if (addedTags.length === 0 || addedTags === undefined) {
+			return true
+		}
+		for (let i = 0; i < addedTags.length; i++) {
+			let successResponse = await linkExerciseTag(exId, addedTags.at(i).id)
+			if (!successResponse) {
+				hasLinked = false
+			}
+		}
+		return hasLinked
+	}
 
-		function timeCallback(id, time) {
-			setTime(time)
-			handleInputChange("time", time)
+
+	/**
+	 * Method for API call when linking a tag to an exercise.
+	 *
+	 * @param exId - id of exercise
+	 * @param tagId - id of tag
+	 */
+	async function linkExerciseTag(exId, tagId) {
+		const requestOptions = {
+			method: "POST",
+			headers: { "Content-type": "application/json", "token": context.token },
+			body: JSON.stringify({ "exerciseId": exId })
 		}
 
-		/**
-		 * Handles logic when add more exercises checkbox is clicked.
-		 * @param checked - a boolean to set if the add checkbox is clicked or not
-		 */
-		function addCheckboxClicked(checked) {
-			handleInputChange("addBoxChecked", checked)
-			handleInputChange("eraseBoxChecked", false)
+		try {
+			const response = await fetch("/api/tags/exercises?tag=" + tagId, requestOptions)
+			if (response.ok) {
+				return true
+			} else {
+				return false
+			}
+		} catch (error) {
+			toast.error("Taggar kunde ej kopplas till övningen")
+			return false
 		}
+	}
 
-		function eraseBoxClicked(checked) {
-			handleInputChange("eraseBoxChecked", checked)
+	/**
+	 * Calls the API calls in the correct order by
+	 * first creating the exercise, then linking the exercise with the chosen tags.
+	 */
+
+	function addExerciseAndTags() {
+		if (checkInput() === true) {
+			setIsBlocking(false)
+			addExercise()
+				.then((exId) => handleExId(exId))
+				.then((exId) => handleSendData(exId))
+				.then((exId) => addTag(exId))
+				.then((linkedTags) => setTags(linkedTags))
 		}
+	}
 
-		return (
-			<>
-				<h1>Skapa övning</h1>
+	function handleExId(exId) {
+		setTempId(exId)
+		return exId
+	}
 
-				<div style={{ height: "1rem" }} />
+	function handleSendData(exId) {
+		setSendData(true)
+		return exId
+	}
 
-				<InputTextField
-					placeholder="Namn"
-					text={exerciseCreateInput.name}
-					onChange={(e) => {
-						setName(e.target.value)
-						handleInputChange("name", e.target.value)
+	/**
+	 * Checks if insert worked, if so redirect back to exercise.
+	 * @param linkedTags - result from linking tags
+	 */
+	function exitProdc(linkedTags) {
+		if (linkedTags) {
+			toast.success("Övningen " + name + " lades till")
+			if (addBoxChecked === false) {
+				navigate(-1)
+			} else {
+				if (eraseBoxChecked === true) {
+					setName("")
+					setTime(0)
+					setDesc("")
+					setAddedTags([])
+				}
+			}
+		}
+	}
+
+	/**
+	 * A function that will validate the users input on name.
+	 * @returns Boolean if the name is ok or not
+	 */
+	function checkInput() {
+		if (exerciseCreateInput.name !== "" && exerciseCreateInput.name !== undefined) {
+			setErrorMessage("")
+			return true
+		} else {
+			setErrorMessage("Övningen måste ha ett namn")
+		}
+		return false
+	}
+
+	/**
+	 * Handles logic when exercise duration is changed.
+	 * @param id - component id
+	 * @param time - a number that denotes the exercise duration (time)
+	 */
+	function timeCallback(id, time) {
+		setTime(time)
+		storeInputChange("time", time)
+	}
+
+	/**
+	 * Handles logic when add more exercises checkbox is clicked.
+	 * @param checked - a boolean to set if the add checkbox is clicked or not
+	 */
+	function addCheckboxClicked(checked) {
+		storeInputChange("addBoxChecked", checked)
+		storeInputChange("eraseBoxChecked", false)
+	}
+
+	/**
+	 * Handles logic when erase exercises checkbox is clicked.
+	 * @param checked - a boolean to set if the erase checkbox is clicked or not
+	 */
+	function eraseBoxClicked(checked) {
+		storeInputChange("eraseBoxChecked", checked)
+	}
+
+	return (
+		<>
+			<h1>Skapa övning</h1>
+
+			<div style={{ height: "1rem" }} />
+
+			<InputTextField
+				placeholder="Namn"
+				text={exerciseCreateInput.name}
+				onChange={(e) => {
+					setName(e.target.value)
+					storeInputChange("name", e.target.value)
+				}}
+				required={true}
+				type="text"
+				id="ExerciseNameInput"
+				errorMessage={errorMessage}
+			/>
+			<TextArea
+				className={styles.standArea}
+				placeholder="Beskrivning"
+				text={exerciseCreateInput.desc}
+				onChange={(e) => {
+					setDesc(e.target.value)
+					storeInputChange("desc", e.target.value)
+				}}
+				required={true}
+				type="text"
+				errorDisabled={true}
+			/>
+
+			<Divider option={"h1_left"} title={"Tid"} />
+
+			<div className={styles.timeSelector} >
+				<MinutePicker
+					initialValue={exerciseCreateInput.time}
+					callback={timeCallback}
+				/>
+			</div>
+
+			<Divider option={"h1_left"} title={"Taggar"} />
+
+			<TagInput
+				addedTags={exerciseCreateInput.addedTags}
+				setAddedTags={setAddedTags}
+				onTagChange={(newTags) => storeInputChange("addedTags", newTags)}
+			/>
+
+			<Divider option={"h1_left"} title={"Media"} />
+
+			<EditGallery
+				id={tempId}
+				exerciseId={tempId}
+				sendData={sendData}
+				undoChanges={undoMediaChanges}
+				done={done}
+			/>
+
+			<CheckBox
+				label={"Fortsätt skapa övningar"}
+				disabled={false}
+				checked={exerciseCreateInput.addBoxChecked}
+				onClick={addCheckboxClicked}
+			/>
+
+			<div style={{ height: "1rem" }} />
+
+			<CheckBox
+				label={"Rensa fält"}
+				disabled={!exerciseCreateInput.addBoxChecked}
+				checked={exerciseCreateInput.eraseBoxChecked}
+				onClick={eraseBoxClicked}
+			/>
+
+			<div style={{ height: "1rem" }} />
+
+			<div className={styles.buttonContainer}>
+				<Button
+					width="100%"
+					outlined={true}
+					onClick={() => {
+						setUndoMediaChanges(true)
+						exitProdc()
 					}}
-					required={true}
-					type="text"
-					id="ExerciseNameInput"
-					errorMessage={errorMessage}
-				/>
-				<TextArea
-					className={styles.standArea}
-					placeholder="Beskrivning"
-					text={exerciseCreateInput.desc}
-					onChange={(e) => {
-						setDesc(e.target.value)
-						handleInputChange("desc", e.target.value)
+				>
+					<p>Tillbaka</p>
+				</Button>
+				<Button
+					width="100%"
+					onClick={() => {
+						addExerciseAndTags()
+						exitProdc(tags)
 					}}
-					required={true}
-					type="text"
-					errorDisabled={true}
-				/>
+				>
+					<p>Lägg till</p>
+				</Button>
+			</div>
 
-				<Divider option={"h1_left"} title={"Tid"} />
-
-				<div className={styles.timeSelector} >
-					<MinutePicker
-						initialValue={exerciseCreateInput.time}
-						callback={timeCallback}
-					/>
-				</div>
-
-				<Divider option={"h1_left"} title={"Taggar"} />
-
-				<TagInput
-					addedTags={exerciseCreateInput.addedTags}
-					setAddedTags={setAddedTags}
-					onTagChange={(newTags) => handleInputChange("addedTags", newTags)}
-				/>
-
-				<Divider option={"h1_left"} title={"Media"} />
-
-				<EditGallery
-					id={tempId}
-					exerciseId={tempId}
-					sendData={sendData}
-					undoChanges={undoMediaChanges}
-					done={done}
-				/>
-
-				<CheckBox
-					label={"Fortsätt skapa övningar"}
-					disabled={false}
-					checked={exerciseCreateInput.addBoxChecked}
-					onClick={addCheckboxClicked}
-				/>
-
-				<div style={{ height: "1rem" }} />
-
-				<CheckBox
-					label={"Rensa fält"}
-					disabled={!exerciseCreateInput.addBoxChecked}
-					checked={exerciseCreateInput.eraseBoxChecked}
-					onClick={eraseBoxClicked}
-				/>
-
-				<div style={{ height: "1rem" }} />
-
-				<div className={styles.buttonContainer}>
-					<Button
-						width="100%"
-						outlined={true}
-						onClick={() => {
-							setUndoMediaChanges(true)
-							exitProdc()
-						}}
-					>
-						<p>Tillbaka</p>
-					</Button>
-					<Button
-						width="100%"
-						onClick={() => {
-							addExerciseAndTags()
-							exitProdc(tags)
-						}}
-					>
-						<p>Lägg till</p>
-					</Button>
-				</div>
-
-				<ConfirmPopup
-					popupText="Du har osparade ändringar. Är du säker att du lämna?"
-					showPopup={showMiniPopup}
-					setShowPopup={setShowMiniPopup}
-					confirmText="Lämna"
-					backText="Avbryt"
-					onClick={blocker.proceed}
-				/>
-			</>
-		)
+			<ConfirmPopup
+				popupText="Du har osparade ändringar. Är du säker att du lämna?"
+				showPopup={showMiniPopup}
+				setShowPopup={setShowMiniPopup}
+				confirmText="Lämna"
+				backText="Avbryt"
+				onClick={blocker.proceed}
+			/>
+		</>
+	)
 }
