8fd2cb3c09f6f4bbebb0c42dd11c2d23154bb241
Daniel Hylander <ens21dhr@cs.umu.se>
2024-04-19 13:35:33 +0200

    Fixed userBlocker error: Mocked userBlocker so render works, and test can be made.

--- frontend/src/__tests__/pages/Plan/PlanCreate.test.jsx
+++ frontend/src/__tests__/pages/Plan/PlanCreate.test.jsx
@@ -1,16 +1,19 @@
-import {configure} from "@testing-library/react"
+import {configure, render, screen} from "@testing-library/react"
 import "@testing-library/jest-dom"
+import PlanCreate from "../../../pages/Plan/PlanCreate"
+import userEvent from "@testing-library/user-event"
 //import { rest } from "msw"
 //import { server } from "../../server"
 configure({testIdAttribute: "id"})
 jest.mock("react-router", () => ({
-	useNavigate: () => jest.fn()
+	useNavigate: () => jest.fn(),
+	unstable_useBlocker: () => jest.fn()
 }))
 
 
 test.todo("Should render some basics on init") 
 test("Should render Skapa Grupp", async () => {
-	//render(<PlanCreate/>)
+	render(<PlanCreate/>)
 
 	//expect(screen.getByText("Skapa grupp")).toBeInTheDocument()
 })
@@ -27,7 +30,44 @@ test("Should render a toast error-message on creating duplicate groups", async (
 	// await the message and expect it to be in the document
 })
 
+describe("Create group/plan should", ()=> {
+	// beforeEach(() => {
+	// 	global.fetch = jest.fn()
+	// })
+	
+	test("not create plan/group when error occours", async () => {
+		jest.spyOn(global, "fetch").mockImplementationOnce(() => Promise.resolve({}))
 
+		const setIsBlocking = jest.fn()
+		const setSuccessToast = jest.fn()
+		const navigate = jest.fn()
+		const setBelts = jest.fn()
+		const planData = { name: "Test Plan" }
+		const beltsChosen = ["Yellow", "Green"]
+		const userId = "123"
+		const token = "fakeToken"
+		const dateHandler = jest.fn()
+		const blocker = false
+	
+		render(
+			<PlanCreate
+				setIsBlocking={setIsBlocking}
+				setSuccessToast={setSuccessToast}
+				navigate={navigate}
+				planData={planData}
+				beltsChosen={beltsChosen}
+				userId={userId}
+				token={token}
+				dateHandler={dateHandler}
+				setBelts={setBelts}
+				useBlocker={blocker}
+			/>)
+	
+		await userEvent.click(screen.getByText("Gå vidare"))
+	
+		expect(global.fetch).not.toHaveBeenCalledWith("/api/plan/add")
+	})
+})
 
 
 
