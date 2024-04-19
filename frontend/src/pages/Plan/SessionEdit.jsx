46d143c8f548d2dae75e29955464c8b90be132e1
Hugo Andersson <c21han@cs.umu.se>
2024-04-19 10:11:03 +0200

    Fixed so you cant begin changing workout until data fetch is done

--- frontend/src/pages/Plan/SessionEdit.jsx
+++ frontend/src/pages/Plan/SessionEdit.jsx
@@ -13,6 +13,7 @@ import { Trash } from "react-bootstrap-icons"
 import {setError as setErrorToast} from "../../utils"
 import ErrorState from "../../components/Common/ErrorState/ErrorState"
 import { unstable_useBlocker as useBlocker } from "react-router-dom"
+import { Spinner } from "react-bootstrap"
 
 /**
  * A page for editing a session.
@@ -44,6 +45,12 @@ export default function SessionEdit() {
 
 	const [goBackPopup, setGoBackPopup] = useState(false)
 	const [isBlocking, setIsBlocking] = useState(false)
+	
+	//Blockers that stops the page from loading the edit functionalities until all data is loaded
+	const [isLoadingGroups, setIsLoadingGroups] = useState([false]);
+	const [isLoadingWorkouts, setIsLoadingWorkouts] = useState([false]);
+	const [isLoadingSession, setIsLoadingSession] = useState([false]);
+
 
 	const blocker = useBlocker(() => {
 		if (isBlocking) {
@@ -63,16 +70,21 @@ export default function SessionEdit() {
 	// get groups
 	useEffect(() => {
 		(async () => {
+			setIsLoadingGroups(true)
 			try {
 				const response = await fetch("/api/plan/all", { headers: { token } })
 				if (response.status === 404) {
+					setIsLoadingGroups(false);
 					return
 				}
 				if (!response.ok) {
+					setIsLoadingGroups(false);
 					throw new Error("Could not fetch groups")
 				}
 				setGroups(await response.json())
+				setIsLoadingGroups(false);
 			} catch (ex) {
+				setIsLoadingGroups(false);
 				setErrorToast("Kunde inte hämta alla grupper")
 				console.error(ex)
 			}
@@ -82,15 +94,20 @@ export default function SessionEdit() {
 	useEffect(() => {
 		(async () => {
 			try {
+				setIsLoadingWorkouts(true);
 				const response = await fetch("/api/workouts/all", { headers: { token } })
 				if (response.status === 204) {
+					setIsLoadingWorkouts(false);
 					return
 				}
 				if (!response.ok) {
+					setIsLoadingWorkouts(false);
 					throw new Error("Could not fetch workouts")
 				}
 				setWorkouts(await response.json())
+				setIsLoadingWorkouts(false);
 			} catch (ex) {
+				setIsLoadingWorkouts(false);
 				setErrorToast("Kunde inte hämta alla pass")
 				console.error(ex)
 			}
@@ -99,12 +116,15 @@ export default function SessionEdit() {
 
 	useEffect(() => {
 		(async () => {
+			setIsLoadingSession(true);
 			try {
 				const response = await fetch("/api/session/get?id=" + params.session_id, { headers: { token } })
 				if (response.status === 204) {
+					setIsLoadingSession(false);
 					return
 				}
 				if (!response.ok) {
+					setIsLoadingSession(false);
 					throw new Error("Could not fetch session")
 				}
 				let session = await response.json()
@@ -118,8 +138,9 @@ export default function SessionEdit() {
 					setOriginalGroup(groups.find(group => group.id === session.plan))
 					setOriginalWorkout(workouts.find(workout => workout.id === session.workout))
 				}
-		
+				setIsLoadingSession(false);
 			} catch (ex) {
+				setIsLoadingSession(false);
 				setError("Kunde inte hämta tillfälle")
 				console.error(ex)
 			}
@@ -173,6 +194,7 @@ export default function SessionEdit() {
 	if (error) {
 		return <ErrorState message={error} onBack={() => navigate("/plan")} />
 	}
+	//Only loads a Spinner untill all data is fetched
 
 	return (
 		<>
@@ -183,52 +205,56 @@ export default function SessionEdit() {
 				<Trash className={styles.trashcanIcon} onClick={() => setShowPopup(true)}></Trash>
 			</div>
 			
-			<Divider option={"h2_left"} title={"Grupp"} />
-			<Dropdown id={"session-dropdown"} text={group?.name || "Grupp"} centered={true}>
-				{groups?.map((plan, index) => (
-					<div className={styles.dropdownRow} key={index} onClick={() => setGroup(plan)}>
-						<p className={styles.dropdownRowText}>{plan.name}</p>
+			{(isLoadingGroups||isLoadingWorkouts||isLoadingSession) ? <Spinner/>:
+				<>
+					<Divider option={"h2_left"} title={"Grupp"} />
+					<Dropdown id={"session-dropdown"} text={group?.name || "Grupp"} centered={true}>
+						{groups?.map((plan, index) => (
+							<div className={styles.dropdownRow} key={index} onClick={() => setGroup(plan)}>
+								<p className={styles.dropdownRowText}>{plan.name}</p>
+							</div>
+						))}
+					</Dropdown>
+					
+					<Divider option={"h2_left"} title={"Datum och Tid"} />
+					<div className={styles.wrapCentering} >
+						<DatePicker selectedDate={date} onChange={e => setDate(e.target.value)} id={"session-datepicker"} />
+						<TimePicker selectedTime={time} onChange={e => setTime(e.target.value)} id={"session-timepicker"} />
 					</div>
-				))}
-			</Dropdown>
-			
-			<Divider option={"h2_left"} title={"Datum och Tid"} />
-			<div className={styles.wrapCentering} >
-				<DatePicker selectedDate={date} onChange={e => setDate(e.target.value)} id={"session-datepicker"} />
-				<TimePicker selectedTime={time} onChange={e => setTime(e.target.value)} id={"session-timepicker"} />
-			</div>
 
-			<Divider option={"h2_left"} title={"Pass"} />
-			<Dropdown id={"session-dropdown"} text={workout?.name || "Sök befintligt pass"} centered={true}>
-				<div className={styles.dropdownRow} onClick={() => setWorkout(null)}>
-					<p className={styles.dropdownRowText}>Inget pass</p>
-				</div>
-				{workouts?.map((workout, index) => (
-					<div className={styles.dropdownRow} key={index} onClick={() => setWorkout(workout)}>
-						<p className={styles.dropdownRowText}>{workout.name}</p>
+					<Divider option={"h2_left"} title={"Pass"} />
+					<Dropdown id={"session-dropdown"} text={workout?.name || "Sök befintligt pass"} centered={true}>
+						<div className={styles.dropdownRow} onClick={() => setWorkout(null)}>
+							<p className={styles.dropdownRowText}>Inget pass</p>
+						</div>
+						{workouts?.map((workout, index) => (
+							<div className={styles.dropdownRow} key={index} onClick={() => setWorkout(workout)}>
+								<p className={styles.dropdownRowText}>{workout.name}</p>
+							</div>
+						))}
+					</Dropdown>
+
+					<Divider option={"h2_middle"} title={"eller"} />
+					<div className={`${styles.wrapCentering} ${styles.createButton}`} >
+						<Link to="/workout/create" style={{width: "150px"}} state={{
+							session: {
+								group,
+								date,
+								time,
+								workout
+							},
+							fromSession: true	
+						}}>
+							<Button id={"session-create"}><p>Skapa pass</p></Button>
+						</Link>
 					</div>
-				))}
-			</Dropdown>
 
-			<Divider option={"h2_middle"} title={"eller"} />
-			<div className={`${styles.wrapCentering} ${styles.createButton}`} >
-				<Link to="/workout/create" style={{width: "150px"}} state={{
-					session: {
-						group,
-						date,
-						time,
-						workout
-					},
-					fromSession: true	
-				}}>
-					<Button id={"session-create"}><p>Skapa pass</p></Button>
-				</Link>
-			</div>
-
-			<div className={styles.wrapCentering} style={{marginBottom: "2rem"}} >
-				<Button onClick={() => navigate("/plan")} id={"session-back"} outlined={true}><p>Tillbaka</p></Button> 
-				<Button onClick={updateSession} id={"session-save"}><p>Spara</p></Button>
-			</div>
+					<div className={styles.wrapCentering} style={{marginBottom: "2rem"}} >
+						<Button onClick={() => navigate("/plan")} id={"session-back"} outlined={true}><p>Tillbaka</p></Button> 
+						<Button onClick={updateSession} id={"session-save"}><p>Spara</p></Button>
+					</div>
+				</>
+			}
 			<ConfirmPopup
 				showPopup={goBackPopup}
 				setShowPopup={setGoBackPopup}
