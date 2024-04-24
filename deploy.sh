43dbec027d164d3b494a5e5ab958fe0362e66c6f
Hampus Sahlen <c21hsn@cs.umu.se>
2024-04-24 14:08:22 +0200

    Changed 3.1 to 3.0 in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,16 +20,16 @@ done
 if command -v docker-compose
 then    
     docker-compose down -v
-    docker build --no-cache -t pvt2024/database:3.1 infra/database
-    docker build --no-cache -t pvt2024/gateway:3.1 backend/gateway
-    docker build --no-cache -t pvt2024/api:3.1 backend/api
-    docker build --no-cache -t pvt2024/frontend:3.1 frontend
+    docker build --no-cache -t pvt2024/database:3.0 infra/database
+    docker build --no-cache -t pvt2024/gateway:3.0 backend/gateway
+    docker build --no-cache -t pvt2024/api:3.0 backend/api
+    docker build --no-cache -t pvt2024/frontend:3.0 frontend
     docker-compose -f docker-compose.release.yml up -d
 else 
     docker compose down -v
-    docker build --no-cache -t pvt2024/database:3.1 infra/database
-    docker build --no-cache -t pvt2024/gateway:3.1 backend/gateway
-    docker build --no-cache -t pvt2024/api:3.1 backend/api 
-    docker build --no-cache -t pvt2024/frontend:3.1 frontend
+    docker build --no-cache -t pvt2024/database:3.0 infra/database
+    docker build --no-cache -t pvt2024/gateway:3.0 backend/gateway
+    docker build --no-cache -t pvt2024/api:3.0 backend/api 
+    docker build --no-cache -t pvt2024/frontend:3.0 frontend
     docker compose -f docker-compose.release.yml up -d
 fi
