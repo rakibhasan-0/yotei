6454b20429f959d61a950a797ddad212cc918a0c
Hannes HH <c21hhn@cs.umu.se>
2024-04-24 08:58:58 +0200

    Fixed rendering issue with videoplayer

--- frontend/src/components/Gallery/Gallery.jsx
+++ frontend/src/components/Gallery/Gallery.jsx
@@ -70,18 +70,18 @@ export default function Gallery({ id }) {
 
 
 	return (
-		<div className={styles["container gallery-container"]} id={id}>
-			<div className={styles["row mt-2 mb-2"]}>
-				<div className={styles["col-sm-12 text-center "]}>
+		<div className={`container ${styles["gallery-container"]} `} id={id}>
+			<div className="row mt-2 mb-2">
+				<div className={`${styles["col-sm-12"]} text-center `}>
 					<Carousel showThumbs={false} showStatus={false} showArrows={true} >                         
 						{pictures.map((image, index) => (
-							<div className={styles["gallery-image-and-description"]} key={index}>
+							<div className="gallery-image-and-description" key={index}>
 								<Image path={image.url} key={index} />
 								{MediaDescription(image)}
 							</div>
 						))}
 						{videos.map((video, index) => (
-							<div className={styles["gallery-video-and-description"]} key={index}>
+							<div className="gallery-video-and-description" key={index}>
 								<VideoPlayerReactPlayer path={video.url} key={index}/>
 								{MediaDescription(video)}
 							</div>
