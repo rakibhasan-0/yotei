d8a07bfe02cb9625d339b8f0e2c02d8c0b602e40
Emil Engberg <c21eeg@cs.umu.se>
2024-04-24 11:42:58 +0200

    Added database to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -20,12 +20,14 @@ done
 if command -v docker-compose
 then    
     docker-compose down -v
+    docker build --no-cache -t pvt2024/database:3.1 infra/database
     docker build --no-cache -t pvt2024/gateway:3.1 backend/gateway
     docker build --no-cache -t pvt2024/api:3.1 backend/api
     docker build --no-cache -t pvt2024/frontend:3.1 frontend
     docker-compose -f docker-compose.release.yml up -d
 else 
     docker compose down -v
+    docker build --no-cache -t pvt2024/database:3.1 infra/database
     docker build --no-cache -t pvt2024/gateway:3.1 backend/gateway
     docker build --no-cache -t pvt2024/api:3.1 backend/api 
     docker build --no-cache -t pvt2024/frontend:3.1 frontend
