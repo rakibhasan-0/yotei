9844ba6943fe8769de96d493fca4399ade902ba8
Natnael Desalegnh <c21ndh@cs.umu.se>
2024-04-29 17:07:15 +0200

    created new activity page, is just a two tabs one for exercise and one for techniques

---
+++ frontend/src/pages/Activity/ActivityIndex.jsx
@@ -0,0 +1,30 @@
+import { Modal } from "react-bootstrap"
+import Tab from "react-bootstrap/Tab"
+import Tabs from "react-bootstrap/Tabs"
+import ExerciseIndex from "../Exercise/ExerciseIndex"
+import TechniqueIndex from "../Technique/TechniqueIndex/TechniqueIndex"
+import styles from "./ActivityIndex.module.css"
+import { useEffect, useState} from "react"
+
+const exerciseURI = "https://jsonplaceholder.typicode.com/users"
+
+export default function ActivityIndex(){
+    const [key, setKey] = useState(sessionStorage.getItem("active-tab") || "default-key")
+
+    useEffect(()=>{
+        sessionStorage.setItem("active-tab", key)
+    }, [key])
+
+    return (
+        <Modal.Body style={{ padding: "0" }}>
+            <Tabs activeKey={key} onSelect={(tab) => setKey(tab)} className={styles.tabs}>
+                <Tab eventKey="technique" title="Tekniker" tabClassName={`nav-link ${styles.tab}`}>
+                    <TechniqueIndex />
+                </Tab>
+                <Tab  eventKey="exercise" title="Övningar" tabClassName={`nav-link ${styles.tab}`}>
+                <ExerciseIndex uri={exerciseURI} />
+                </Tab>
+            </Tabs>
+        </Modal.Body>
+    )
+} 
