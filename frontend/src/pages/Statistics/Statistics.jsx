ad689f7d1c70473914e19936d4e2757cad99699e
Gazi Md Rakibul Hasan <rakibhasan_1@hotmail.com>
2024-05-02 15:48:31 +0200

    API has been updated

--- frontend/src/pages/Statistics/Statistics.jsx
+++ frontend/src/pages/Statistics/Statistics.jsx
@@ -23,9 +23,7 @@ export default function Statistics() {
 			try {
 				console.log("Fetching group data for ID:", groupID)
 
-				const response = await fetch(`/api/statistics/${groupID}`, {
-					headers: { token },
-				})
+				const response = await fetch("/api/plan/all", {headers: { token }});
 
 				if (!response.ok) {
 					throw new Error("Failed to fetch group data")
@@ -48,31 +46,29 @@ export default function Statistics() {
 	}, [groupID, token])
 
 	return (
-		<div>
-			{loading ? (
-				<Spinner />
-			) : (
-				<h1 style={{ fontSize: "35px" }}>
-					{group ? `${group.name}` : "Gruppen hittades inte"}
-				</h1>
-			)}
+    <div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <h1 style={{ fontSize: "35px" }}>
+          {group ? `${group.name}` : "Gruppen hittades inte"}
+        </h1>
+      )}
 
-			<div className={style.FilterAndSortContainer}>
-				<FilterStatistics />
-				<StatisticsPopUp />
-			</div>
-			{/*here we will show the technique, exercise and color of the belts, number of the exercise 	
-				we will use the TechniqueCard component to show the data
-			*/}
+      <div className={style.FilterAndSortContainer}>
+        <FilterStatistics />
+        <StatisticsPopUp />
+      </div>
+		{/*here we will show the technique, exercise and color of the belts, number of the exercise 	
+			we will use the TechniqueCard component to show the data ..../api/statistics/${groupID}
+		*/}
 
-
-	
-			<div className={style.buttonContainer}>
-				<Button width="25%" outlined={true} onClick={() => navigate(-1)}>
-					<p>Tillbaka</p>
-				</Button>
-			</div>
-		</div>
-	)
+      <div className={style.buttonContainer}>
+        <Button width="25%" outlined={true} onClick={() => navigate(-1)}>
+          <p>Tillbaka</p>
+        </Button>
+      </div>
+    </div>
+  );
 
 }
