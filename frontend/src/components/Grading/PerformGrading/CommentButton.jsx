c359fff687ca1516c1db91bbfeb5f716e7fd07fc
Lars Jonsson <ens21ljn@cs.umu.se>
2024-05-02 14:29:58 +0200

    implemented info component

--- frontend/src/components/Grading/PerformGrading/CommentButton.jsx
+++ frontend/src/components/Grading/PerformGrading/CommentButton.jsx
@@ -1,4 +1,5 @@
-import Button from "./../../Common/Button/Button.jsx"
+import RadioButton from "../../Common/RadioButton/RadioButton"
+
 import {Sliders} from "react-bootstrap-icons"
 /**
  * THIS IS A WORK IN PROGRESS.
@@ -16,15 +17,11 @@ import {Sliders} from "react-bootstrap-icons"
  */
 export default function CommentButton( { showComment, buttonToggle } ) {
 	return (
-		<div>
-			<Button
-				id={"comment-button"}
-				onClick={showComment}
-				outlined={false}
-				isToggled={buttonToggle}
-				width='40px'>
-				<Sliders/>
-			</Button>
-		</div>
+		<>
+			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-file-text" viewBox="0 0 16 16">
+				<path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5M5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1z"/>
+				<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1" />
+			</svg>
+		</>
 	)
 }
--- frontend/src/components/Grading/PerformGrading/TechniqueInfoPanel.jsx
+++ frontend/src/components/Grading/PerformGrading/TechniqueInfoPanel.jsx
@@ -1,5 +1,8 @@
 import {React} from "react"
-
+import styles from "../PerformGrading/TechniqueInfoPanel.module.css"
+import CommentButton from "../PerformGrading/CommentButton"
+import { AlignEnd, AlignTop } from "react-bootstrap-icons"
+import RadioButton from "../../Common/RadioButton/RadioButton"
 
 /**
  * This is a WORK IN PROGRESS.
@@ -17,33 +20,41 @@ import {React} from "react"
  * @returns {Element}
  * @constructor
  */
+
 export default function TechniqueInfoPanel( {
+	beltcolor = '#FFDD33',
 	techniqueId, 
 	categoryTitle = "Test Kategori",
-	currentTechniqueTitle = "Test nuvarande teknik",
-	nextTechniqueTitle = "Test nästa teknik",
+	currentTechniqueTitle = "1. Grepp i två handleder framifrån och svingslag Frigöring – Ju morote jodan uke",
+	nextTechniqueTitle = " 2. Stryptag framifrån och svingslag, backhand Frigöring – Ju morote jodan uke, ude osae, ude osae gatame",
 	mainCategoryTitle = "Huvudkategori"
 }) {
 
 
 
 	return (
-		<div id={techniqueId}>
-			<fieldset style={"height: 20px; width: 100%; margin-bottom: 10px; "}>
-				<div>
-					<h3>{mainCategoryTitle}</h3>
-				</div>
-			</fieldset>
-			<fieldset>
-				<legend>{categoryTitle}</legend>
-				<div>
-					<h1>{currentTechniqueTitle}</h1>
-				</div>
-				<div>
-					<h3>{nextTechniqueTitle}</h3>
-				</div>
-			</fieldset>
-		</div>
+		
+	<>
+		<fieldset style={{ height: '30px', width: '100%', marginBottom: '10px', backgroundColor: beltcolor}}>
+			<div>
+				<h2>{mainCategoryTitle}</h2>
+			</div>
+		</fieldset>
+		<fieldset style={{ height: 'auto', width: '100%', marginBottom: '5px' }}>
+			<div>
+				<h3>{categoryTitle}</h3>
+			</div>
+			<div>
+				<h1>{currentTechniqueTitle}</h1>
+			</div>
+			<div style={{width: '70%', float: 'left'}}>
+				<h3><b>Nästa:</b>{nextTechniqueTitle}</h3>
+			</div>
+			<div style={{ display: 'flex', justifyContent: 'flex-end'}}>
+				<CommentButton />
+			</div>
+		</fieldset>
+	</>
 	)
 
 }
