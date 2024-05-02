db7ac785c5da80199d00011ddf5c8c7cf9ea38b6
Gazi Md Rakibul Hasan <ens20ghn@umu.cs.umu.se>
2024-05-02 11:19:07 +0200

    some files have been updated

--- frontend/src/pages/Statistics/Statistics.jsx
+++ frontend/src/pages/Statistics/Statistics.jsx
@@ -8,7 +8,6 @@ import StatisticsPopUp from "./StatisticsPopUp";
 import FilterStatistics from "./FilterStatistics";
 
 export default function Statistics() {
-<<<<<<< HEAD
   const navigate = useNavigate();
   const { groupID } = useParams();
   const [group, setGroup] = useState(null);
@@ -16,7 +15,7 @@ export default function Statistics() {
   const { token } = useContext(AccountContext);
 
   useEffect(() => {
-	async function fetchGroupData() {
+    async function fetchGroupData() {
       try {
         console.log("Fetching group data for ID:", groupID);
         const response = await fetch("/api/plan/all", { headers: { token } });
@@ -48,10 +47,9 @@ export default function Statistics() {
       )}
 
       <div className={style.FilterAndSortContainer}>
-        <FilterStatistics />
+    	<FilterStatistics />
         <StatisticsPopUp />
       </div>
-
 
       <div className={style.buttonContainer}>
         <Button width="25%" outlined={true} onClick={() => navigate(-1)}>
@@ -60,85 +58,4 @@ export default function Statistics() {
       </div>
     </div>
   );
-=======
-	const navigate = useNavigate()
-	const { groupID } = useParams()
-	const [group, setGroup] = useState(null)
-	const [loading, setLoading] = useState(true)
-	const { token } = useContext(AccountContext)
-
-
-
-	useEffect(() => {
-		async function fetchGroupData() {
-			try {
-				console.log('Fetching group data for ID:', groupID)
-				const response = await fetch("/api/plan/all", { headers: { token } })
-
-				if (!response.ok) {
-					throw new Error('Failed to fetch group data')
-				}
-				const data = await response.json()
-				const groupData = data.find(group => group.id === parseInt(groupID))
-				setGroup(groupData);
-
-			} catch (error) {
-				console.error('Fetching error:', error) // proper handling of error should be implemented
-			} finally {
-				setLoading(false)
-			}
-		}
-
-		fetchGroupData()
-	}, [groupID, token])
-
-
-	return (
-		<div>
-			{loading ? <Spinner /> : <h1 style={{ fontSize: '35px' }}>{group ? `${group.name}` : 'Gruppen hittades inte'}</h1>}
-
-			<div style={{ position: 'relative' }}>
-
-				<FilterContainer id="filter-container" title="Filtering" numFilters={0}>
-
-					<div className={style.dateContainer}>
-						<h2>Från</h2>
-						<div></div>
-						<DatePicker
-							id="start-date-picker"
-							minDate={"2023-05-11"}
-							maxDate={"2026-05-07"}
-						/>
-					</div>
-					<div className={style.dateContainer}>
-						<h2>Till</h2>
-						<div></div>
-						<DatePicker
-							id="end-date-picker"
-							minDate={"2023-05-11"}
-							maxDate={"2026-05-07"}
-						/>
-					</div>
-					<BeltPicker id={"techniqueFilter-BeltPicker"} onToggle={() => { }} states={[]} onClearBelts={() => { }} filterWhiteBelt={false} />
-
-					<div className={style.checkboxContainer}>
-						<h2>Visa övningar</h2>
-						<div></div>
-						<CheckBox id={"techniqueFilter-KihonCheck"} checked={false} onClick={() => { }} />
-					</div>
-
-				</FilterContainer>
-
-				<StatisticsPopUp />
-
-			</div>
-
-			<div className={style.buttonContainer}>
-				<Button width="25%" outlined={true} onClick={() => navigate(-1)}> <p>Tillbaka</p> </Button>
-			</div>
-
-
-		</div>
-	)
->>>>>>> 58309dc50199d80ce65c8299ae55ff30ab7101d6
 }
