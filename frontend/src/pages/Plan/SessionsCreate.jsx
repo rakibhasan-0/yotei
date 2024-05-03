e06ba31a70dde809fece05d11337cd3d730bbbc1
Natnael Desalegnh <c21ndh@cs.umu.se>
2024-05-03 16:06:54 +0200

    Visual properties of SesssionsCreate works, but no practical usability yet

--- frontend/src/pages/Plan/SessionsCreate.jsx
+++ frontend/src/pages/Plan/SessionsCreate.jsx
@@ -2,12 +2,16 @@ import { React, useState, useContext, useEffect } from "react"
 import { AccountContext } from "../../context"
 import { useNavigate } from "react-router"
 import PlanForm from "../../components/Forms/PlanForm.jsx"
-import styles from "./PlanCreate.module.css"
+import styles from "./SessionsCreate.module.css"
 import {setError as setErrorToast, setSuccess as setSuccessToast} from "../../utils"
 import { unstable_useBlocker as useBlocker } from "react-router"
 import ConfirmPopup from "../../components/Common/ConfirmPopup/ConfirmPopup"
 import { useLocation } from "react-router-dom"
-
+import Dropdown from "../../components/Common/List/Dropdown"
+import Divider from "../../components/Common/Divider/Divider.jsx"
+import DatePicker from "../../components/Common/DatePicker/DatePicker"
+import WeekdayTimePicker from "../../components/Plan/WeekdayTimePicker.jsx"
+import Button from "../../components/Common/Button/Button.jsx"
 
 
 /**
@@ -24,6 +28,14 @@ import { useLocation } from "react-router-dom"
 
 
 export default function SessionsCreate(){
+
+	
+
+
+	const today = new Date()
+	const twoYearsFromNow = new Date()
+	twoYearsFromNow.setFullYear(today.getFullYear()+2)
+
     const { state } = useLocation()
     const navigate = useNavigate()
     const { token } = useContext(AccountContext)
@@ -63,6 +75,291 @@ export default function SessionsCreate(){
 	}, [token])
 
 
+/**
+     * Local model of a plan.
+     */
+	const [planData, setPlanData] = useState({
+		name: "",
+		startDate: "",
+		endDate: "",
+		weekdaysSelected: false,
+	})
+
+
+
+	/**
+     * Will hold the dates for the weekdays
+     * initially selected in the form.
+     */
+	var firstDatesArray = []
+
+	/**
+     * Will hold the dates for all weekdays
+     * selected in the form, between the
+     * starting date and ending date.
+     */
+	var allDatesArray = []
+
+	/**
+     * Model of what fields are correctly modeled.
+     */
+	const [fieldCheck, setFieldCheck] = useState({
+		name: false,
+		startDate: false,
+		endDate: false,
+	})
+
+	/**
+     * Is called when the data is modified.
+     * Updates the local data.
+     *
+     * @param variableName  The name of the variable being updated.
+     * @param value         The updated value.
+     */
+	const dataClickHandler = (variableName, value) => {
+		setTextInputErrorMsg("")
+		setPlanData({ ...planData, [variableName]: value })
+	}
+
+
+	/**
+     * Is called when a weekday is chosen in the form.
+     * Updates the local data.
+     *
+     * @param dayName   The 3-4 letter name of the day.
+     */
+	const weekdayClickHandler = (dayName) => {
+
+		var dayRow
+
+		for (var i = 0; i < 7; i++) {
+			dayRow = weekdays[i]
+
+			if (dayRow.name === dayName) {
+				dayRow.value = !dayRow.value
+				setWeekdays({ ...weekdays, [i]: dayRow })
+				break
+			}
+		}
+		setWeekdaysSelected()
+	}
+
+
+	/**
+     * Is called when the time slot for a day is modified in the form.
+     * Updates the local data.
+     *
+     * @param dayName   The 3-4 letter name of the day.
+     * @param value     The time value.
+     */
+	const dayTimeClickHandler = (dayName, value) => {
+
+		var dayRow
+
+		for (var i = 0; i < 7; i++) {
+			dayRow = weekdays[i]
+
+			if (dayRow.name === dayName) {
+				dayRow.time = value
+				setWeekdays({ ...weekdays, [i]: dayRow })
+				break
+			}
+		}
+	}
+
+
+	/**
+     * A function that checks that a given field in the form is not empty.
+     * Updates the fieldCheck useState.
+     *
+     * @param fieldName     The name of the field being checked
+     * @returns             If the field is not empty.
+     */
+	function checkNotEmpty(fieldName) {
+
+		const value = planData[fieldName]
+		const isEmpty = value === undefined || value === null || value === ""
+
+		if (!fieldCheck[fieldName]) {
+			if (!isEmpty)
+				setFieldCheck({ ...fieldCheck, [fieldName]: true })
+		}
+		else {
+			if (isEmpty)
+				setFieldCheck({ ...fieldCheck, [fieldName]: false })
+		}
+
+		return fieldCheck[fieldName]
+	}
+
+	/**
+     * A function that checks if both date-fields are empty, or a given
+     * date-field in the form is not empty.
+     *
+     * @param dateFieldName The name of the date-field being checked
+     * @returns             True if both date fields are empty.
+     *                      Else returns whether the field is not empty.
+     */
+	function checkDate(dateFieldName) {
+
+		checkNotEmpty("startDate")
+		checkNotEmpty("endDate")
+
+		if (!fieldCheck.startDate && !fieldCheck.endDate)
+			return true
+
+		else
+			checkNotEmpty(dateFieldName)
+
+		return fieldCheck[dateFieldName]
+	}
+
+
+	
+	/**
+     * A state representing each day of the week. The booleafieldCheckn values are used for deciding which days should be displayed.
+     * The user can choose a time for each day.
+     */
+	const [weekdays, setWeekdays] = useState([
+		{ name: "Mån", value: false, time: "" },
+		{ name: "Tis", value: false, time: "" },
+		{ name: "Ons", value: false, time: "" },
+		{ name: "Tors", value: false, time: "" },
+		{ name: "Fre", value: false, time: "" },
+		{ name: "Lör", value: false, time: "" },
+		{ name: "Sön", value: false, time: "" }
+	])
+
+
+	/**
+     * Checks if any weekdays selected
+     * Return true if 1+ weekdays selected, false if none
+     */
+	function setWeekdaysSelected() {
+		for (let i = 0; i < weekdays.length; i++) {
+			if (weekdays[i].value === true) {
+				setPlanData({ ...planData, weekdaysSelected: true})
+				return true
+			}
+		}
+		setPlanData({ ...planData, weekdaysSelected: false})
+		return false
+	}
+
+	/**
+     * Handles the dates for the chosen sessions. Populates allDatesArray with
+     * the days chosen by the user within the span
+     * between startDate and endDate.
+     *
+     * @param plan  the id of group
+     */
+	function dateHandler(plan) {
+
+		/* Format Date to ISO standard (YYYY-MM-DD) */
+		const formatDate = (date) => {
+			const [dateStr] = new Date(date).toISOString().split("T")
+			return dateStr
+		}
+
+		/* Check for start and end date */
+		if (fieldCheck.startDate) {
+			if (fieldCheck.endDate) {
+
+
+				let startDate = new Date(planData.startDate)
+				let endDate = new Date(planData.endDate)
+				let dayNr = startDate.getDay()
+
+				var tempDate
+				var tempTime
+				console.log("Inside DateHandler")
+				/* Find first date occurences of chosen weekdays */
+				for (var i = 0; i < 7; i++) {
+
+					if (weekdays[dayNr].value) {
+
+						tempDate = new Date(startDate)
+						tempTime = weekdays[dayNr].time.target.value + ":00"
+
+						firstDatesArray.push({ date: tempDate, time: tempTime })
+					}
+
+					/* Update loop variables */
+					dayNr = (dayNr + 1) % 7
+					startDate.setDate(startDate.getDate() + 1)
+				}
+
+				/* If days chosen */
+				if (firstDatesArray.length !== 0) {
+
+					/* Find all date occurences of chosen weekdays */
+					firstDatesArray.forEach((e) => {
+
+						tempDate = new Date(e.date)
+						tempTime = e.time
+
+						while (tempDate <= endDate) {
+							allDatesArray.push({ plan: plan, date: formatDate(tempDate), time: tempTime })
+
+							tempDate.setDate(tempDate.getDate() + 7)
+						}
+					})
+
+					/* Add plan id to each object in array */
+					allDatesArray = allDatesArray.map(session => {
+						return { plan: plan, ...session }
+					})
+
+					addSessions()
+				} 
+			}
+		}
+	}
+
+	/**
+     * Function for api call when creating a plan
+     */
+	async function addSessions() {
+		allDatesArray = allDatesArray.map(session => {
+			let x = new Date(session.date)
+			let hoursDiff = x.getHours() - x.getTimezoneOffset() / 60
+			let minutesDiff = (x.getHours() - x.getTimezoneOffset()) % 60
+			x.setHours(hoursDiff)
+			x.setMinutes(minutesDiff)
+			x.setDate(x.getDate()+1)
+			return { ...session, date: new Date(x)}
+		})
+		console.log("Inside ADDSESSION")
+		const requestOptions = {
+			method: "POST",
+			headers: { "Content-type": "application/json", "token": token },
+			body: JSON.stringify(allDatesArray)
+		}
+
+		/* Post Sessions */
+		try {
+			const response = await fetch("/api/session/addList", requestOptions)
+			await response.json()
+
+			
+			if (response.ok) {
+				setSuccessToast("Tillfällen lades till.")
+			}
+
+		} catch (error) {
+			setErrorToast("Tillfällen kunde inte läggas till.")
+		}
+	}
+
+
+	
+
+
+	
+
+
+
+
 
     return (
         <>
@@ -75,16 +372,117 @@ export default function SessionsCreate(){
 				setShowPopup={setGoBackPopup}
 				popupText={"Är du säker på att du vill lämna sidan? Dina ändringar kommer inte att sparas."}
 			/>
+			<title>Skapa flera tillfällen</title>
+			<h1 style={{ marginTop: "2rem" }}>Skapa flera tillfällen</h1>
+			<Divider option={"h2_left"} title={"Grupp"} />
             <Dropdown errorMessage={groupError} id={"session-dropdown"} text={group?.name || "Grupp"} centered={true}>
-				{groups?.length > 0 ? groups.map((plan, index) => (
-					<div className={styles.dropdownRow} key={index} onClick={() => setGroup(plan)}>
-						<p className={styles.dropdownRowText}>{plan.name}</p>
+				{groups?.length > 0 ? groups.map((group, index) => (
+					<div className={styles.dropdownRow} key={index} onClick={() => setGroup(group)}>
+						<p className={styles.dropdownRowText}>{group.name}</p>
 					</div>
 				)) : <div className={styles.dropdownRow}>
 					<p className={styles.dropdownRowText}>Kunde inte hitta några grupper</p>
 				</div>}
 			</Dropdown>
+
+			<Divider title="Period" option="h2_left"/>
+			<div className={styles.start_end_date}>
+				<p className={styles.p_date_name}>Från:</p>
+				<div className={styles.p_date_picker}>
+					<DatePicker 
+						id="start-date-picker"
+						onChange={(e) => {onClickData("startDate", e.target.value) }} 
+						minDate={dateFormatter(today)}   
+					/>
+				</div>
+				<p className={styles.p_date-name}>Till:</p>
+				<div className={styles.p_date_picker}>
+					<DatePicker 
+						id="end-date-picker"
+						onChange={(e) => {onClickData("endDate", e.target.value) }}
+						maxDate={dateFormatter(twoYearsFromNow)}
+					/>
+				</div>
+			</div>
+			<Divider title="Dagar" option="h2_left"/>
+			<div style={{marginTop: "20px"}}>
+				<div className={styles.weekday_container}>
+					<div className={styles.div_week_day_time}>
+						<WeekdayTimePicker 
+							dayName="Mån" 
+							weekdays={weekdays}
+							weekdayClickHandler={weekdayClickHandler}
+							dayTimeClickHandler={dayTimeClickHandler}
+						/>
+						<WeekdayTimePicker 
+							dayName="Tis" 
+							weekdays={weekdays}
+							weekdayClickHandler={weekdayClickHandler}
+							dayTimeClickHandler={dayTimeClickHandler}
+						/>
+						<WeekdayTimePicker 
+							dayName="Ons" 
+							weekdays={weekdays}
+							weekdayClickHandler={weekdayClickHandler}
+							dayTimeClickHandler={dayTimeClickHandler}
+						/>
+						<WeekdayTimePicker 
+							dayName="Tors" 
+							weekdays={weekdays}
+							weekdayClickHandler={weekdayClickHandler}
+							dayTimeClickHandler={dayTimeClickHandler}
+						/>
+						<WeekdayTimePicker 
+							dayName="Fre" 
+							weekdays={weekdays}
+							weekdayClickHandler={weekdayClickHandler}
+							dayTimeClickHandler={dayTimeClickHandler}
+						/>
+						<WeekdayTimePicker 
+							dayName="Lör" 
+							weekdays={weekdays}
+							weekdayClickHandler={weekdayClickHandler}
+							dayTimeClickHandler={dayTimeClickHandler}
+						/>
+						<WeekdayTimePicker 
+							dayName="Sön"
+							weekdays={weekdays}
+							weekdayClickHandler={weekdayClickHandler}
+							dayTimeClickHandler={dayTimeClickHandler}
+						/>
+					</div>
+				</div>
+			</div>
+			<div className={styles.button_container}>
+				<Button 
+					//onClick={navigate(-1)}
+					outlined={true}
+				>
+					<p>
+						Tillbaka
+					</p>
+				</Button>
+				<Button onClick={dateHandler(group)}>
+					<p>
+						Gå vidare
+					</p>
+				</Button>
+			</div> 
         
         </>
     )
+}
+
+/**
+	 * Formats given date to a string with formatting 'YYYY-MM-DD'
+	 * 
+	 * @param { string } today - Date to be formatted.
+	 * @returns Date as a string on the correct format.
+	 */
+function dateFormatter(today) {
+	const date = today
+	const year = date.getFullYear()
+	const month = String(date.getMonth() + 1).padStart(2, "0")
+	const day = String(date.getDate()).padStart(2, "0")
+	return [year, month, day].join("-")
 }
