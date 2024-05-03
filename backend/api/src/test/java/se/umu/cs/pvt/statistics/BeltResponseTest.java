e6044a02ec88f6d4f45b3f4189a0ed2653e4c641
Linus Wallenborg <c21lwg@cs.umu.se>
2024-05-03 11:01:02 +0200

    Added unit tests for response entities

---
+++ backend/api/src/test/java/se/umu/cs/pvt/statistics/BeltResponseTest.java
@@ -0,0 +1,37 @@
+package se.umu.cs.pvt.statistics;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import se.umu.cs.pvt.belt.Belt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BeltResponseTest {
+    
+        
+    BeltResponse beltResponse;
+
+    @BeforeEach
+    void init() {
+        Belt belt = new Belt(1L, "Gult", "123123", false);
+        beltResponse = new BeltResponse(belt);
+    }
+
+    @Test
+    void beltResponseShouldReturnName() {
+        assertEquals("Gult",beltResponse.getBelt_name());
+    }
+
+    @Test
+    void beltResponseShouldReturnColor() {
+        assertEquals("123123",beltResponse.getBelt_color());
+    }
+
+    @Test
+    void beltResponseShouldReturnIsChild() {
+        assertEquals(false,beltResponse.getIs_child());
+    }
+
+
+}
