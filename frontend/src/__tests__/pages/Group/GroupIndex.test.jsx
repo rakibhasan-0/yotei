1402abc4196e8b21d608ab0b9b5129cefe07c88e
Tor Larsson <c21tln@cs.umu.se>
2024-05-03 09:28:05 +0200

    Fixed the test for text not appearing on the page when there are groups.

--- frontend/src/__tests__/pages/Group/GroupIndex.test.jsx
+++ frontend/src/__tests__/pages/Group/GroupIndex.test.jsx
@@ -3,7 +3,6 @@ import "@testing-library/jest-dom"
 import GroupIndex from "../../../pages/Plan/GroupIndex/GroupIndex"
 import { rest } from "msw"
 import { server } from "../../server"
-import { act } from '@testing-library/react' //This is to get rid of warnings when testing.
 
 /**
  * Unit-test for the GroupIndex page, 
@@ -27,14 +26,13 @@ jest.mock("react-router-dom", () => ({
 }))
 
 test("Should render title on init", async () => {
-	act( () => { render(<GroupIndex/>) } )
+	render(<GroupIndex/>)
 	//Gets the first "Grupper" so there can exist a header and a tab title with the same name.
 	expect(screen.getAllByText("Grupper")[0]).toBeInTheDocument()
 })
 
 // This test should make sure that the group search bar is working as intended (is case insensitive)
 test("Should render a group when searching for it", async () => {
-act( () => {
 	render(<GroupIndex/>)
 
 	server.use(
@@ -75,11 +73,10 @@ act( () => {
 						]
 					},
 				])
-                
+			
 			)
 		})
 	)
-})
 
 	/*After rendering screen and fetching info, 
     the test finds the searchbar and gives it the input "grönt" 
@@ -88,19 +85,19 @@ act( () => {
 	await screen.findByTestId("searchbar-groups")
 	const searchInput = screen.getByTestId("searchbar-input")
     
-	act( () => fireEvent.change(searchInput, { target: { value: "gRöNt" } }) )
+	fireEvent.change(searchInput, { target: { value: "gRöNt" } })
 
 	//Test 2: Should only one of the belts, this time with uppercase letters.
 	expect(screen.getByText("Grönt bälte träning")).toBeInTheDocument()
 	expect(screen.queryByText("Orange och Gult bälte träning")).not.toBeInTheDocument()
 
-	act( () => fireEvent.change(searchInput, { target: { value: "g" } }) )
+	fireEvent.change(searchInput, { target: { value: "g" } })
 
 	//Test 3: Should find both belts.
 	expect(screen.getByText("Grönt bälte träning")).toBeInTheDocument()
 	expect(screen.getByText("Orange och Gult bälte träning")).toBeInTheDocument()
 
-	act( () => fireEvent.change(searchInput, { target: { value: "G" } }) )
+	fireEvent.change(searchInput, { target: { value: "G" } })
 
 	//Test 4: Should find both belts, this time with uppercase letters again.
 
@@ -112,23 +109,57 @@ act( () => {
 
 //This tests the edge case of there being no groups (previously caused a bug).
 test("Should display proper message when there are no groups", async () => {
-	
-	act( () => {
+	render(<GroupIndex/>)
 
-		render(<GroupIndex/>)
+	server.use(
+		rest.get("api/plan/all", async (req, res, ctx) => {
+			return res(
+				ctx.status(200),
+				//ctx.json([{},]) //This needs to be commented out/removed. Leaving it empty still counts as a group being made.
+			)
+		})
+	)
 
-		server.use(
-			rest.get("api/plan/all", async (req, res, ctx) => {
-				return res(
-					ctx.status(200),
-					//ctx.json([{},]) //This needs to be commented out/removed. Leaving it empty still counts as a group being made.
-				)
-			})
-		)
-
-	} )
 	await screen.findByTestId("searchbar-groups") //This is needed for the test to work.
 	//const searchInput = screen.getByTestId("searchbar-input")
 	//act( () => fireEvent.change(searchInput, { target: { value: "g" } }) )
 	expect(screen.getByText("Det finns inga grupper att visa")).toBeInTheDocument() // This line should be changed to the updated error message if the test fails. Or this should not be tested in this way...
+	//If the line is updated then make sure to fix the test below for when there are groups.
 })
+
+
+//This tests the edge case of there being no groups (previously caused a bug).
+test("Should not display groups missing message when there are groups", async () => {
+
+	render(<GroupIndex/>)
+
+	server.use(
+		rest.get("api/plan/all", async (req, res, ctx) => {
+			return res(
+				ctx.status(200),
+				ctx.json([
+					{
+						"id": 1,
+						"name": "Grönt bälte träning",
+						"userId": 1,
+						"belts": [
+							{
+								"id": 7,
+								"name": "Grönt",
+								"color": "0C7D2B",
+								"child": false
+							}
+						]
+					},
+				])
+			) 
+		})
+	)
+
+	await screen.findByTestId("searchbar-groups") //This is needed for the test to work.
+
+	//Test that the text does not exist on the page.
+	const testText = screen.queryByText("Det finns inga grupper att visa") //This text needs to be updated in the test too if it is changed.
+	expect(testText).toBeNull()
+})
+
