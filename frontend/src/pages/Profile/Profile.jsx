06019fea3d887cf3ed941040c43b1d6770c0d0f0
Lee Lannerblad <ens19lld@cs.umu.se>
2024-05-06 09:58:30 +0200

    styling for ProfileListItem

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react"
 import { Tab, Tabs } from "react-bootstrap"
-import {setError as setErrorToast, setSuccess as setSuccessToast} from "../../utils"
+import { setError as setErrorToast, setSuccess as setSuccessToast } from "../../utils"
 import ActivityList from "../../components/Activity/ActivityList"
 import Button from "../../components/Common/Button/Button"
 import SearchBar from "../../components/Common/SearchBar/SearchBar"
@@ -13,9 +13,8 @@ import useMap from "../../hooks/useMap"
 import Divider from "../../components/Common/Divider/Divider"
 import Spinner from "../../components/Common/Spinner/Spinner"
 import ProfileListItem from "./ProfileListItem"
-import {Lock,Unlock, Eye,StarFill} from "react-bootstrap-icons"
+import { Lock, Unlock, Eye, StarFill } from "react-bootstrap-icons"
 import StarButton from "../../components/Common/StarButton/StarButton"
-
 
 /**
  * @author Chimera, Team Mango (Group 4), Team Pomegranate(Group 1), Team Durian (Group 3) (2024-04-23)
@@ -26,10 +25,10 @@ import StarButton from "../../components/Common/StarButton/StarButton"
 export default function Profile() {
 	const { userId, token } = useContext(AccountContext)
 
-	const [ workouts, setWorkouts ] = useState()
-	const [ searchText, setSearchText ] = useState("")
+	const [workouts, setWorkouts] = useState()
+	const [searchText, setSearchText] = useState("")
 
-	const [ cache, cacheActions ] = useMap()
+	const [cache, cacheActions] = useMap()
 	const [password, setPassword] = useState("")
 	const [newPassword, setNewPassword] = useState("")
 	const [verifyNewPassword, setVerifyNewPassword] = useState("")
@@ -41,45 +40,58 @@ export default function Profile() {
 	const [verifyUsernamePassword, setVerifyUsernamePassword] = useState("")
 
 	const MockList = {
-		"list_id": -1,
-		"list_name": "TestList",
-		"state": "Favourite",
-		"amountOfWorkouts": 3,
-		"author": "Oliver"
+		list_id: 1,
+		list_name: "TestList",
+		state: "Locked",
+		amountOfWorkouts: 3,
+		author: "Oliver",
 		//Locked
 		//Public
 	}
 	/* Workout management */
 
-	const WorkoutList = ({list}) => {
+	const WorkoutList = ({ list }) => {
 		// eslint-disable-next-line no-unused-vars
 		const onFavorite = async (_, workout) => {
 			// Workaround for updating the list
 			// due to how the star button in the activity list works
-			setWorkouts(old => 
-				[...old.map(w => {
+			setWorkouts((old) => [
+				...old.map((w) => {
 					if (w.workoutID === workout.workoutID) {
 						w.favourite = !w.favourite
 					}
 					return w
-				})]
-			)
+				}),
+			])
 		}
 		return (
 			<>
-				{list && <ActivityList id="workout-list" activities={list} apiPath={"workouts"} favoriteCallback={onFavorite} />}
+				{list && (
+					<ActivityList
+						id="workout-list"
+						activities={list}
+						apiPath={"workouts"}
+						favoriteCallback={onFavorite}
+					/>
+				)}
 			</>
 		)
 	}
 
 	useEffect(() => {
-		getWorkouts({ text: searchText, isFavorite: false, id: userId, selectedTags: ""}, token, cache, cacheActions, list => {
-			if (list.error) {
-				return setErrorToast("Kunde inte hämta pass!")
+		getWorkouts(
+			{ text: searchText, isFavorite: false, id: userId, selectedTags: "" },
+			token,
+			cache,
+			cacheActions,
+			(list) => {
+				if (list.error) {
+					return setErrorToast("Kunde inte hämta pass!")
+				}
+				setWorkouts(list.results)
+				setIsLoading(false)
 			}
-			setWorkouts(list.results)
-			setIsLoading(false)
-		})
+		)
 	}, [searchText, token, userId, cache, cacheActions])
 
 	/* Account management */
@@ -92,9 +104,14 @@ export default function Profile() {
 		setWrongPassword(undefined)
 
 		const requestOptions = {
-			headers: {"Content-Type": "application/json", token},
+			headers: { "Content-Type": "application/json", token },
 			method: "PUT",
-			body: JSON.stringify({newPassword: newPassword, verifyNewPassword: verifyNewPassword, oldPassword: password, id: userId})
+			body: JSON.stringify({
+				newPassword: newPassword,
+				verifyNewPassword: verifyNewPassword,
+				oldPassword: password,
+				id: userId,
+			}),
 		}
 		const response = await fetch("/api/users/password", requestOptions)
 		if (!response.ok) {
@@ -107,9 +124,9 @@ export default function Profile() {
 		setVerifyUsernamePassword(undefined)
 
 		const requestOptions = {
-			headers: {"Content-Type": "application/json", token},
+			headers: { "Content-Type": "application/json", token },
 			method: "PUT",
-			body: JSON.stringify({newUsername: newUsername, password: UsernamePassword, id: userId})
+			body: JSON.stringify({ newUsername: newUsername, password: UsernamePassword, id: userId }),
 		}
 		const response = await fetch("/api/users/name", requestOptions)
 		if (response.status === 400) {
@@ -124,76 +141,123 @@ export default function Profile() {
 		setSuccessToast("Användarnamnet har ändrats.")
 	}
 
-	const getIconFromState = (state) =>{
-		switch(state){
+	const getIconFromState = (state) => {
+		switch (state) {
 			case "Locked":
 				console.log("Locked")
-				return <Lock/>
+				return <Lock size={36} />
 			case "Shared":
 				console.log("Shared")
-				return <Unlock/>
+				return <Unlock size={36} />
 			case "Public":
 				console.log("Public")
-				return <Eye/>
+				return <Eye size={36} />
 			case "Favourite":
 				console.log("Favourite!")
 				//Här borde jag fixa en route till favoritsidans grej :)
 				return <StarButton id="starbutton-example" toggled={true} onClick={null} />
-				//return StarFill
+			//return StarFill
 			default:
 				console.log("Default")
-				return <Lock/>
+				return <Lock />
 		}
 	}
 
-	let icon = getIconFromState(MockList.state);
+	let icon = getIconFromState(MockList.state)
 
 	return (
 		<Tabs defaultActiveKey={"FavoriteWorkouts"} className={style.tabs}>
-			
 			<Tab eventKey={"FavoriteWorkouts"} title={"Favoritpass"} className={style.tab}>
-				<SearchBar 
-					id="searchbar-workouts-1" 
-					placeholder="Sök efter pass" 
-					text={searchText} 
+				<SearchBar
+					id="searchbar-workouts-1"
+					placeholder="Sök efter pass"
+					text={searchText}
 					onChange={setSearchText}
 				/>
 				{loading ? <Spinner /> : <ProfileListItem item={MockList} Icon={icon} />}
 			</Tab>
 			<Tab eventKey={"MyWorkouts"} title={"Mina Pass"} className={style.tab}>
-				<SearchBar 
-					id="searchbar-workouts-2" 
-					placeholder="Sök efter pass" 
-					text={searchText} 
+				<SearchBar
+					id="searchbar-workouts-2"
+					placeholder="Sök efter pass"
+					text={searchText}
 					onChange={setSearchText}
 				/>
-				{loading ? <Spinner /> :<WorkoutList list={workouts?.filter(w => w.author === userId)} />}
+				{loading ? <Spinner /> : <WorkoutList list={workouts?.filter((w) => w.author === userId)} />}
 			</Tab>
 			<Tab eventKey={"Settings"} title={"Inställningar"} className={style.tab}>
-				<div className={style.divider}><Divider option={"h2_center"} title={"Lösenord"} /></div>
+				<div className={style.divider}>
+					<Divider option={"h2_center"} title={"Lösenord"} />
+				</div>
 				<title>Min sida</title>
-				<InputTextFieldBorderLabel errorMessage={wrongPassword} onChange={e => {setPassword(e.target.value)}} id="current-password" type="password" label="Nuvarande lösenord" />
-				<div className='mb-2' />
-				<InputTextFieldBorderLabel onChange={e => {setNewPassword(e.target.value)}} id="new-password" type="password" label="Nytt lösenord" />
-				<div className='mb-2' />
-				<InputTextFieldBorderLabel errorMessage={missMatchPassword} onChange={e => {setVerifyNewPassword(e.target.value)}} id="verify-password" type="password" label="Bekräfta lösenord" />
-				<div className='mb-2' />
+				<InputTextFieldBorderLabel
+					errorMessage={wrongPassword}
+					onChange={(e) => {
+						setPassword(e.target.value)
+					}}
+					id="current-password"
+					type="password"
+					label="Nuvarande lösenord"
+				/>
+				<div className="mb-2" />
+				<InputTextFieldBorderLabel
+					onChange={(e) => {
+						setNewPassword(e.target.value)
+					}}
+					id="new-password"
+					type="password"
+					label="Nytt lösenord"
+				/>
+				<div className="mb-2" />
+				<InputTextFieldBorderLabel
+					errorMessage={missMatchPassword}
+					onChange={(e) => {
+						setVerifyNewPassword(e.target.value)
+					}}
+					id="verify-password"
+					type="password"
+					label="Bekräfta lösenord"
+				/>
+				<div className="mb-2" />
 				<div className={style.floatRight}>
-					<Button className="btn btn-primary" onClick={changePassword}>Ändra</Button>
+					<Button className="btn btn-primary" onClick={changePassword}>
+						Ändra
+					</Button>
 				</div>
-				<div className={style.divider}><Divider option={"h2_center"} title={"Användarnamn"} /></div>
-				<InputTextFieldBorderLabel onChange={e => {setNewUsername(e.target.value)}} id="username" type="text" label="Nytt användarnamn" />
-				<div className='mb-2' />
-				<InputTextFieldBorderLabel onChange={e => {setUsernamePassword(e.target.value)}} errorMessage={verifyUsernamePassword} id="change-username-password" type="password" label="Lösenord" />
-				<div className='mb-2' />
+				<div className={style.divider}>
+					<Divider option={"h2_center"} title={"Användarnamn"} />
+				</div>
+				<InputTextFieldBorderLabel
+					onChange={(e) => {
+						setNewUsername(e.target.value)
+					}}
+					id="username"
+					type="text"
+					label="Nytt användarnamn"
+				/>
+				<div className="mb-2" />
+				<InputTextFieldBorderLabel
+					onChange={(e) => {
+						setUsernamePassword(e.target.value)
+					}}
+					errorMessage={verifyUsernamePassword}
+					id="change-username-password"
+					type="password"
+					label="Lösenord"
+				/>
+				<div className="mb-2" />
 				<div className={style.floatRight}>
-					<Button className="btn btn-primary" onClick={changeUsername}>Ändra</Button>
+					<Button className="btn btn-primary" onClick={changeUsername}>
+						Ändra
+					</Button>
 				</div>
-				<Divider option={"h2_center"}/>
+				<Divider option={"h2_center"} />
 				<div>
-					<Button onClick={logOut} width={"100%"} className="btn btn-primary">Logga ut</Button>
+					<Button onClick={logOut} width={"100%"} className="btn btn-primary">
+						Logga ut
+					</Button>
 				</div>
-				<br/>
+				<br />
 			</Tab>
 		</Tabs>
 	)
--- frontend/src/pages/Profile/ProfileListItem.jsx
+++ frontend/src/pages/Profile/ProfileListItem.jsx
@@ -2,46 +2,50 @@
  * This class is responsible to create the UI for a profile List-item in the list.
  * It's made up by one stripe row with the name and an arrow and redirects the user
  * to the appropriate list. Allows for custom logo to the rightmost of the list item
- * 
+ *
  *
  * @author Tomtato (Group 6)
  * @since 2024-05-02
  * @version 1.0
  */
- import styles from "./ProfileListItem.module.css"
- import { Link } from "react-router-dom"
- import { Dot } from 'react-bootstrap-icons';
+import styles from "./ProfileListItem.module.css"
+import { Link } from "react-router-dom"
+import { Dot } from "react-bootstrap-icons"
+import { ChevronRight } from "react-bootstrap-icons"
 
- 
- export default function ProfileListItem({item, Icon}) {
-    //{console.log(list) 
-    console.log("Test")
+export default function ProfileListItem({ item, Icon }) {
+	//{console.log(list)
+	console.log("Test")
 
-     return (
-
-         
-         <div className={`row align-items-center font-weight-bold ${styles["profile-item-row"]}`} style={{marginRight: 0, marginLeft: 0}}>
-            <div className={`col-2 ${styles["workout-star"]}`}>
-                {/* Handles both Icons and JSX elements */}
-                {typeof Icon === 'string' ? <img src={Icon} alt="Icon" /> : Icon}
-            </div>
-             <Link className={`col align-items-center align-self-center ${styles["profile-text"]} fill`} to={`/profile/${item.list_id}`}>
-                 <div className="listItemDiv">
-                     <div className="listItemInfoDiv"></div>
-                 </div>
-                 <div>
-                    {item.list_name} 
-                    <div>
-                        {item.list_id==-1?  <p>{item.amountOfWorkouts} pass</p>: <p>av {item.author} <Dot/> {item.amountOfWorkouts} aktiviterer</p> }
-                      
-                    </div>
-                    
-                 </div>
-             </Link>
-             <Link className={`col-2 align-items-center align-self-center ${styles["profile-text"]} fill`} to={`/profile/${item.list_id}`}>
-                 <i id="profile-detail-arrow" className="bi-chevron-right h4"/>
-             </Link>
-         </div>
-     )
- }
- 
+	return (
+		<div className={`${styles["profile-item-row"]}`}>
+			<Link to={`/profile/${item.list_id}`}>
+				<div className={"row align-items-center font-weight-bold px-3 py-2"}>
+					<div className={"col-2 "}>
+						{/* Handles both Icons and JSX elements */}
+						{typeof Icon === "string" ? (
+							<img className={`${styles["profile-text"]}`} src={Icon} alt="Icon" />
+						) : (
+							Icon
+						)}
+					</div>
+					<div className={`col-8 ${styles["profile-text"]}`}>
+						<div className="text-left">
+							{item.list_name}
+							{item.list_id == -1 ? (
+								<p className="mb-0">{item.amountOfWorkouts} pass</p>
+							) : (
+								<p className="mb-0">
+									av {item.author} <Dot /> {item.amountOfWorkouts} aktiviterer
+								</p>
+							)}
+						</div>
+					</div>
+					<div className="col-2 pr-0">
+						<ChevronRight size={36} />
+					</div>
+				</div>
+			</Link>
+		</div>
+	)
+}
