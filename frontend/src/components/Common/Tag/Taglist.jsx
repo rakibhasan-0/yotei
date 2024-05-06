b7c0816bce814caa15392b4ce1b201589103b862
Carl Hjortsberg <dv22chg@cs.umu.se>
2024-05-06 16:30:17 +0200

    Fixed indents

--- frontend/src/components/Common/Tag/Taglist.jsx
+++ frontend/src/components/Common/Tag/Taglist.jsx
@@ -1,40 +1,39 @@
 import React from "react"
 import { Pencil } from "react-bootstrap-icons"
 import styles from "./TagList.module.css"
-import Tag from "./Tag"
 import { Link } from "react-router-dom"
-import { ChevronRight } from "react-bootstrap-icons"
 import { Trash } from "react-bootstrap-icons"
 import CheckBox from "../CheckBox/CheckBox"
+import MiniPopup from "../MiniPopup/MiniPopup.jsx"
 
-export default function TagList({ item, text, id, index, tagType }) {
+export default function TagList({ text, id, index, tagType }) {
 	return (
 		<div className={styles["exercise-list-container"]} data-testid="ExerciseListItem">
-		<div className={styles["exercise-list-header"]} style={{ backgroundColor: (index % 2 === 0) ? "var(--red-secondary)" : "var(--background)" }}>
-			{tagType=="added" ? <CheckBox checked={true}/> : <CheckBox checked={false}/>}
+			<div className={styles["exercise-list-header"]} style={{ backgroundColor: (index % 2 === 0) ? "var(--red-secondary)" : "var(--background)" }}>
+				{tagType=="added" ? <CheckBox checked={true}/> : <CheckBox checked={false}/>}
 			
-			<Link to={id} data-testid="ExerciseListItem-link" style={{width: "100%"}}>
-				<div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-					<div style={{display: "flex", alignItems: "center"}}>
-					<div style={{width: '10px'}}></div>
-						<div className={styles["exercise-list-duration"]} data-testid="ExerciseListItem-text" >
-							<p>{text}</p>
+				<Link to={id} data-testid="ExerciseListItem-link" style={{width: "100%"}}>
+					<div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+						<div style={{display: "flex", alignItems: "center"}}>
+							<div style={{width: "10px"}}></div>
+							<div className={styles["exercise-list-duration"]} data-testid="ExerciseListItem-text" >
+								<p>{text}</p>
+							</div>
+						</div>
+						<div className={styles["flex-shrink-0"]} style={{display: "flex", alignItems: "center"}}>
+						
+							<Pencil size="25px" color="#BE3B41"/>
+							<div style={{width: "10px"}}></div>
+							<Trash
+								size="25px"
+								color="var(--red-primary)"
+								style={{cursor: "pointer"}}
+								onClick={<MiniPopup title={"Test"} isOpen={true}></MiniPopup>}
+							/>	
 						</div>
 					</div>
-					<div className={styles["flex-shrink-0"]} style={{display: "flex", alignItems: "center"}}>
-						
-						<Pencil size="25px" color="#BE3B41"/>
-						<div style={{width: '10px'}}></div>
-						<Trash
-                            size="25px"
-                            color="var(--red-primary)"
-                            style={{cursor: "pointer"}}
-                        />
-							
-					</div>
-				</div>
-			</Link>
+				</Link>
+			</div>
 		</div>
-	</div>
-	);
-  }
+	)
+}
