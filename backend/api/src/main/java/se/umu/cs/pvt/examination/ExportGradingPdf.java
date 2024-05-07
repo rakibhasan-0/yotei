9e0b230355ca396efe373a0cec02b1b18046b59b
Marcus Andersson <c21man@cs.umu.se>
2024-05-07 16:30:29 +0200

    Adding techniques

--- backend/api/src/main/java/se/umu/cs/pvt/examination/ExportGradingPdf.java
+++ backend/api/src/main/java/se/umu/cs/pvt/examination/ExportGradingPdf.java
@@ -33,7 +33,8 @@ public class ExportGradingPdf {
     private PDDocument document;
     private int pageWidth, pageHeight;
     private Long grading_id;
-    private String grading_protocol;
+    private String gradingProtocol;
+    private List<ExaminationTechniqueCategory> examinationTechniqueCategories;
     private final int   MAX_NUM_COLUMNS = 6,
                         MAX_NUM_ROWS = 15,
                         TABLE_START_X_POS = 50,
@@ -46,7 +47,8 @@ public class ExportGradingPdf {
         this.numPages = (int)Math.ceil((double)examinees.size() / MAX_NUM_COLUMNS);
         document = new PDDocument();
         this.grading_id = grading_id;
-        this.grading_protocol = Files.readString(Paths.get(System.getProperty("user.dir") + "/frontend/public/grading_protocol_yellow_1.json"));
+        this.gradingProtocol = Files.readString(Paths.get(System.getProperty("user.dir") + "/frontend/public/grading_protocol_yellow_1.json"));
+        this.examinationTechniqueCategories = new ArrayList<>();
     }
 
     public void drawImage(String path, int x, int y, PDPageContentStream contentStream) throws IOException {
@@ -56,7 +58,6 @@ public class ExportGradingPdf {
 
     public Map<String, Object> parseJson(String json) {
         org.springframework.boot.json.JsonParser parser = JsonParserFactory.getJsonParser();
-        Map<String, Object> protocol = parseJson(grading_protocol);
         return parser.parseMap(json);
     }
 
@@ -75,13 +76,11 @@ public class ExportGradingPdf {
         PDPageContentStream contentStream = new PDPageContentStream(document,page);
         contentStream.setStrokingColor(Color.DARK_GRAY);
         contentStream.setLineWidth(1);
-        Map<String, Object> protocol = parseJson(grading_protocol);
+        Map<String, Object> protocol = parseJson(gradingProtocol);
         Map<String, Object> gradingProtocolObj = (Map<String, Object>) protocol.get("grading_protocol");
 
         String code = (String) gradingProtocolObj.get("code");
         String color = (String) gradingProtocolObj.get("color");
-        System.out.println("Code: " + code);
-        System.out.println("Color: " + color);
 
         createHeader(code + " " + color, "test, test2, test3", "2024-05-07", contentStream);
 
@@ -113,6 +112,30 @@ public class ExportGradingPdf {
             initY -=CELL_HEIGHT;
         }
 
+        for(int i = 0; i < examinationTechniqueCategories.size(); i++) {
+            contentStream.addRect(initX, initY, CELL_WIDTH*7+30, -CELL_HEIGHT);
+
+            contentStream.beginText();
+            contentStream.newLineAtOffset(initX+10,initY-CELL_HEIGHT+10);
+            contentStream.setFont(font,10);
+            contentStream.showText(examinationTechniqueCategories.get(i).getCategoryName());
+            contentStream.endText();
+
+            for(int j = 0; j < examinationTechniqueCategories.get(i).getTechniques().size(); j++) {
+                contentStream.addRect(initX, initY, CELL_WIDTH, -CELL_HEIGHT);
+
+                contentStream.beginText();
+                contentStream.newLineAtOffset(initX+10,initY-CELL_HEIGHT+10);
+                contentStream.setFont(font,10);
+                contentStream.showText(examinationTechniqueCategories.get(i).getTechniques().get(j).toString());
+                contentStream.endText();
+
+                initX+=CELL_WIDTH;
+            }
+
+            initX = TABLE_START_X_POS;
+            initY -=CELL_HEIGHT;
+        }
 
         contentStream.stroke();
         contentStream.close();
@@ -139,8 +162,20 @@ public class ExportGradingPdf {
     }
 
     public void generate() throws IOException {
-        parseJson(grading_protocol);
-        
+        Map<String, Object> parsedProtocol = parseJson(gradingProtocol);
+
+        List<Map<String, Object>> categories = (List<Map<String, Object>>)parsedProtocol.get("categories");
+
+        for(int i = 0; i < categories.size(); i++) {
+            ExaminationTechniqueCategory category = new ExaminationTechniqueCategory(categories.get(i).get("category_name").toString());
+            List<Map<String, Object>> techniques = (List<Map<String, Object>>)categories.get(i).get("techniques");
+
+            for(int j = 0; j < techniques.size(); j++) {
+                category.addTechnique(techniques.get(j).get("text").toString());
+            }
+            examinationTechniqueCategories.add(category);
+        }
+
         for(int i = 0; i < numPages; i++) {
             this.createTable(i);
         }
