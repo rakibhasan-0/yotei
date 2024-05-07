04f956ba7661e8ddc0532868b1cec175d5e41892
id20ehm <id20ehm@cs.umu.se>
2024-05-07 09:43:09 +0200

    runs system tests only on one device in pipeline to speed up the process

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -38,10 +38,28 @@ export default defineConfig({
 
 	/* Configure projects for major browsers */
 	projects: [
-		{
-			name: "Mobile Chrome",
-			use: { ...devices["Pixel 5"] },
-		}
+    /* Test against desktop browsers */
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+    /* Test against mobile viewports. */
+    {
+      name: 'MobileChrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
 	],
 
 	/* Start frontend & backend before tests */
