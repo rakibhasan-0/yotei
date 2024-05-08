5d33c616dd08b18e01cf52f5ad861d3ddfee9d56
Oliver Lundqvist <dv22olt@cs.umu.se>
2024-05-08 13:00:39 +0000

    Removed unused import

--- backend/api/src/main/java/se/umu/cs/pvt/permission/Permission.java
+++ backend/api/src/main/java/se/umu/cs/pvt/permission/Permission.java
@@ -2,7 +2,6 @@ package se.umu.cs.pvt.permission;
 
 import java.io.Serializable;
 
-import javax.naming.InvalidNameException;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
