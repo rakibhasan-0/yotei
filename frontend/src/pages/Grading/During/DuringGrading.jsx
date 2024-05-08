f192d58ae93c4c271c481e2ade6f8a18ea45ead3
iceproo <isak@elan.nu>
2024-05-08 09:43:40 +0200

    Fixed linting

--- frontend/src/pages/Grading/During/DuringGrading.jsx
+++ frontend/src/pages/Grading/During/DuringGrading.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useContext } from "react"
-import { useParams, useNavigate } from "react-router-dom"
-import { AccountContext } from "../../../context"
-import TechniqueInfoPanel from "../../../components/Grading/PerformGrading/TechniqueInfoPanel";
+import React, { useState } from "react"
+
+import TechniqueInfoPanel from "../../../components/Grading/PerformGrading/TechniqueInfoPanel"
 
 // Temp
 import ProtocolYellow from "./yellowProtocolTemp.json"
@@ -20,28 +19,28 @@ import ProtocolYellow from "./yellowProtocolTemp.json"
  */
 function getTechniqueNameList(gradingProtocolJSON) {
 	// Store data in an array for chronological traversal
-	const chronologicalData = [];
+	const chronologicalData = []
 
 	// Traverse the categories and techniques to store in chronological order
 	gradingProtocolJSON.categories.forEach(category => {
-		const categoryName = category.category_name;
-		const techniques = category.techniques;
+		const categoryName = category.category_name
+		const techniques = category.techniques
 
 		techniques.forEach((technique, index) => {
-			const nextTechnique = index < techniques.length - 1 ? techniques[index + 1] : null;
-			chronologicalData.push({categoryName, technique, nextTechnique});
-		});
-	});
+			const nextTechnique = index < techniques.length - 1 ? techniques[index + 1] : null
+			chronologicalData.push({ categoryName, technique, nextTechnique })
+		})
+	})
 
 	// Remove null to not get errors when printing
 	chronologicalData.forEach((element, index) => {
 		// Check if element's nextTechnique is null and index is not the last element
-		if(element.nextTechnique === null && index < chronologicalData.length - 1){
-			element.nextTechnique = chronologicalData[index+1].technique
-		} 
+		if (element.nextTechnique === null && index < chronologicalData.length - 1) {
+			element.nextTechnique = chronologicalData[index + 1].technique
+		}
 		// Check if element's nextTechnique is null and index is the last element
-		else if(element.nextTechnique === null && index === chronologicalData.length - 1) {
-			element.nextTechnique = '---'
+		else if (element.nextTechnique === null && index === chronologicalData.length - 1) {
+			element.nextTechnique = "---"
 		}
 	})
 	return chronologicalData
@@ -58,28 +57,28 @@ function getTechniqueNameList(gradingProtocolJSON) {
  * @author Team Apelsin (2024-05-07)
  */
 function getCategoryIndices(dataArray) {
-	const res = [];
-	const seenCategories = new Set(); // To keep track of seen categories
-  
+	const res = []
+	const seenCategories = new Set() // To keep track of seen categories
+
 	dataArray.forEach((element, index) => {
-	  const category = element.categoryName;
-	  if (category && !seenCategories.has(category)) {
-		res.push({ category, index });
-		seenCategories.add(category); // Add category to the set
-	  }
-	});
-   
-	return res;
-  }
+		const category = element.categoryName
+		if (category && !seenCategories.has(category)) {
+			res.push({ category, index })
+			seenCategories.add(category) // Add category to the set
+		}
+	})
+
+	return res
+}
 
 export default function DuringGrading() {
-	const navigate = useNavigate()
-	const { examinationID } = useParams()
-	const { token } = useContext(AccountContext)
-	const [currentIndex, setCurrentIndex] = useState(0);
+	//const navigate = useNavigate()
+	//const { examinationID } = useParams()
+	//const { token } = useContext(AccountContext)
+	const [currentIndex, setCurrentIndex] = useState(0)
 
 	const goToNextTechnique = () => {
-		setCurrentIndex(currentIndex + 1);
+		setCurrentIndex(currentIndex + 1)
 	}
 	// TODO: Loads everytime the button is pressed. Should only happen once at start. useEffect?
 	const techniqueNameList = getTechniqueNameList(ProtocolYellow)
@@ -89,11 +88,11 @@ export default function DuringGrading() {
 
 	return (
 		<div>
-			<TechniqueInfoPanel 
+			<TechniqueInfoPanel
 				categoryTitle=""
-				currentTechniqueTitle = {techniqueNameList[currentIndex].technique.text}
-				nextTechniqueTitle = {techniqueNameList[currentIndex].nextTechnique.text}
-				mainCategoryTitle = {techniqueNameList[currentIndex].categoryName}></TechniqueInfoPanel>
+				currentTechniqueTitle={techniqueNameList[currentIndex].technique.text}
+				nextTechniqueTitle={techniqueNameList[currentIndex].nextTechnique.text}
+				mainCategoryTitle={techniqueNameList[currentIndex].categoryName}></TechniqueInfoPanel>
 			<button onClick={goToNextTechnique}>Next</button>
 		</div>
 	)
