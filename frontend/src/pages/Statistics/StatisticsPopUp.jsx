12faf7e7f4c042e4e9b35c8b77473c776bd1a5e7
Gabriel Morberg <c21gmg@cs.umu.se>
2024-05-08 14:26:46 +0200

    Linter issues

--- frontend/src/pages/Statistics/StatisticsPopUp.jsx
+++ frontend/src/pages/Statistics/StatisticsPopUp.jsx
@@ -1,10 +1,10 @@
-	import React, { useState,useEffect } from "react"
-	import style from "./StatisticsButton.module.css"
-	import Popup from "../../components/Common/Popup/Popup"
-	import StarRatings from "react-star-ratings"
-	import BeltColorChart from "../../components/Common/BeltColorChart/BeltColorChart"
+import React, { useState,useEffect } from "react"
+import style from "./StatisticsButton.module.css"
+import Popup from "../../components/Common/Popup/Popup"
+import StarRatings from "react-star-ratings"
+import BeltColorChart from "../../components/Common/BeltColorChart/BeltColorChart"
 
-	/**
+/**
 	 * Statistics pop-up component.
 	 * @input groupActivities - The activities to be displayed in the pop-up.
 	 * @input dates - The date interval of the sessions
@@ -16,61 +16,61 @@
 	 * @since 2024-05-08
 	 * @version 1.0
 	 */
-	export default function StatisticsPopUp({groupActivities,dates,averageRating,numberOfSessions}) {
-		const startDate = dates.from
-		const endDate = dates.to
-		const [beltColorsData, setBeltColorsData] = useState({})
+export default function StatisticsPopUp({groupActivities,dates,averageRating,numberOfSessions}) {
+	const startDate = dates.from
+	const endDate = dates.to
+	const [beltColorsData, setBeltColorsData] = useState({})
 
-		useEffect(() => {
-			// Function to calculate the amount of techniques by belt color
-			const calculateBeltColorsData = () => {
-				const colorsCount = {}
-				groupActivities.forEach(activity => {
-					if(activity.type == "technique") {
-						activity.beltColors.forEach(beltColor => {
-							const color = beltColor.belt_name
-							colorsCount[color] = (colorsCount[color] || 0) + activity.count
-						})
-					}
-				})
-				setBeltColorsData(colorsCount)
-			}
-		
-			// Call the function to calculate belt colors data when groupActivities change
-			calculateBeltColorsData()
-		}, [groupActivities])
-
-
-		const [showPopup, setShowPopup] = useState(false)
-
-		const togglePopup = () => {
-			setShowPopup(!showPopup)
-
+	useEffect(() => {
+		// Function to calculate the amount of techniques by belt color
+		const calculateBeltColorsData = () => {
+			const colorsCount = {}
+			groupActivities.forEach(activity => {
+				if(activity.type == "technique") {
+					activity.beltColors.forEach(beltColor => {
+						const color = beltColor.belt_name
+						colorsCount[color] = (colorsCount[color] || 0) + activity.count
+					})
+				}
+			})
+			setBeltColorsData(colorsCount)
 		}
-
-		return (
-			<div className={style.statisticsButtonContainer}>
-				<button onClick={togglePopup}>
-					<img src="/Statistics.svg" />
-				</button>
-
-				<Popup title={"Sammanställning av tillfällen"} id="statistics-popup" isOpen={showPopup} setIsOpen={setShowPopup}>
-					<StarRatings rating={averageRating} starRatedColor="#ffcc00" numberOfStars={5} name='rating' starDimension="50px" starSpacing="4px" />
-					<p style = {{color: "#b9b0b0"}}>
-						Genomsnittligt betyg {averageRating}/5 för {numberOfSessions} tillfällen från {<br />} datum {startDate} till {endDate}
-					</p>
-					<p style = {{ fontSize: "25px" }}>
-						Bält-tekniker
-					</p>
-					<div style={{ overflowY: "auto" }}>
-						<BeltColorChart beltColorsData={beltColorsData} /> 
-					</div>
-					
-				</Popup>
+		
+		// Call the function to calculate belt colors data when groupActivities change
+		calculateBeltColorsData()
+	}, [groupActivities])
 
 
-			</div>
+	const [showPopup, setShowPopup] = useState(false)
 
-		)
+	const togglePopup = () => {
+		setShowPopup(!showPopup)
+
 	}
+
+	return (
+		<div className={style.statisticsButtonContainer}>
+			<button onClick={togglePopup}>
+				<img src="/Statistics.svg" />
+			</button>
+
+			<Popup title={"Sammanställning av tillfällen"} id="statistics-popup" isOpen={showPopup} setIsOpen={setShowPopup}>
+				<StarRatings rating={averageRating} starRatedColor="#ffcc00" numberOfStars={5} name='rating' starDimension="50px" starSpacing="4px" />
+				<p style = {{color: "#b9b0b0"}}>
+						Genomsnittligt betyg {averageRating}/5 för {numberOfSessions} tillfällen från {<br />} datum {startDate} till {endDate}
+				</p>
+				<p style = {{ fontSize: "25px" }}>
+						Bält-tekniker
+				</p>
+				<div style={{ overflowY: "auto" }}>
+					<BeltColorChart beltColorsData={beltColorsData} /> 
+				</div>
+					
+			</Popup>
+
+
+		</div>
+
+	)
+}
 
