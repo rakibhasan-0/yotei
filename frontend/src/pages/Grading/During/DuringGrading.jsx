f6efd92a4d268a572568277254ae0d092b75d067
iceproo <isak@elan.nu>
2024-05-08 09:11:08 +0200

    Made some cleanUp on code and added comments to functions (getTechniqueNameList and getCategoryIndicies)

--- frontend/src/pages/Grading/During/DuringGrading.jsx
+++ frontend/src/pages/Grading/During/DuringGrading.jsx
@@ -1,23 +1,29 @@
 import React, { useState, useEffect, useContext } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import { AccountContext } from "../../../context"
+import TechniqueInfoPanel from "../../../components/Grading/PerformGrading/TechniqueInfoPanel";
 
 // Temp
 import ProtocolYellow from "./yellowProtocolTemp.json"
-import TechniqueInfoPanel from "../../../components/Grading/PerformGrading/TechniqueInfoPanel";
-
-/* Achive:
--- Take out 
-
-*/
 
 
-function getTechniqueNameList() {
+/**
+ * Get all techniques from grading protocol and 
+ * returns array that could be iterated in order
+ * 
+ * @param gradingProtocolJSON - A JSON object that contains data for a specific grading
+ * 
+ * @returns Array with techniques that is in order. 
+ * 			Each item contains {categoryName, current_technique and next_technique } for a specific JSON-file  
+ * 
+ * @author Team Apelsin (2024-05-07)
+ */
+function getTechniqueNameList(gradingProtocolJSON) {
 	// Store data in an array for chronological traversal
 	const chronologicalData = [];
 
 	// Traverse the categories and techniques to store in chronological order
-	ProtocolYellow.categories.forEach(category => {
+	gradingProtocolJSON.categories.forEach(category => {
 		const categoryName = category.category_name;
 		const techniques = category.techniques;
 
@@ -41,6 +47,16 @@ function getTechniqueNameList() {
 	return chronologicalData
 }
 
+/**
+ * Gets a map from categoryIndex to index in dataArray.
+ * 
+ * @param dataArray - An array with chronologicalData from a given JSON-file
+ * 
+ * @returns A map that takes a category name and maps it to an index in the dataArray
+ * 			Could be used for navigation to correct technique 
+ * 
+ * @author Team Apelsin (2024-05-07)
+ */
 function getCategoryIndices(dataArray) {
 	const res = [];
 	const seenCategories = new Set(); // To keep track of seen categories
@@ -52,7 +68,7 @@ function getCategoryIndices(dataArray) {
 		seenCategories.add(category); // Add category to the set
 	  }
 	});
-  
+   
 	return res;
   }
 
@@ -65,8 +81,8 @@ export default function DuringGrading() {
 	const goToNextTechnique = () => {
 		setCurrentIndex(currentIndex + 1);
 	}
-	// TODO: Loads everytime the button is pressed. Should only happen once at start.
-	const techniqueNameList = getTechniqueNameList()
+	// TODO: Loads everytime the button is pressed. Should only happen once at start. useEffect?
+	const techniqueNameList = getTechniqueNameList(ProtocolYellow)
 	const categoryIndexMap = getCategoryIndices(techniqueNameList)
 
 	console.log(categoryIndexMap)
