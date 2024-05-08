0c2aa1e345e88f0a59d160f6a5230a1c80dca64e
Linus Wallenborg <c21lwg@cs.umu.se>
2024-05-08 17:01:32 +0200

    Added solution - unimplemented

--- frontend/src/components/Common/BeltColorChart/BeltColorChart.jsx
+++ frontend/src/components/Common/BeltColorChart/BeltColorChart.jsx
@@ -14,7 +14,6 @@ import { Bar } from "react-chartjs-2"
 
 ChartJS.register(BarElement,CategoryScale, LinearScale, Tooltip,Title)
 
-
 /** 
  * BeltColorChart is a component that displays a bar chart of belt colors.
  * @input beltColorsData: An object containing belt colors as keys and their counts as values
@@ -66,9 +65,33 @@ export default function BeltColorChart({ beltColorsData }) {
 
 	}
 
+	const verticalLinePlugin = {
+		id: "verticalLine",
+		afterDraw: (chart) => {
+			const ctx = chart.ctx
+			chart.data.datasets.forEach((dataset, datasetIndex) => {
+				const meta = chart.getDatasetMeta(datasetIndex)
+				meta.data.forEach((bar, index) => {
+			  		const model = bar.tooltipPosition()
+					console.log(dataset)
+					// Draw a yellow vertical line on a black bar
+					if (dataset.backgroundColor[index] === "black") {
+						ctx.save()
+						ctx.strokeStyle = "yellow" // Set line color
+						ctx.lineWidth = 15 // Set line width
+						ctx.beginPath()
+						ctx.moveTo(model.x, bar.y) // Start at the top of the bar
+						ctx.lineTo(model.x, bar.base) // End at the bottom
+						ctx.stroke()
+						ctx.restore()
+					}
+				})
+			})
+		},
+	}
 	return (
 		<div className = {styles.chart}>
-			<Bar data={chartData} options={chartOptions} />
+			<Bar data={chartData} options={chartOptions} plugins={[verticalLinePlugin]}/>
 		</div>
 	)
 }
