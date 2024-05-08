5bae72ce33f62c59e7e559cf3187c21bf6fa76f2
Lars Jonsson <ens21ljn@cs.umu.se>
2024-05-08 11:49:56 +0200

    Added functionality/logic to commentbutton

--- frontend/src/components/Grading/PerformGrading/ExamineeBox.jsx
+++ frontend/src/components/Grading/PerformGrading/ExamineeBox.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react"
 import ExamineeButton from "./ExamineeButton"
 import CommentButton from "./CommentButton"
 import styles from "./ExamineeBox.module.css"
+import Popup from "../../Common/Popup/Popup"
+import TextArea from "../../Common/TextArea/TextArea"
+import Button from "../../Common/Button/Button"
+import ConfirmPopup from "../../Common/ConfirmPopup/ConfirmPopup"
 
 /**
  * this is a box containing the Examinee's information
@@ -22,20 +26,75 @@ import styles from "./ExamineeBox.module.css"
  * @version 1.0 
  */
 
-export default function ExamineeBox({ id, examineeName, onClickComment}) {
+export default function ExamineeBox({ id, examineeName}) {
 	const [selectedButton, setSelectedButton] = useState(null)
+	const [showDiscardComment, setShowDiscardComment] = useState(false)
+	const [isAddingComment, setAddComment] = useState(false)
+	const [commentText, setCommentText] = useState()
+	const [commentError, setCommentError] = useState()
+
 
 	const handleButtonClick = (buttonId) => {
 		setSelectedButton(prev => prev === buttonId ? null : buttonId)
 		console.log(`Pressed ${buttonId} button`)
 	}
+
+	const onDiscardComment = async () => {
+		setCommentText("")
+		setAddComment(false)
+	}
+	/**
+	 * Closes the addComment popup, unless it contains text in which case it shows
+	 * a warning that the user input will be lost.
+	 * @param {bool} show Whether the add comment popup should be shown.
+	 */
+	const toggleAddComment = async (show) => {
+		if (!show && commentText && commentText.trim().length > 0) {
+			setShowDiscardComment(true)
+			return
+		}
+		setAddComment(show)
+	}
+
+	/**
+	 * Handles the addition of a comment by sending a POST request to the API.
+	 * Validates the comment text and displays an error if it is empty.
+	 * Clears the comment text and sets addComment to false after a successful addition.
+	 * Fetches the updated comments by calling fetchComments.
+	 */
+	const onAddComment = async () => {
+		if (!commentText || !commentText.trim() || commentText.length === 0) {
+			setCommentError("Kommentaren får inte vara tom")
+			return
+		}
+		console.log("API ANROP SOM INTE FINNS ÄNNU. Detta skulle läggas in: " + commentText)
+		/* API ANROP HÄR...
+		const response = await fetch(`/api/comment/exercise/add?id=${ex_id}`, {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+				token,
+				userId
+			},
+			body: JSON.stringify({
+				commentText
+			})
+		})
+		if (response.status != 201) {
+			setErrorToast("Ett fel uppstod när kommentaren skulle läggas till")
+			return
+		}
+		*/
+		onDiscardComment()
+	}
+
 	return (
 		<div id={id} className={styles.examineeContainer} style={{backgroundColor: "#FFFFFF"}}>
 			<fieldset className={styles.examineeFieldset}>
 				<div className={styles.examineeName}>
 					<p id="ExamineeName" style={{height:"52px", margin:"0"}}>{examineeName}</p>
 				</div>
-				<CommentButton onClick={onClickComment} className={styles.commentButtonContainer}/>
+				<CommentButton onClick={() => setAddComment(true)} className={styles.commentButtonContainer}/>
 				<div className={styles.buttonContainer}>
 					<ExamineeButton
 						id="pass-button"
@@ -56,6 +115,28 @@ export default function ExamineeBox({ id, examineeName, onClickComment}) {
 						<p>U</p>
 					</ExamineeButton>
 				</div>
+				<Popup 
+				id={"examinee-comment-popup"} 
+				title={"Lägg till kommentar till: " + examineeName} 
+				isOpen={isAddingComment} 
+				setIsOpen={toggleAddComment}
+				onClose={() => setCommentError(false)}
+				style={{ overflow: "hidden", overflowY: "hidden", maxHeight: "85vh", height: "unset"}}
+			>
+				<TextArea 
+					autoFocus={true}
+					onInput={e => {setCommentText(e.target.value); setCommentError(false)}}
+					errorMessage={commentError}
+				/>
+				<Button onClick={onAddComment}>Lägg till</Button>
+			</Popup>
+			<ConfirmPopup
+				popupText={"Är du säker på att du vill ta bort kommentarsutkastet?"}
+				showPopup={showDiscardComment}
+				onClick={() => onDiscardComment()}
+				setShowPopup={() => setShowDiscardComment(false)}
+				zIndex={200} // Above the comment popup.
+			/>
 			</fieldset>
 		</div>
 	)
--- frontend/src/components/Grading/PerformGrading/TechniqueInfoPanel.jsx
+++ frontend/src/components/Grading/PerformGrading/TechniqueInfoPanel.jsx
@@ -1,6 +1,10 @@
-import {React} from "react"
+import {React,useState} from "react"
 import CommentButton from "../PerformGrading/CommentButton"
 import styles from "./TechniqueInfoPanel.module.css"
+import Popup from "../../Common/Popup/Popup"
+import TextArea from "../../Common/TextArea/TextArea"
+import Button from "../../Common/Button/Button"
+import ConfirmPopup from "../../Common/ConfirmPopup/ConfirmPopup"
 
 /**
  * This panel  present the main categori, the current technique and the next
@@ -38,33 +42,112 @@ export default function TechniqueInfoPanel( {
 	categoryTitle = "Test Kategori",
 	currentTechniqueTitle = "1. Grepp i två handleder framifrån och svingslag Frigöring – Ju morote jodan uke",
 	nextTechniqueTitle = " 2. Stryptag framifrån och svingslag, backhand Frigöring – Ju morote jodan uke, ude osae, ude osae gatame",
-	mainCategoryTitle = "Huvudkategori",
-	onButtonClicked
+	mainCategoryTitle = "Huvudkategori"
+	//onButtonClicked
 }) {
+
+	const [showDiscardComment, setShowDiscardComment] = useState(false)
+	const [isAddingComment, setAddComment] = useState(false)
+	const [commentText, setCommentText] = useState()
+	const [commentError, setCommentError] = useState()
+
+
+	const onDiscardComment = async () => {
+		setCommentText("")
+		setAddComment(false)
+	}
+	/**
+	 * Closes the addComment popup, unless it contains text in which case it shows
+	 * a warning that the user input will be lost.
+	 * @param {bool} show Whether the add comment popup should be shown.
+	 */
+	const toggleAddComment = async (show) => {
+		if (!show && commentText && commentText.trim().length > 0) {
+			setShowDiscardComment(true)
+			return
+		}
+		setAddComment(show)
+	}
+
+	/**
+	 * Handles the addition of a comment by sending a POST request to the API.
+	 * Validates the comment text and displays an error if it is empty.
+	 * Clears the comment text and sets addComment to false after a successful addition.
+	 * Fetches the updated comments by calling fetchComments.
+	 */
+	const onAddComment = async () => {
+		if (!commentText || !commentText.trim() || commentText.length === 0) {
+			setCommentError("Kommentaren får inte vara tom")
+			return
+		}
+		console.log("API ANROP SOM INTE FINNS ÄNNU. Detta skulle läggas in: " + commentText)
+		/* API ANROP HÄR...
+		const response = await fetch(`/api/comment/exercise/add?id=${ex_id}`, {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+				token,
+				userId
+			},
+			body: JSON.stringify({
+				commentText
+			})
+		})
+		if (response.status != 201) {
+			setErrorToast("Ett fel uppstod när kommentaren skulle läggas till")
+			return
+		}
+		*/
+		onDiscardComment()
+	}
+
 
 	return (
 		
 		<div style={styles}>
 			<fieldset role="fieldsetBelt" style={{ height: "30px", width: "100%", marginBottom: "10px", backgroundColor: beltColor}}>
 				<div>
-					<h2 role="mainCategoryTitle">{mainCategoryTitle}</h2>
+					<h2 className={styles.mainCategoryTitle} role="mainCategoryTitle">{mainCategoryTitle}</h2>
 				</div>
 			</fieldset>
 			<fieldset style={{ height: "auto", width: "100%", marginBottom: "5px" }}>
 				<div>
-					<h3 role="categoryTitle">{categoryTitle}</h3>
+					<h3 className={styles.categoryTitle} role="categoryTitle">{categoryTitle}</h3>
 				</div>
 				<div>
-					<h1 role="currentTechniqueTitle">{currentTechniqueTitle}</h1>
+					<h1 className={styles.currentTechnique} role="currentTechniqueTitle">{currentTechniqueTitle}</h1>
 				</div>
 				<div style={{width: "70%", float: "left"}}>
-					<h3 role="nextTechniqueTitle"><b>Nästa:</b>{nextTechniqueTitle}</h3>
+					<h3 className={styles.nextTechnique} role="nextTechniqueTitle"><b>Nästa:</b>{nextTechniqueTitle}</h3>
 				</div>
 				<div style={{ display: "flex", justifyContent: "flex-end"}}>
-					<CommentButton onClick={onButtonClicked} />
+					<CommentButton onClick={() => setAddComment(true)} />
 				</div>
 			</fieldset>
+			<Popup 
+				id={"group-comment-popup"} 
+				title={"Lägg till grupp kommentar"} 
+				isOpen={isAddingComment} 
+				setIsOpen={toggleAddComment}
+				onClose={() => setCommentError(false)}
+				style={{ overflow: "hidden", overflowY: "hidden", maxHeight: "85vh", height: "unset"}}
+			>
+				<TextArea 
+					autoFocus={true}
+					onInput={e => {setCommentText(e.target.value); setCommentError(false)}}
+					errorMessage={commentError}
+				/>
+				<Button onClick={onAddComment}>Lägg till</Button>
+			</Popup>
+			<ConfirmPopup
+				popupText={"Är du säker på att du vill ta bort kommentarsutkastet?"}
+				showPopup={showDiscardComment}
+				onClick={() => onDiscardComment()}
+				setShowPopup={() => setShowDiscardComment(false)}
+				zIndex={200} // Above the comment popup.
+			/>
 		</div>
+		
 	)
 
 }
