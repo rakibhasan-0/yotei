2d8740d4651eba081b17ad0ee1de90c25ed6cc4c
id20ehm <id20ehm@cs.umu.se>
2024-05-08 10:20:15 +0200

    updates documentation for canary test

--- frontend/sys-test/canaryTest.spec.ts
+++ frontend/sys-test/canaryTest.spec.ts
@@ -1,7 +1,15 @@
 import { test, expect } from './fixtures';
 
-test.describe('ST-1 Login', () => {
-  test('1. Canary test', async ({ page }) => {
+/**
+ * A canary test for testing the system test environment. Verifies that fixtures for login as an admin works.
+ * Can be deleted later on when other tests are implemented. 
+ *
+ *  @author Team Mango (Group 4)
+ *  @since 2024-05-8
+ *  @version 2.0
+ */
+test.describe('ST-1 Canary Test', () => {
+  test('1. Planering title should be visible if the fixtures work', async ({ page }) => {
     await page.locator('center').getByRole('heading', { name: 'Planering' }).click();
   });
 });
