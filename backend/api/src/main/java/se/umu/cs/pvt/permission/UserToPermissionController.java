4ea6f0b635364386f47e3b21c9a7c8a2a0f714dd
Daniel Hylander <ens21dhr@cs.umu.se>
2024-05-13 14:20:16 +0200

    Created method for deleting user permissions

--- backend/api/src/main/java/se/umu/cs/pvt/permission/UserToPermissionController.java
+++ backend/api/src/main/java/se/umu/cs/pvt/permission/UserToPermissionController.java
@@ -1,17 +1,18 @@
 package se.umu.cs.pvt.permission;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -66,23 +67,50 @@ public class UserToPermissionController {
     @PostMapping("/{user_id}/permission")
     public ResponseEntity<UserToPermission> postUserPermissionPair(
         @RequestBody Permission permission, @PathVariable("user_id") Long userId) {
-            List<Permission> userPermissions = repository.findAllByUserId(userId);
-            if (userPermissions == null) {
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            
-            } else {
-                for (Permission userPermission : userPermissions) {
-                    if (userPermission.equals(permission)) {
-                        return new ResponseEntity<>(HttpStatus.OK);
-                    }
+        List<Permission> userPermissions = repository.findAllByUserId(userId);
+        if (userPermissions == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        
+        } else {
+            for (Permission userPermission : userPermissions) {
+                if (userPermission.equals(permission)) {
+                    return new ResponseEntity<>(HttpStatus.OK);
                 }
             }
-            
-            UserToPermission userPermissionToAdd = new UserToPermission(
-                userId ,permission.getPermissionId());
-
-            repository.save(userPermissionToAdd);
-
-            return new ResponseEntity<>(userPermissionToAdd, HttpStatus.OK);
         }
+        
+        UserToPermission userPermissionToAdd = new UserToPermission(
+            userId ,permission.getPermissionId());
+
+        repository.save(userPermissionToAdd);
+
+        return new ResponseEntity<>(userPermissionToAdd, HttpStatus.OK);
+    }
+
+    /**
+     * Deletes a user permission by user id and permission id.
+     *
+     * @param userId the id of the user with the permission to delete.
+     * @param roleId the id of the permission to delete.
+     * @return response, 200 OK on success.
+     */
+    @DeleteMapping("")
+    public ResponseEntity<Object> deleteUserPermissionPair(
+        @RequestBody Map<String, String> userPermissionToDelete) {
+        Long userId = Long.parseLong(userPermissionToDelete.get("user_id"));
+        Long permissionId = Long.parseLong(userPermissionToDelete.get("permission_id"));
+        UserToPermission userToPermission = repository.findByUserIdAndPermissionId(
+            userId, permissionId);
+
+        if (userToPermission == null) {
+            return new ResponseEntity<>(
+                "User with ID: " + userId +  
+                "does not have the permission with ID:" + permissionId + 
+                ".", HttpStatus.BAD_REQUEST);
+        }
+
+        repository.deleteByUserIdAndPermissionId(userId, permissionId);
+        
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
