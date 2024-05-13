d574fc2fa34f79bc2a03c0c5727fe72ee72e44a2
Daniel Hylander <ens21dhr@cs.umu.se>
2024-05-13 13:25:39 +0200

    Wokring on method for posting new userpermission

--- backend/api/src/main/java/se/umu/cs/pvt/permission/UserToPermissionController.java
+++ backend/api/src/main/java/se/umu/cs/pvt/permission/UserToPermissionController.java
@@ -8,7 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -41,7 +44,8 @@ public class UserToPermissionController {
      * @return HTTP status code and body, where body could be a message or the permissions.
      */
     @GetMapping("/{user_id}")
-	public ResponseEntity<List<Permission>> getUserPermissions(@PathVariable("user_id") Long userId) {
+	public ResponseEntity<List<Permission>> getUserPermissions(
+        @PathVariable("user_id") Long userId) {
 		List<Permission> result = repository.findAllByUserId(userId);
         
         if (result == null) {
@@ -53,4 +57,21 @@ public class UserToPermissionController {
 
         return new ResponseEntity<>(result, HttpStatus.OK);
 	}
+
+    /**
+     * (POST) Method for creating/adding new permissions for a user.
+     * @param roleToAdd Body with permission info to be added to user.
+     * @return A response with either the newly created user permission or an error message.
+     */
+    @PostMapping("/{user_id}/permission")
+    public ResponseEntity<UserToPermission> postUserPermissionPair(
+        @RequestBody Permission permission, @PathVariable("user_id") Long userId) {
+            if (repository.findAllByUserId(userId) == null) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
+            UserToPermission userPermissionToAdd = new UserToPermission(permission.get)
+
+            repository.save()
+        }
 }
