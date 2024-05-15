fdbe2f45cf7e82a00904e24d1c3cd191166ebfd6
Lee Lannerblad <ens19lld@cs.umu.se>
2024-05-15 16:56:39 +0200

    adds id as attribute to be able to use model when updating an activitylist

--- backend/api/src/main/java/se/umu/cs/pvt/activitylist/AddActivityListRequest.java
+++ backend/api/src/main/java/se/umu/cs/pvt/activitylist/AddActivityListRequest.java
@@ -3,6 +3,7 @@ package se.umu.cs.pvt.activitylist;
 import java.util.Set;
 
 public class AddActivityListRequest {
+    private Long id;
     private String name;
     private String description;
     private Boolean hidden;
@@ -11,6 +12,14 @@ public class AddActivityListRequest {
 
     public boolean hasNullFields() {
         return name == null || hidden == null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
