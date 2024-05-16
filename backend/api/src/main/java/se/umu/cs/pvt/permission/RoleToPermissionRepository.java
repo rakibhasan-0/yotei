dc690144b1d10f0b8a94047cb571f9541b3b1ecb
Daniel Hylander <ens21dhr@cs.umu.se>
2024-05-16 14:16:35 +0200

    Changed the retrun value of the roletopermissions repository functions to role permission pairs

--- backend/api/src/main/java/se/umu/cs/pvt/permission/RoleToPermissionRepository.java
+++ backend/api/src/main/java/se/umu/cs/pvt/permission/RoleToPermissionRepository.java
@@ -8,16 +8,16 @@ import java.util.List;
 /**
  * Interface for role_to_permission table
  *
- * @author Team Mango (Grupp 4) - 2024-05-13
+ * @author Team Mango (Grupp 4) - 2024-05-16
  */
 @Repository
 public interface RoleToPermissionRepository extends JpaRepository<RoleToPermission, Long> {
     /**
-     * Find all permissions belonging to a single role
+     * Find all role permission pairs belonging to a single role
      * @param roleId The id of the role
-     * @return All permissions of said role
+     * @return All role permission pairs of said role
      */
-    List<Permission> findAllByRoleId(Long roleId);
+    List<RoleToPermission> findAllByRoleId(Long roleId);
 
     /**
      * Remove a permission from a role.
@@ -27,10 +27,10 @@ public interface RoleToPermissionRepository extends JpaRepository<RoleToPermissi
     void deleteByRoleIdAndPermissionId(Long roleId, Long permissionId);
 
     /**
-     * Checks if a pair of roleid and permissionid exists
-     * @param roleId
-     * @param permissionId
-     * @return
+     * Finds a role permission pair from the roleid and permissionid.
+     * @param roleId The id of the role
+     * @param permissionId The id of the permission
+     * @return The role permission pair.
      */
-    Permission findByRoleIdAndPermissionId(Long roleId, Long permissionId);
+    RoleToPermission findByRoleIdAndPermissionId(Long roleId, Long permissionId);
 }
