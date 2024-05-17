e15492b2fdeb8446b272723769204772059bc716
Victor Østergaard Månsson <c17vsn@cs.umu.se>
2024-05-17 09:43:32 +0200

    Small changes to playwright test

--- frontend/sys-test/addtagpopup.spec.ts
+++ frontend/sys-test/addtagpopup.spec.ts
@@ -12,23 +12,23 @@ test.describe("AddTagPopup tests", () => {
 	test('.1 Lägg till en tagg', async ({ page }) => {
 		await page.getByRole('button', { name: 'Hantera tagg' }).click();
 		await page.getByPlaceholder('Sök eller skapa tagg').click();
-		await page.getByPlaceholder('Sök eller skapa tagg').fill('testtagg');
+		await page.getByPlaceholder('Sök eller skapa tagg').fill('testar');
 		await page.locator('#tag-add-button').click();
 		await page.locator('#checkbox-element').getByRole('img').click();
 		await page.getByPlaceholder('Sök eller skapa tagg').dblclick();
-		await page.getByPlaceholder('Sök eller skapa tagg').fill('kan ej ta bort');
+		await page.getByPlaceholder('Sök eller skapa tagg').fill('sluta');
 		await page.locator('#tag-add-button').click();
 		await page.locator('#save-and-close-button').click();
 		await page.getByRole('button', { name: 'Spara' }).click();
 		await page.goto('technique/138');
-		await expect(page.getByText("kan ej ta bort")).toHaveCount(1);
-		await expect(page.getByText("testtagg")).toHaveCount(0);
+		await expect(page.getByText("sluta")).toHaveCount(1);
+		await expect(page.getByText("testar")).toHaveCount(0);
 	})
 
 	test('.2 Redigera tagg', async({ page }) => {
 		await page.getByRole('button', { name: 'Hantera tagg' }).click();
 		await page.getByPlaceholder('Sök eller skapa tagg').click();
-		await page.getByPlaceholder('Sök eller skapa tagg').fill('testtagg');
+		await page.getByPlaceholder('Sök eller skapa tagg').fill('testar');
 		await page.locator('#pencil-icon').click();
 		await page.locator('#edit-element').fill('test');
 		await page.locator('#accept-icon').click();
@@ -40,7 +40,7 @@ test.describe("AddTagPopup tests", () => {
 	test('.3 Kan inte ta bort tagg som används', async({ page }) => {
 		await page.getByRole('button', { name: 'Hantera tagg' }).click();
 		await page.getByPlaceholder('Sök eller skapa tagg').click();
-		await page.getByPlaceholder('Sök eller skapa tagg').fill('kan ej ta bort');
+		await page.getByPlaceholder('Sök eller skapa tagg').fill('sluta');
 		await page.locator('#close-icon').click();
 		await expect(page.getByText("Taggen kan inte tas bort")).toHaveCount(1);
 	})
@@ -52,7 +52,7 @@ test.describe("AddTagPopup tests", () => {
 		await page.locator('#close-icon').click();
 		await page.getByRole('button', { name: 'Ta bort', exact: true }).click();
 		await page.getByPlaceholder('Sök eller skapa tagg').click();
-		await page.getByPlaceholder('Sök eller skapa tagg').fill('kan ej ta bort');
+		await page.getByPlaceholder('Sök eller skapa tagg').fill('sluta');
 		await page.locator('#checkbox-element').getByRole('img').click();
 		await page.locator('#save-and-close-button').click();
 		await page.getByRole('button', { name: 'Spara' }).click();
@@ -60,14 +60,14 @@ test.describe("AddTagPopup tests", () => {
 		await page.getByRole('link').click();
 		await page.getByRole('button', { name: 'Hantera tagg' }).click();
 		await page.getByPlaceholder('Sök eller skapa tagg').click();
-		await page.getByPlaceholder('Sök eller skapa tagg').fill('kan ej ta bort');
+		await page.getByPlaceholder('Sök eller skapa tagg').fill('sluta');
 		await page.locator('#close-icon').click();
 		await page.getByRole('button', { name: 'Ta bort' }).click();
 		await page.locator('#save-and-close-button').click();
 		await page.getByRole('button', { name: 'Spara' }).click();
 		await page.goto('technique/138');
 		await expect(page.getByText("test")).toHaveCount(0);
-		await expect(page.getByText("kan ej ta bort")).toHaveCount(0);
+		await expect(page.getByText("sluta")).toHaveCount(0);
 	})
 
 	
