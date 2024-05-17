d51dc37a40524a43606b0e40c09da354ebd3955a
Linus Wallenborg <c21lwg@cs.umu.se>
2024-05-17 13:45:58 +0200

    Added new tables to database

---
+++ infra/database/init/defaults/grading_protocols.sql
@@ -0,0 +1,50 @@
+--
+-- INSERTS FOR GRADING PROTOCOLS
+--
+
+
+
+--- 5 KYU GULT BÄLTE
+INSERT INTO grading_protocol (belt_id, protocol_code, protocol_name) VALUES (3 , '5 KYU', 'GULT BÄLTE');
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (1, 'KIHON WAZA - ATEMI WAZA', 1);
+--- ADD TECHNIQUES TO CATEGORY HERE
+
+
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (1, 'KIHON WAZA - KANSETSU WAZA', 2);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (1, 'KIHON WAZA - NAGE WAZA', 3);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (1, 'RENRAKU WAZA', 4);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (1, 'YAKUSOKU GEIKO', 5);
+
+
+--- 4 KYU ORANGE BÄLTE
+INSERT INTO grading_protocol (belt_id, protocol_code, protocol_name) VALUES (5 , '4 KYU', 'ORANGE BÄLTE');
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (2, 'KIHON WAZA - ATEMI WAZA', 1);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (2, 'KIHON WAZA - KANSUTSU WAZA', 2);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (2, 'KIHON WAZA - NAGE WAZA', 3);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (2, 'RENRAKU WAZA', 4);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (2, 'YAKUSOKU GEIKO', 5);
+
+
+--- 3 KYU GRÖNT BÄLTE
+INSERT INTO grading_protocol (belt_id, protocol_code, protocol_name) VALUES (7 , '3 KYU', 'GRÖNT BÄLTE');
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (3, 'KIHON WAZA - ATEMI WAZA', 1);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (3, 'KIHON WAZA - KANSUTSU WAZA', 2);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (3, 'KIHON WAZA - NAGE WAZA', 3);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (3, 'RENRAKU WAZA', 4);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (3, 'YAKUSOKU GEIKO', 5);
+
+--- 2 KYU BLÅTT BÄLTE
+INSERT INTO grading_protocol (belt_id, protocol_code, protocol_name) VALUES (9 , '2 KYU', 'BLÅTT BÄLTE');
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (4, 'KIHON WAZA - ATEMI WAZA', 1);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (4, 'KIHON WAZA - KANSUTSU WAZA', 2);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (4, 'KIHON WAZA - NAGE WAZA', 3);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (4, 'RENRAKU WAZA', 4);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (4, 'RANDORI', 5);
+
+--- 1 KYU BRUNT BÄLTE
+INSERT INTO grading_protocol (belt_id, protocol_code, protocol_name) VALUES (11 , '1 KYU', 'BRUNT BÄLTE');
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (5, 'KIHON WAZA - ATEMI WAZA', 1);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (5, 'KIHON WAZA - KANSUTSU WAZA', 2);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (5, 'KIHON WAZA - NAGE WAZA', 3);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (5, 'RENRAKU WAZA', 4);
+INSERT INTO grading_protocol_category (protocol_id, category_name, category_order) VALUES (5, 'RANDORI', 5);
