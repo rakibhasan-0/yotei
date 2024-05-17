a523d3397811218b352cca241086aeea361dc4a3
Gabriel Morberg <c21gmg@cs.umu.se>
2024-05-17 16:37:49 +0200

    Fixing linter issues

--- frontend/src/components/Grading/GradingProtocolsRows.jsx
+++ frontend/src/components/Grading/GradingProtocolsRows.jsx
@@ -1,28 +1,28 @@
-import React from "react";
-import styles from "./GradingProtocolsRows.module.css";
-import TechniqueCard from "../Common/Technique/TechniqueCard/TechniqueCard";
+import React from "react"
+import styles from "./GradingProtocolsRows.module.css"
+import TechniqueCard from "../Common/Technique/TechniqueCard/TechniqueCard"
 
 export default function GradingProtocolsRows({ data, beltColors }) {
-    return (
-        <div className={styles.container}>
-            {data.map((category, index) => (
-                <div key={index} className={styles.category}>
-                    <h3>{category.name}</h3>
-                    <ul className={styles.techniques}>
-                        {category.techniques.map((technique, idx) => {
-                            // Add the "type" attribute and set it to 'technique'
-                            const updatedTechnique = { 
-                                ...technique, 
-                                beltColors, 
-                                type: 'technique' // Add the new attribute
-                            };
-                            return (
-                                <TechniqueCard key={idx} technique={updatedTechnique} checkbox={false} id={updatedTechnique.activity_id} />
-                            );
-                        })}
-                    </ul>
-                </div>
-            ))}
-        </div>
-    );
+	return (
+		<div className={styles.container}>
+			{data.map((category, index) => (
+				<div key={index} className={styles.category}>
+					<h3>{category.name}</h3>
+					<ul className={styles.techniques}>
+						{category.techniques.map((technique, idx) => {
+							// Add the "type" attribute and set it to 'technique'
+							const updatedTechnique = { 
+								...technique, 
+								beltColors, 
+								type: "technique" // Add the new attribute
+							}
+							return (
+								<TechniqueCard key={idx} technique={updatedTechnique} checkbox={false} id={updatedTechnique.activity_id} />
+							)
+						})}
+					</ul>
+				</div>
+			))}
+		</div>
+	)
 }
--- frontend/src/pages/Statistics/GradingStatisticsPopup.jsx
+++ frontend/src/pages/Statistics/GradingStatisticsPopup.jsx
@@ -11,10 +11,10 @@ const mockGradingProtocols = [
 ]
 const mockData = [
 	{
-	   code: "5 KYU",
-	   name: "GULT BÄLTE",
-	   
-	   belt: [
+		code: "5 KYU",
+		name: "GULT BÄLTE",
+		
+		belt: [
 			{
 				belt_color: "f1c40f",
 				belt_name: "Gult",
@@ -22,107 +22,106 @@ const mockData = [
 				length: 1
 			}
 		],
-   
-	   categories: [
-		   {
-			   name: "KIHON WAZA - ATEMI WAZA",
-			   techniques: [
-				   {
-					   activity_id: 151,
-					   name: "Shotei uchi, jodan, rak stöt med främre och bakre handen",
-					   count: 1
-				   },
-				   {
-					   name: "Shotei uchi, chudan, rak stöt med främre och bakre handen",
-					   activity_id: 151,
-					   count: 1
-				   },
-				   {
-					   activity_id: 153,
-					   name: "Gedan geri, rak spark med främre och bakre benet",
-					   count: 1
-				   }
-			   ]
-		   },
-		   {
-			   name: "KIHON WAZA - KANSUTSU WAZA",
-			   techniques: [
-				   {
-					   name: "O soto osae, utan grepp, nedläggning snett bakåt",
-					   activity_id: 187,
-					   count: 1
-				   }
-			   ]
-		   },
-		   {
-			   name: "KIHON WAZA - NAGE WAZA",
-			   techniques: [
-				   {
-					   name: "Koshi otoshi, utan grepp, nedläggning snett bakåt",
-					   activity_id: 248,
-					   count: 0
-				   }
-			   ]
-		   },
-		   {
-			   name: "JIGO WAZA",
-			   techniques: [
-				   {
-					   name: "Grepp i två handleder framifrån - Frigöring",
-					   activity_id: 158,
-					   count: 0
-				   },
-				   {
-					   name: "Grepp i två handleder bakifrån - Frigöring",
-					   activity_id: 159,
-					   count: 0
-				   },
-				   {
-					   name: "Grepp i håret bakifrån - Tettsui uchi, frigöring",
-					   activity_id: 161,
-					   count: 0
-				   },
-				   {
-					   name: "Försök till stryptag framifrån - Jodan soto uke",
-					   activity_id: 216,
-					   count: 0
-				   },
-				   {
-					   name: "Stryptag framifrån - Kawashi, frigöring",
-					   activity_id: 162,
-					   count: 0
-				   },
-				   {
-					   name: "Stryptag bakifrån - Maesabaki, kawashi, frigöring",
-					   activity_id: 163,
-					   count: 0
-				   },
-				   {
-					   name: "Stryptag med armen - Maesabaki, kuzure ude osae, ude henkan gatame",
-					   activity_id: 164,
-					   count: 0
-				   },
-				   {
-					   name: "Försök till kravattgrepp från sidan - Jodan chikai uke, kawashi, koshi otoshi, ude henkan gatame",
-					   activity_id: 165,
-					   count: 0
-				   },
-				   {
-					   name: "Grepp i ärmen med drag - O soto osae, ude henkan gatame",
-					   activity_id: 154,
-					   count: 0
-				   },
-				   {
-					   name: "Livtag under armarna framifrån - Tate hishigi, ude henkan gatame",
-					   activity_id: 169,
-					   count: 0
-				   }
-			   ]
-		   }
-	   ]
+	
+		categories: [
+			{
+				name: "KIHON WAZA - ATEMI WAZA",
+				techniques: [
+					{
+						activity_id: 151,
+						name: "Shotei uchi, jodan, rak stöt med främre och bakre handen",
+						count: 1
+					},
+					{
+						name: "Shotei uchi, chudan, rak stöt med främre och bakre handen",
+						activity_id: 151,
+						count: 1
+					},
+					{
+						activity_id: 153,
+						name: "Gedan geri, rak spark med främre och bakre benet",
+						count: 1
+					}
+				]
+			},
+			{
+				name: "KIHON WAZA - KANSUTSU WAZA",
+				techniques: [
+					{
+						name: "O soto osae, utan grepp, nedläggning snett bakåt",
+						activity_id: 187,
+						count: 1
+					}
+				]
+			},
+			{
+				name: "KIHON WAZA - NAGE WAZA",
+				techniques: [
+					{
+						name: "Koshi otoshi, utan grepp, nedläggning snett bakåt",
+						activity_id: 248,
+						count: 0
+					}
+				]
+			},
+			{
+				name: "JIGO WAZA",
+				techniques: [
+					{
+						name: "Grepp i två handleder framifrån - Frigöring",
+						activity_id: 158,
+						count: 0
+					},
+					{
+						name: "Grepp i två handleder bakifrån - Frigöring",
+						activity_id: 159,
+						count: 0
+					},
+					{
+						name: "Grepp i håret bakifrån - Tettsui uchi, frigöring",
+						activity_id: 161,
+						count: 0
+					},
+					{
+						name: "Försök till stryptag framifrån - Jodan soto uke",
+						activity_id: 216,
+						count: 0
+					},
+					{
+						name: "Stryptag framifrån - Kawashi, frigöring",
+						activity_id: 162,
+						count: 0
+					},
+					{
+						name: "Stryptag bakifrån - Maesabaki, kawashi, frigöring",
+						activity_id: 163,
+						count: 0
+					},
+					{
+						name: "Stryptag med armen - Maesabaki, kuzure ude osae, ude henkan gatame",
+						activity_id: 164,
+						count: 0
+					},
+					{
+						name: "Försök till kravattgrepp från sidan - Jodan chikai uke, kawashi, koshi otoshi, ude henkan gatame",
+						activity_id: 165,
+						count: 0
+					},
+					{
+						name: "Grepp i ärmen med drag - O soto osae, ude henkan gatame",
+						activity_id: 154,
+						count: 0
+					},
+					{
+						name: "Livtag under armarna framifrån - Tate hishigi, ude henkan gatame",
+						activity_id: 169,
+						count: 0
+					}
+				]
+			}
+		]
 	}
-   ];
-   
+]
 
 
 export default function GradingStatisticsPopup({ id, gradingProtocols = mockGradingProtocols , data}) {
