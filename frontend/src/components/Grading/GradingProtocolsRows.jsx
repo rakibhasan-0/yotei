4886f357a30b5f72cfde9bc993eeb69a96cffefe
Gabriel Morberg <c21gmg@cs.umu.se>
2024-05-17 15:26:16 +0200

    Grading protocol statistics pop up now show colors

--- frontend/src/components/Grading/GradingProtocolsRows.jsx
+++ frontend/src/components/Grading/GradingProtocolsRows.jsx
@@ -1,21 +1,28 @@
-import React from "react"
-import styles from "./GradingProtocolsRows.module.css"
-import TechniqueCard from "../Common/Technique/TechniqueCard/TechniqueCard"
+import React from "react";
+import styles from "./GradingProtocolsRows.module.css";
+import TechniqueCard from "../Common/Technique/TechniqueCard/TechniqueCard";
 
-export default function GradingProtocolsRows({ data,chosenProtocol }) {
-	return (
-		<div className={styles.container}>
-			{data.map((category, index) => (
-				<div key={index} className={styles.category}>
-					<h3>{category.name}</h3>
-					<ul className={styles.techniques}>
-						{category.techniques.map((technique, idx) => (
-							<TechniqueCard key={idx} technique={technique} />
-                        
-						))}
-					</ul>
-				</div>
-			))}
-		</div>
-	)
+export default function GradingProtocolsRows({ data, beltColors }) {
+    return (
+        <div className={styles.container}>
+            {data.map((category, index) => (
+                <div key={index} className={styles.category}>
+                    <h3>{category.name}</h3>
+                    <ul className={styles.techniques}>
+                        {category.techniques.map((technique, idx) => {
+                            // Add the "type" attribute and set it to 'technique'
+                            const updatedTechnique = { 
+                                ...technique, 
+                                beltColors, 
+                                type: 'technique' // Add the new attribute
+                            };
+                            return (
+                                <TechniqueCard key={idx} technique={updatedTechnique} checkbox={false} id={updatedTechnique.activity_id} />
+                            );
+                        })}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    );
 }
--- frontend/src/pages/Statistics/GradingStatisticsPopup.jsx
+++ frontend/src/pages/Statistics/GradingStatisticsPopup.jsx
@@ -10,103 +10,119 @@ const mockGradingProtocols = [
 	"4 KYU ORANGE BÄLTE",
 ]
 const mockData = [
-    {
-        name: "KIHON WAZA - ATEMI WAZA",
-        techniques: [
-            {
-                name: "Shotei uchi, jodan, rak stöt med främre och bakre handen",
-                id: 151,
-                count: 1
-				
-            },
-            {
-                name: "Shotei uchi, chudan, rak stöt med främre och bakre handen",
-                id: 151,
-                count: 1
-            },
-            {
-                name: "Gedan geri, rak spark med främre och bakre benet",
-                id: 153,
-                count: 1
-            }
-        ]
-    },
-    {
-        name: "KIHON WAZA - KANSUTSU WAZA",
-        techniques: [
-            {
-                name: "O soto osae, utan grepp, nedläggning snett bakåt",
-                id: 187,
-                count: 1
-            }
-        ]
-    },
-    {
-        name: "KIHON WAZA - NAGE WAZA",
-        techniques: [
-            {
-                name: "Koshi otoshi, utan grepp, nedläggning snett bakåt",
-                id: 248,
-                count: 0
-            }
-        ]
-    },
-    {
-        name: "JIGO WAZA",
-        techniques: [
-            {
-                name: "Grepp i två handleder framifrån - Frigöring",
-                id: 158,
-                count: 0
-            },
-            {
-                name: "Grepp i två handleder bakifrån - Frigöring",
-                id: 159,
-                count: 0
-            },
-            {
-                name: "Grepp i håret bakifrån - Tettsui uchi, frigöring",
-                id: 161,
-                count: 0
-            },
-            {
-                name: "Försök till stryptag framifrån - Jodan soto uke",
-                id: 216,
-                count: 0
-            },
-            {
-                name: "Stryptag framifrån - Kawashi, frigöring",
-                id: 162,
-                count: 0
-            },
-            {
-                name: "Stryptag bakifrån - Maesabaki, kawashi, frigöring",
-                id: 163,
-                count: 0
-            },
-            {
-                name: "Stryptag med armen - Maesabaki, kuzure ude osae, ude henkan gatame",
-                id: 164,
-                count: 0
-            },
-            {
-                name: "Försök till kravattgrepp från sidan - Jodan chikai uke, kawashi, koshi otoshi, ude henkan gatame",
-                id: 165,
-                count: 0
-            },
-            {
-                name: "Grepp i ärmen med drag - O soto osae, ude henkan gatame",
-                id: 154,
-                count: 0
-            },
-            {
-                name: "Livtag under armarna framifrån - Tate hishigi, ude henkan gatame",
-                id: 169,
-                count: 0
-            }
-        ]
-    }
-];
+	{
+	   code: "5 KYU",
+	   name: "GULT BÄLTE",
+	   
+	   belt: [
+			{
+				belt_color: "83530C",
+				belt_name: "Gult",
+				is_child: false,
+				length: 1
+			}
+		],
+   
+	   categories: [
+		   {
+			   name: "KIHON WAZA - ATEMI WAZA",
+			   techniques: [
+				   {
+					   activity_id: 151,
+					   name: "Shotei uchi, jodan, rak stöt med främre och bakre handen",
+					   count: 1
+				   },
+				   {
+					   name: "Shotei uchi, chudan, rak stöt med främre och bakre handen",
+					   activity_id: 151,
+					   count: 1
+				   },
+				   {
+					   activity_id: 153,
+					   name: "Gedan geri, rak spark med främre och bakre benet",
+					   count: 1
+				   }
+			   ]
+		   },
+		   {
+			   name: "KIHON WAZA - KANSUTSU WAZA",
+			   techniques: [
+				   {
+					   name: "O soto osae, utan grepp, nedläggning snett bakåt",
+					   activity_id: 187,
+					   count: 1
+				   }
+			   ]
+		   },
+		   {
+			   name: "KIHON WAZA - NAGE WAZA",
+			   techniques: [
+				   {
+					   name: "Koshi otoshi, utan grepp, nedläggning snett bakåt",
+					   activity_id: 248,
+					   count: 0
+				   }
+			   ]
+		   },
+		   {
+			   name: "JIGO WAZA",
+			   techniques: [
+				   {
+					   name: "Grepp i två handleder framifrån - Frigöring",
+					   activity_id: 158,
+					   count: 0
+				   },
+				   {
+					   name: "Grepp i två handleder bakifrån - Frigöring",
+					   activity_id: 159,
+					   count: 0
+				   },
+				   {
+					   name: "Grepp i håret bakifrån - Tettsui uchi, frigöring",
+					   activity_id: 161,
+					   count: 0
+				   },
+				   {
+					   name: "Försök till stryptag framifrån - Jodan soto uke",
+					   activity_id: 216,
+					   count: 0
+				   },
+				   {
+					   name: "Stryptag framifrån - Kawashi, frigöring",
+					   activity_id: 162,
+					   count: 0
+				   },
+				   {
+					   name: "Stryptag bakifrån - Maesabaki, kawashi, frigöring",
+					   activity_id: 163,
+					   count: 0
+				   },
+				   {
+					   name: "Stryptag med armen - Maesabaki, kuzure ude osae, ude henkan gatame",
+					   activity_id: 164,
+					   count: 0
+				   },
+				   {
+					   name: "Försök till kravattgrepp från sidan - Jodan chikai uke, kawashi, koshi otoshi, ude henkan gatame",
+					   activity_id: 165,
+					   count: 0
+				   },
+				   {
+					   name: "Grepp i ärmen med drag - O soto osae, ude henkan gatame",
+					   activity_id: 154,
+					   count: 0
+				   },
+				   {
+					   name: "Livtag under armarna framifrån - Tate hishigi, ude henkan gatame",
+					   activity_id: 169,
+					   count: 0
+				   }
+			   ]
+		   }
+	   ]
+	}
+   ];
+   
 
 
 export default function GradingStatisticsPopup({ id, gradingProtocols = mockGradingProtocols , data}) {
@@ -123,7 +139,6 @@ export default function GradingStatisticsPopup({ id, gradingProtocols = mockGrad
 	}
 
 	data = mockData
-	
 
 	return (
 		<div className={style.gradingStatisticsContainer} id={id}>
@@ -144,11 +159,10 @@ export default function GradingStatisticsPopup({ id, gradingProtocols = mockGrad
 					<GradingProtocolsRowsMenu protocols={gradingProtocols} onSelectRow = {onSelectRow} />
 				</Dropdown>
 
-				<GradingProtocolsRows data={data} chosenProtocol={chosenProtocol}>
+				<GradingProtocolsRows data={data[0].categories} chosenProtocol={chosenProtocol} beltColors = {data[0].belt} >
 
 				</GradingProtocolsRows>
 
-				<p>Här kommer att visas data</p>
 			</Popup>
 		</div>
 	)
