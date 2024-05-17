4543ea8a1501a0304e9ead292349d7057eaa200a
Joar Eliasson <c19jen@ad.umu.se>
2024-05-17 09:59:17 +0200

    Removed unused import

--- backend/api/src/main/java/se/umu/cs/pvt/examination/ExaminationController.java
+++ backend/api/src/main/java/se/umu/cs/pvt/examination/ExaminationController.java
@@ -6,8 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import se.umu.cs.pvt.belt.BeltRepository;
 
-import static org.mockito.ArgumentMatchers.isNotNull;
-
 import java.util.List;
 import java.util.Optional;
 
