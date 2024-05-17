98b39e27ebe6e7cebc55924d3b59241e12c3b21f
id20ehm <id20ehm@cs.umu.se>
2024-05-17 15:21:55 +0200

    add expect to canary test

--- frontend/SystemTests/TestFiles/canaryTest.spec.ts
+++ frontend/SystemTests/TestFiles/canaryTest.spec.ts
@@ -1,15 +1,15 @@
-import { test, expect } from '../fixtures';
+import { test, expect } from '../fixtures'
 
 /**
  * A canary test for testing the system test environment. Verifies that fixtures for login as an admin works.
  * Can be deleted later on when other tests are implemented. 
  *
  *  @author Team Mango (Group 4)
- *  @since 2024-05-8
+ *  @since 2024-05-17
  *  @version 2.0
  */
-test.describe('ST-1 Canary Test', () => {
+test.describe('Canary test', () => {
   test('1. Planering title should be visible if the fixtures work', async ({ page }) => {
-    await page.locator('center').getByRole('heading', { name: 'Planering' }).click();
+    await expect(page.locator('center').getByRole('heading', { name: 'Planering' })).toBeVisible()
   });
-});
+}); 
