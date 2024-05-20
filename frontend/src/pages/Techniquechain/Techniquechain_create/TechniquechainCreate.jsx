2e640b0296d318cb84dbe551cf4cc8c6e6011a1e
Isac Norlander <c21inr@cs.umu.se>
2024-05-20 16:17:27 +0200

    added the content for the create chain page

--- frontend/src/pages/Techniquechain/Techniquechain_create/TechniquechainCreate.jsx
+++ frontend/src/pages/Techniquechain/Techniquechain_create/TechniquechainCreate.jsx
@@ -1,7 +1,123 @@
+import InputTextField from "../../../components/Common/InputTextField/InputTextField.jsx"
+import React, { useContext, useEffect, useState, useRef } from "react"
+import styles from "./TechniquechainCreate.module.css"
+import TextArea from "../../../components/Common/TextArea/TextArea.jsx"
+import Divider from "../../../components/Common/Divider/Divider.jsx"
+import Dropdown from "../../../components/Common/List/Dropdown"
+import { useLocation } from "react-router-dom"
+import Button from "../../../components/Common/Button/Button.jsx"
+import { useNavigate } from "react-router"
+
 export default function TechniquechainCreate() {
+
+    const { state } = useLocation()
+	const [errorMessage, setErrorMessage] = useState("")
+    const [groups, setGroups] = useState()
+	const [group, setGroup] = useState(state?.session?.group)
+    const navigate = useNavigate()
+
+	// true when data has been saved, when unmounting and rebuilding view.
+	const [techniquechainCreateInput, setTechniquechainCreateInput] = useState(() => {
+		const retTechniquechainCreateInput = JSON.parse(localStorage.getItem("techniqueCreateLocalStorageKey"))
+		if (retTechniquechainCreateInput) {
+			return retTechniquechainCreateInput
+		} else {
+			return {
+				name: "",
+				desc: "",
+				addBoxChecked: false,
+				eraseBoxChecked: false,
+				addedTags: []
+			}
+		}
+	})
+
+	const clearTechniqueCreateInput = (addBoxChecked, eraseBoxChecked) => {
+		setTechniquechainCreateInput({
+			name: "",
+			desc: "",
+			addBoxChecked: addBoxChecked,
+			eraseBoxChecked: eraseBoxChecked,
+			addedTags: []
+		})
+
+		localStorage.removeItem("techniqueCreateLocalStorageKey")
+	}
+
+	const [name, setName] = useState(() => {
+		return techniquechainCreateInput.name
+	})
+	const [desc, setDesc] = useState(() => {
+		return techniquechainCreateInput.desc
+	})
+	const [time, setTime] = useState(() => {
+		return techniquechainCreateInput.time
+	})
+
+	const [addedTags, setAddedTags] = useState(() => {
+		return techniquechainCreateInput.addedTags
+	})
+
+	/**
+	 * Updates the exerciseCreateInput object when new input is added
+	 * @param fieldName The exerciseCreateInput objects attribute to be changed
+	 * @param value The new value
+	 */
+	const storeInputChange = (fieldName, value) => {
+		setTechniquechainCreateInput(prevState => ({
+			...prevState,
+			[fieldName]: value
+		}))
+	}
+
 	return (
 		<div>
-            hello from create
+			<title>Skapa Kedja</title>
+			<h1>Skapa kedja</h1>
+			<div style={{ height: "1rem"}}/>
+
+			<InputTextField
+				placeholder="Namn"
+				text={techniquechainCreateInput.name}
+				onChange={(e) => {
+					setName(e.target.value)
+					storeInputChange("name", e.target.value)
+				}}
+				required={true}
+				type="text"
+				id="ExerciseNameInput"
+				errorMessage={errorMessage}
+			/>
+			<TextArea
+				className={styles.standArea}
+				placeholder="Beskrivning"
+				text={techniquechainCreateInput.desc}
+				onChange={(e) => {
+					setDesc(e.target.value)
+					storeInputChange("desc", e.target.value)
+				}}
+				required={true}
+				type="text"
+				id = "exercise-description-input"
+				errorDisabled={true}
+			/>
+			<Divider option={"h1_left"} title={"Teknikväv"} />
+
+			<Dropdown id={"techniqueweave-dropdown"} text={group?.name || "Teknikväv"} centered={true}>
+				{groups?.length > 0 ? groups.map((plan, index) => (
+					<div className={styles.dropdownRow} key={index} onClick={() =>{ 
+						setGroup(plan)
+					}}>
+						<p className={styles.dropdownRowText}>{plan.name}</p>
+					</div>
+				)) : <div className={styles.dropdownRow}>
+					<p className={styles.dropdownRowText}>Kunde inte hitta några Teknikvävar</p>
+				</div>}
+			</Dropdown>
+
+            <div className={styles.wrapCentering} style={{ marginBottom: "2rem", marginTop: "1rem" }} >
+				<Button onClick= {() => navigate("/techniquechain")} id = {"sessions-back"}outlined={true}><p>Tillbaka</p></Button>
+			</div>
 		</div>
 	)
 
--- frontend/src/pages/Techniquechain/Techniquechain_page/Techniquechain_page.jsx
+++ frontend/src/pages/Techniquechain/Techniquechain_page/Techniquechain_page.jsx
@@ -5,14 +5,25 @@ import { isEditor } from "../../../utils"
 import { AccountContext } from "../../../context"
 import PrintButton from "../../../components/Common/PrintButton/PrintButton"
 import styles from "./Techniquechain_page.module.css"
+import Spinner from "../../../components/Common/Spinner/Spinner"
+import InfiniteScrollComponent from "../../../components/Common/List/InfiniteScrollComponent"
+import TechniquechainCard from "../../../components/Common/TechniquechainCard/TechniquechainCard"
+import Button from "../../../components/Common/Button/Button.jsx"
 
 export default function Techniquechain_page() {
 
 	const navigate = useNavigate()
 	const techniqueId = localStorage.getItem("stored_techniquechain")
+
+	//this is the currently chosen technique, onely get the id when mounting so need to get all the other info from db
 	const [techniquechain, settechniquechain] = useState({id: techniqueId, name: "test övning 1", description: "hejsan mycket rolig kedja"})
 	const accountRole = useContext(AccountContext)
 	const [showDeletePopup, setShowDeletePopup] = useState(false)
+
+	//this is a list of all the techniques to be displayed in the list. read all the real techniqus from the db insted of hard coded
+	const [chainTechniques, setChaintechniques] = useState([{id: techniqueId, name: "test övning 1", description: "hejsan mycket rolig kedja"}, {id: techniqueId, name: "test övning 2", description: "hejsan mycket rolig kedja"}])
+	const [loading, setIsLoading] = useState(false)
+	const detailURL = "/techniquechain/techniquechain_page/"
 
 	return (
 		<div style={{ display: "flex", flexDirection: "column" }}>
@@ -46,6 +57,33 @@ export default function Techniquechain_page() {
 			<p style={{ textAlign: "left", whiteSpace: techniquechain?.description ? "pre-line" : "normal", fontStyle: !techniquechain?.description ? "italic" : "normal", color: !techniquechain?.description ? "var(--gray)" : "inherit" }}>
 				{techniquechain?.description || "Beskrivning saknas."}
 			</p>
+			{ loading ? <Spinner/> :
+				<div>
+					<InfiniteScrollComponent>
+						{ chainTechniques.map((technique, index) => {
+							return (
+								<div key={technique.id} style={{ display: "flex", alignItems: "center", marginBottom: "1px", width: "100%" }}>
+									<span style={{ marginRight: "10px", fontSize: "25px", marginTop: "10px" }}>{index + 1}</span>
+									<div style={{ flex: 1 }}>
+										<TechniquechainCard
+											item={technique.name}
+											key={technique.id}
+											id={technique.id}
+											detailURL={detailURL}
+											index={index}>
+										</TechniquechainCard>
+									</div>
+								</div>
+							)
+						})}
+					</InfiniteScrollComponent>
+				</div>
+			}
+			{/* TODOO add the graph component to show the parent graph that the chain is created from, the chain needs to be marked somehow in the graph */}
+            
+			<div className={styles.wrapCentering} style={{ marginBottom: "2rem", marginTop: "1rem" }} >
+				<Button onClick= {() => navigate("/techniquechain")} id = {"sessions-back"}outlined={true}><p>Tillbaka</p></Button>
+			</div>	
 		</div>
 	)
 
