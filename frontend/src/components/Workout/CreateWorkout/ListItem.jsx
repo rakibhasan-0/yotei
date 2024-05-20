20b34e42e5c925eee375e6c0597064c23e870c7b
Willy Själin <willys@cs.umu.se>
2024-05-20 14:16:01 +0200

    Documentation

--- frontend/src/components/Workout/CreateWorkout/ListItem.jsx
+++ frontend/src/components/Workout/CreateWorkout/ListItem.jsx
@@ -6,7 +6,7 @@ import style from "./ListItem.module.css"
  * @param {Object} item 	The activity such as techniques and exercises.
  * @param {JSX} checkBox 	If a checkbox is wanted, send it as a prop.
  * @param {String} id 		The id of the activity that is in the list.
- * 	INDEXXXXXXXNXNXNXNXNXNXNXNNXNXN
+ * @param {integer} index	The index of the item in the list.
  *
  * @author	Team Tomato
  * @since	2024-05-20
@@ -27,6 +27,8 @@ export default function ListItem({ item, checkBox, id, index}) {
 			navigate("/exercise/exercise_page/" + item.path)
 		}
 	}
+
+	
 	return (
 		<div style={{ backgroundColor: bgColor }}>
 			
