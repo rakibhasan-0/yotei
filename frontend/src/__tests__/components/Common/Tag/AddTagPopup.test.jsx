6a056dfcfb5f1e8580f49067030f8b664315adea
Carl Hjortsberg <dv22chg@cs.umu.se>
2024-05-20 11:21:40 +0200

    Fixed AddTagPopupTest

--- frontend/src/__tests__/components/Common/Tag/AddTagPopup.test.jsx
+++ frontend/src/__tests__/components/Common/Tag/AddTagPopup.test.jsx
@@ -1,61 +1,40 @@
 /** @jest-environment jsdom */
-//import {React} from "react"
-import {render, configure} from "@testing-library/react"
+import {React} from "react"
+import {render, screen, configure} from "@testing-library/react"
 import "@testing-library/jest-dom"
-import AddTagPopup from "../../../../components/Common/Tag/AddTagPopup"
 import { MemoryRouter } from "react-router"
-import TagInput from "../../../../components/Common/Tag/TagInput"
-import { fireEvent } from "@testing-library/react"
+import AddTagPopup from "../../../../components/Common/Tag/AddTagPopup"
 
 /**
  * Test for the AddTagPopup component.
  *
- * @author Team Minotaur
+ * @author Team Minotaur, Team Durian (Group 3)
  * @version 1.0
  * @since 2023-05-08
  */
 
 configure({testIdAttribute: "id"})
-test("Tag: Added tag should have checked checkbox", async() => {
-	// ARRANGE
-	const addedTags = [{id:1,name:"Tag1"}]
 
-	// ACT
-	const {getByText} =  render(<div><MemoryRouter>
-		<TagInput onClick={
-			()=> <AddTagPopup id ="addTagPopupDiv" addedTags={addedTags}/>} id ="tagChooser" addedTags={addedTags}></TagInput></MemoryRouter></div>)
-
-	// ACT
-	const button = getByText("Hantera tagg")
-	fireEvent.click(button)
-
-	// ASSERT
-	const popup = getByText("Tag1")
-	expect(popup).toBeVisible()
-})
-
-/*test("Tag: Should render the component as a whole", async() => {
+//Simple component test. To test the functionallity of everything in the popup, write a playwright test. 
+test("Tag: Should show popup", async() => {
 	// ARRANGE
 	const addedTags = [{id:1,name:"Tag1"}]
 
 	render(
 		<div>
-			<AddTagPopup id ="tagChooser" addedTags={addedTags}/>
+			<MemoryRouter>
+				<AddTagPopup id ="tagChooser" addedTags={addedTags}/>
+			</MemoryRouter>
 		</div>
 	)
 
 	// ACT
-	//const addedTag = screen.getByText("Tag1")
-	const searchBar = screen.queryByPlaceholderText("Sök eller skapa tagg")
-	//const addedTagHeading = screen.getByText("Tillagda taggar")
-
+	const searchbar = screen.getByPlaceholderText("Sök eller skapa tagg")
+	const saveButton = screen.getByTestId("save-and-close-button")
+	const sorter = screen.getByText("Sortering")
 
 	// ASSERT
-	//expect(addedTag).toBeVisible()
-	expect(searchBar).toBeVisible()
-	//expect(addedTagHeading).toBeVisible()
-})*/
-
-test("Expect true" , async() => {
-	expect(true).toBe(true)
+	expect(saveButton).toBeVisible()
+	expect(searchbar).toBeVisible()
+	expect(sorter).toBeVisible()
 })
