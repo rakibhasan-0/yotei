a287c240d525402ec53d997650d23c0dfc6d3779
Willy Själin <willys@cs.umu.se>
2024-05-20 13:16:18 +0200

    ListItem component visually done.

--- frontend/src/components/Workout/CreateWorkout/ListItem.jsx
+++ frontend/src/components/Workout/CreateWorkout/ListItem.jsx
@@ -1,47 +1,48 @@
-import TechniqueCard from "../../Common/Technique/TechniqueCard/TechniqueCard"
-import ExerciseListItem from "../../Common/ExerciseCard/ExerciseListItem"
-
+import style from "./ListItem.module.css"
 /**
  * This component is used to display the content of a list that is 
  * inside of a drop down menu.
- * 
  * @param {Object} item 	The activity such as techniques and exercises.
  * @param {JSX} checkBox 	If a checkbox is wanted, send it as a prop.
  * @param {String} id 		The id of the activity that is in the list.
+ * 	INDEXXXXXXXNXNXNXNXNXNXNXNNXNXN
  *
  * @author	Team Tomato
  * @since	2024-05-20
  */
-export default function ListItem({item, checkBox, id}) {
 
-	//TODO: Kanske ett namnbyte på komponenten? ListItem är något vagt?
-	
+export default function ListItem({ item, checkBox, id, index}) {
 
-	return(
-		<div>
+	// Determine background color based on index
+	const bgColor = index % 2 === 0 ? "#F8EBEC" : "#FFFFFF"
+
+	return (
+		<div style={{ backgroundColor: bgColor }}>
 			
 			{item.type === "technique" ? (
-				<TechniqueCard
-					id={"technique-list-item-" + id}
-					checkBox={checkBox}
-					technique={item}
-				/>
-			) : null
-			}
+				
+				<div 
+					id={"technique-list-item-" + id}>
+					<div className={style.innerDiv}>
+						<div className={style.checkboxDiv}> {checkBox} </div>
+						<div className={style.nameTag}> <p>{item.name}</p> </div>
 
-			{
-				item.type === "exercise" ? (
-					<ExerciseListItem
-						id={id}
-						text={item.duration + " min"}
-						detailURL={"/exercise/exercise_page/"}
-						checkBox={checkBox}
-						item={item.name}
-						index={item.id}
-						path={item.path}
-					/>
-				) : null
-			}
+					</div>
+				</div>
+				
+			) : null}
+
+			{item.type === "exercise" ? (
+				<div  id={id}>
+					<div className={style.innerDiv}>
+
+					{checkBox}
+						<div className={style.nameTag}> <p>{item.name}</p> </div>
+						<div className={style.durationTag}> <p>{item.duration} min</p> </div>
+					</div>
+					{/* <a href={"/exercise/exercise_page/" + id}>More Details</a> */}
+				</div>
+			) : null}
 		</div>
 	)
 }
