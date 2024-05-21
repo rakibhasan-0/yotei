5b9f9c26ea8e931fadd4ae0b45989ffd11cb89d1
Adam Lumio <dv22alo@cs.umu.se>
2024-05-21 15:08:12 +0200

    fixed lint issue

--- frontend/src/__tests__/components/Grading/EditableListItem.test.jsx
+++ frontend/src/__tests__/components/Grading/EditableListItem.test.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, screen, configure} from "@testing-library/react"
+import { render, screen, configure } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import EditableListItem from "../../../components/Common/EditableListItem/EditableListItem.jsx"
 import { BrowserRouter } from "react-router-dom"
@@ -11,37 +11,37 @@ import { BrowserRouter } from "react-router-dom"
  * @version 1.0
  */
 
-test("User can be created test", async() => {
-    var name = "Test 123"
+test("User can be created test", async () => {
+	var name = "Test 123"
 	var id = 1
 	var index = 0
 
-    render(
-        <BrowserRouter>
-            <EditableListItem
-                id={id}
-                item={name}
-                index={index}
-            />
-        </BrowserRouter>
-    )
-    expect(screen.getByTestId("EditableListItem")).toHaveTextContent(id, 1)
+	render(
+		<BrowserRouter>
+			<EditableListItem
+				id={id}
+				item={name}
+				index={index}
+			/>
+		</BrowserRouter>
+	)
+	expect(screen.getByTestId("EditableListItem")).toHaveTextContent(id, 1)
 })
 
-test("Trash icon rendered test", async() => {
-    var name = "Test 123"
+test("Trash icon rendered test", async () => {
+	var name = "Test 123"
 	var id = 1
 	var index = 0
-    
-    render(
-        <BrowserRouter>
-            <EditableListItem
-                id={id}
-                item={name}
-                index={index}
-                showTrash={true}
-            />
-        </BrowserRouter>
-    )
-    expect(screen.getByTestId("trash-icon")).toBeInTheDocument()
+
+	render(
+		<BrowserRouter>
+			<EditableListItem
+				id={id}
+				item={name}
+				index={index}
+				showTrash={true}
+			/>
+		</BrowserRouter>
+	)
+	expect(screen.getByTestId("trash-icon")).toBeInTheDocument()
 })
