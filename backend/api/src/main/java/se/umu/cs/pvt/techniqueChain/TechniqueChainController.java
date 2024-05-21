70ce2973755fcc927c068cd6fd24827aa07341fa
Isac Norlander <c21inr@cs.umu.se>
2024-05-21 15:23:52 +0200

    smal changes

--- backend/api/src/main/java/se/umu/cs/pvt/techniqueChain/TechniqueChainController.java
+++ backend/api/src/main/java/se/umu/cs/pvt/techniqueChain/TechniqueChainController.java
@@ -1,4 +1,6 @@
 package se.umu.cs.pvt.techniqueChain;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +13,22 @@ public class TechniqueChainController {
 
     private TechniqueChainNodeRepository nodeRepository; 
 
-    
+    @Autowired
+    public TechniqueChainController(TechniqueChainNodeRepository nodeRepository) {
+        this.nodeRepository = nodeRepository;
+    }
 
     protected TechniqueChainController() {}
+
+    @PostMapping("/node/create")
+    public ResponseEntity<Object> createNewNode(@RequestBody TechniqueChainNode node) {
+        TechniqueChainNode newNode = nodeRepository.save(node);
+        return new ResponseEntity<>(newNode, HttpStatus.OK);
+    }
     
+    @GetMapping("/node/all")
+    public ResponseEntity<List<TechniqueChainNode>> getNodes() {
+        return new ResponseEntity<>(nodeRepository.findAll(), HttpStatus.OK);
+    }
+
 }
--- frontend/src/pages/Techniquechain/TechniquechainIndex.jsx
+++ frontend/src/pages/Techniquechain/TechniquechainIndex.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react"
+import { useState, useEffect, useContext } from "react"
 import { Modal } from "react-bootstrap"
 import TechniqueWeaveIndex from "./TechniqueWeave/TechniqueWeaveIndex"
 import Tab from "react-bootstrap/Tab"
 import Tabs from "react-bootstrap/Tabs"
 import styles from "./TechniquechainIndex.module.css"
 import Techniquechain from "./Techniquechain/Techniquechain.jsx"
+
+
+import { AccountContext } from "../../context"
+
 
 /**
  * The technique index page.
@@ -16,8 +20,41 @@ import Techniquechain from "./Techniquechain/Techniquechain.jsx"
  */
 export default function TechniquechainIndex() {
 
+
+
+	const context = useContext(AccountContext)
+
+
+
 	const [key, setKey] = useState(sessionStorage.getItem("active-tab") || "technique")
 
+	useEffect(() => {
+
+		const list = {parentWeaveId: 1, nodeName: "test nod 1", description: "kollar om det funkar", techniqueId: 1, attacker: false, partisipant: 1, connected_to: [1,2]}
+
+		const requestOptions = {
+			method: "POST",
+			headers: { "token": context.token },
+			body: JSON.stringify(list)
+		}
+
+		console.log(requestOptions)
+
+		fetch("/api/techniquechain/node/create", requestOptions)
+			.then(response => {
+				if(!response.ok) {
+					console.log("respone not ok")
+				}
+				console.log(response)
+				return response.json()
+			})
+			.catch(() => {
+				console.log("nått gick fel!")
+			})
+	
+
+	}, [])
+	
 	return (
 		<Modal.Body style={{ padding: "0" }}>
 			<title>Teknikkedjor</title>
