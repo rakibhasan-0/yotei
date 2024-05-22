aa5f3688467246382a8c74d32c9d0f9ea937355c
Daniel Hylander <ens21dhr@cs.umu.se>
2024-05-22 16:27:26 +0200

    Refactored out the methods for validating a users permissions from AuthFilter to PermisisonsValidator.

--- backend/gateway/src/main/java/se/umu/cs/pvt/gateway/AuthFilter.java
+++ backend/gateway/src/main/java/se/umu/cs/pvt/gateway/AuthFilter.java
@@ -3,9 +3,7 @@ package se.umu.cs.pvt.gateway;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.deser.DataFormatReaders.Match;
 
-import ch.qos.logback.classic.Level;
 import org.springframework.http.HttpMethod;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -20,19 +18,11 @@ import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author UNKNOWN (Doc: Griffin c20wnn)
- * @author Team Mango (Grupp 4) - 2024-05-20
+ * @author Team Mango (Group 4) - 2024-05-22
  * @since 2024-05-20
  *
  *         AuthFilter.java - Authorization class.
@@ -48,27 +38,7 @@ public class AuthFilter implements GlobalFilter, Ordered {
      */
     private final String secret = "PVT";
 
-    // Enum for all existing permissions
-    // These are listed in permissions.sql and should mirror 
-    // what is present in utils.js
-    private enum permissionList {
-        ADMIN_RIGHTS(1),
-	    SESSION_OWN(2), //Edit your own sessions.
-	    SESSION_ALL(3), //Edit all sessions.
-	    PLAN_OWN(4),
-	    PLAN_ALL(5),
-	    WORKOUT_OWN(6),
-	    WORKOUT_ALL(7),
-	    TEHCNIQUE_EXERCISE_OWN(8),
-	    TEHCNIQUE_EXERCISE_ALL(9),
-	    GRADING_OWN(10),
-	    GRADING_ALL(11);
-
-        private final int value;
-        private permissionList(int value) {
-            this.value = value;
-        }
-    }
+    PermissionValidator permisisonValidater = new PermissionValidator();
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -135,156 +105,29 @@ public class AuthFilter implements GlobalFilter, Ordered {
         //Logger.log(Level.DEBUG, "permissions: " + permissions);
         
         if (role.equals("ADMIN") || 
-            permissions.contains(permissionList.ADMIN_RIGHTS.value) ||
+            permissions.contains(permisisonValidater.getAdminRightsValue()) ||
             request.getMethod().equals(HttpMethod.GET)) {
             return true;
         }
                 
         // Check for each permission-locked api path and determine if user is allowed through
         if (path.startsWith("/api/session") 
-            && !checkSessionPermissions(path, permissions)) return false;
+            && !permisisonValidater.checkSessionPermissions(path, permissions)) return false;
 
         if (path.startsWith("/api/plan")
-            && !checkPlanPermissions(path, permissions)) return false;
+            && !permisisonValidater.checkPlanPermissions(path, permissions)) return false;
 
         if ((path.startsWith("/api/techniques") || path.startsWith("/api/exercises"))
-            && !checkTechniqueExercisePermissions(path, permissions)) return false;
+            && !permisisonValidater.checkTechniqueExercisePermissions(path, permissions)) return false;
 
         if (path.startsWith("/api/examination")
-            && !checkGradingPermissions(path, permissions)) return false;
+            && !permisisonValidater.checkGradingPermissions(path, permissions)) return false;
 
         if (path.startsWith("/api/workouts") 
-        && !checkWorkoutPermissions(path, permissions, request)) return false;
+            && !permisisonValidater.checkWorkoutPermissions(path, permissions)) return false;
 
         return !isAdminLockedEndPoints(path);
 
-    }
-
-    private boolean checkSessionPermissions(String path, List<Integer> permissions) {
-        Pattern[] patterns = {
-            // From SessionController
-            Pattern.compile("^/api/session/add$"),
-            Pattern.compile("^/api/session/addList$"),
-            Pattern.compile("^/api/session/delete$"),
-            Pattern.compile("^/api/session/deleteByPlan$"),
-            Pattern.compile("^/api/session/update$"),
-            // From SessionReviewController
-            Pattern.compile("^/api/session/\\d+/review$"),
-            Pattern.compile("^/api/session/\\d+/review/\\d+/activity$"),
-            Pattern.compile("^/api/session/\\d+/review/\\d+/activity/\\d+$"),
-            Pattern.compile("^/api/session/\\d+/review/\\d+$")
-        };
-    
-        Integer[] permissionsToCheck = {
-            permissionList.SESSION_ALL.value,
-            permissionList.SESSION_OWN.value
-        };
-
-        return hasPermission(path, permissions, Arrays.asList(patterns), Arrays.asList(permissionsToCheck));
-    }
-
-    private boolean checkPlanPermissions(String path, List<Integer> permissions) {
-        Pattern[] patterns = {
-            // From PlanController
-            Pattern.compile("^/api/plan/add$"),
-            Pattern.compile("^/api/plan/remove$"),
-            Pattern.compile("^/api/plan/update$"),
-        };
-    
-        Integer[] permissionsToCheck = {
-            permissionList.PLAN_ALL.value,
-            permissionList.PLAN_OWN.value
-        };
-
-        return hasPermission(path, permissions, Arrays.asList(patterns), Arrays.asList(permissionsToCheck));
-    }
-
-    private boolean checkTechniqueExercisePermissions(String path, List<Integer> permissions) {
-        Pattern[] patterns = {
-            // From TechniqueController
-            // TODO: This should only be valid for the POST and PUT method
-            Pattern.compile("^/api/techniques$"),
-            Pattern.compile("^/api/techniques/\\d+$"),
-            // TODO: Only for the POST, DELETE and PUT methods
-            Pattern.compile("^/api/techniques/reviews$"),
-            // From ExerciseController
-            Pattern.compile("^/api/exercises/add$"),
-            Pattern.compile("^/api/exercises/add/update$"),
-            Pattern.compile("^/api/exercises/add/remove/\\d+$"),
-            Pattern.compile("^/api/exercises/add/image$")
-        };
-    
-        Integer[] permissionsToCheck = {
-            permissionList.TEHCNIQUE_EXERCISE_ALL.value,
-            permissionList.TEHCNIQUE_EXERCISE_OWN.value
-        };
-
-        return hasPermission(path, permissions, Arrays.asList(patterns), Arrays.asList(permissionsToCheck));
-    }
-
-    private boolean checkGradingPermissions(String path, List<Integer> permissions) {
-        Pattern[] patterns = {
-            // From ExaminationController
-            Pattern.compile("^/api/examination/grading$"),
-            Pattern.compile("^/api/examination/grading/\\d+$"),
-            Pattern.compile("^/api/examination/examinee$"),
-            Pattern.compile("^/api/examination/examinee/\\d+$"),
-            Pattern.compile("^/api/examination/pair$"),
-            Pattern.compile("^/api/examination/pair/\\d+$"),
-            Pattern.compile("^/api/examination/comment$"),
-            Pattern.compile("^/api/examination/comment/\\d+$"),
-            Pattern.compile("^/api/examination/examresult$"),
-            Pattern.compile("^/api/examination/examresult/\\d+$")
-        };
-    
-        Integer[] permissionsToCheck = {
-            permissionList.GRADING_ALL.value,
-            permissionList.GRADING_OWN.value
-        };
-
-        return hasPermission(path, permissions, Arrays.asList(patterns), Arrays.asList(permissionsToCheck));
-    }
-
-    private boolean checkWorkoutPermissions(
-        String path, List<Integer> permissions, ServerHttpRequest request) {
-        // Might be a better way but this makes it so that no 
-        // newly created endpoint is permission-locked from the get-go
-        // Any new endpoint that needs to be locked has to be included here
-        Pattern[] patterns = {
-            // From WorkoutController
-            Pattern.compile("^/api/workouts$"),
-            Pattern.compile("^/api/workouts/update_full_workout$"),
-            Pattern.compile("^/api/workouts/reviews$"),
-            Pattern.compile("^/api/workouts/favorites$"),
-            Pattern.compile("^/api/workouts/add_full_workout$"),
-            Pattern.compile("^/api/workouts/delete_full_workout/\\d+$"),
-            Pattern.compile("^/api/workouts/delete/\\d+$"),
-            // From UserWorkoutController
-            Pattern.compile("^/api/workouts/add/workout/\\d+/user/\\d+$"),
-            Pattern.compile("^/api/workouts/remove/workout/\\d+/user/\\d+$"),
-        };
-    
-        Integer[] permissionsToCheck = {
-            permissionList.WORKOUT_ALL.value,
-            permissionList.WORKOUT_OWN.value
-        };
-
-        return hasPermission(
-            path, permissions, Arrays.asList(patterns), Arrays.asList(permissionsToCheck));
-    }
-
-
-    private boolean hasPermission(String path, List<Integer> permissions, List<Pattern> patterns, List<Integer> permissionsToCheck) {
-        for (Pattern pattern : patterns) {
-            Matcher matcher = pattern.matcher(path);
-            if (matcher.matches()) {
-                if (Collections.disjoint(permissions, permissionsToCheck)) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
     }
 
     private boolean isAdminLockedEndPoints(String path) {
---
+++ backend/gateway/src/main/java/se/umu/cs/pvt/gateway/PermissionValidator.java
@@ -0,0 +1,244 @@
+package se.umu.cs.pvt.gateway;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * PermissionValidator class is a helper class for AuthFilter for validation 
+ * of user permissions.
+ * 
+ * When adding a new feature requiring new permissions:
+ * 		- Create new permissions when necessary.
+ *          - These should be added to permissions.sql AND util.js so 
+ *            they all match.
+ * 		- Add the permissions into the "permissionList".
+ * 		- Create a new method for checking the permissions associated with the 
+ *        feature.
+ * 		- Create a list of regex patterns for each endpoint.
+ *
+ * @author Team Mango (Group 4) - 2024-05-22
+ * 
+ */
+public class PermissionValidator {
+    // Enum for all existing permissions
+    // These are listed in permissions.sql and should mirror 
+    // what is present in utils.js
+    private enum permissionList {
+        ADMIN_RIGHTS(1),
+	    SESSION_OWN(2), //Edit your own sessions.
+	    SESSION_ALL(3), //Edit all sessions.
+	    PLAN_OWN(4),
+	    PLAN_ALL(5),
+	    WORKOUT_OWN(6),
+	    WORKOUT_ALL(7),
+	    TECHNIQUE_EXERCISE_OWN(8),
+	    TECHNIQUE_EXERCISE_ALL(9),
+	    GRADING_OWN(10),
+	    GRADING_ALL(11);
+
+        private final int value;
+        private permissionList(int value) {
+            this.value = value;
+        }
+	}
+
+	public int getAdminRightsValue() {return permissionList.ADMIN_RIGHTS.value;}
+
+    /**
+     * Checks if a user has permissions to access API endpoints associated 
+     * with Sessions.
+     * 
+     * Checks when a user tries to access Sessions via POST, PUT, DELETE http methods. 
+     * 
+     * @param path The path for the API call
+     * @param permissions The permissions belonging to the user
+     * 
+     * @return True if the user has the required permissions; else false.
+     */
+	public boolean checkSessionPermissions(String path, List<Integer> permissions) {
+        Pattern[] patterns = {
+            // From SessionController
+            Pattern.compile("^/api/session/add$"),
+            Pattern.compile("^/api/session/addList$"),
+            Pattern.compile("^/api/session/delete$"),
+            Pattern.compile("^/api/session/deleteByPlan$"),
+            Pattern.compile("^/api/session/update$"),
+            // From SessionReviewController
+            Pattern.compile("^/api/session/\\d+/review$"),
+            Pattern.compile("^/api/session/\\d+/review/\\d+/activity$"),
+            Pattern.compile("^/api/session/\\d+/review/\\d+/activity/\\d+$"),
+            Pattern.compile("^/api/session/\\d+/review/\\d+$")
+        };
+    
+        Integer[] permissionsToCheck = {
+            permissionList.SESSION_ALL.value,
+            permissionList.SESSION_OWN.value
+        };
+
+        return hasPermission(path, permissions, Arrays.asList(patterns), Arrays.asList(permissionsToCheck));
+    }
+
+    /**
+     * Checks if a user has permissions to access API endpoints associated 
+     * with Plan/Group.
+     * 
+     * Checks when a user tries to access Plan/Group via POST, PUT, DELETE http methods. 
+     * 
+     * @param path The path for the API call
+     * @param permissions The permissions belonging to the user
+     * 
+     * @return True if the user has the required permissions; else false.
+     */
+    public boolean checkPlanPermissions(String path, List<Integer> permissions) {
+        Pattern[] patterns = {
+            // From PlanController
+            Pattern.compile("^/api/plan/add$"),
+            Pattern.compile("^/api/plan/remove$"),
+            Pattern.compile("^/api/plan/update$"),
+        };
+    
+        Integer[] permissionsToCheck = {
+            permissionList.PLAN_ALL.value,
+            permissionList.PLAN_OWN.value
+        };
+
+        return hasPermission(path, permissions, Arrays.asList(patterns), Arrays.asList(permissionsToCheck));
+    }
+
+    /**
+     * Checks if a user has permissions to access API endpoints associated 
+     * with Technique/Exercise.
+     * 
+     * Checks when a user tries to access Technique/Exercise via 
+     * POST, PUT, DELETE http methods. 
+     * 
+     * @param path The path for the API call
+     * @param permissions The permissions belonging to the user
+     * 
+     * @return True if the user has the required permissions; else false.
+     */
+    public boolean checkTechniqueExercisePermissions(String path, List<Integer> permissions) {
+        Pattern[] patterns = {
+            // From TechniqueController
+            Pattern.compile("^/api/techniques$"),
+            Pattern.compile("^/api/techniques/\\d+$"),
+            Pattern.compile("^/api/techniques/reviews$"),
+            // From ExerciseController
+            Pattern.compile("^/api/exercises/add$"),
+            Pattern.compile("^/api/exercises/add/update$"),
+            Pattern.compile("^/api/exercises/add/remove/\\d+$"),
+            Pattern.compile("^/api/exercises/add/image$")
+        };
+    
+        Integer[] permissionsToCheck = {
+            permissionList.TECHNIQUE_EXERCISE_ALL.value,
+            permissionList.TECHNIQUE_EXERCISE_OWN.value
+        };
+
+        return hasPermission(path, permissions, 
+            Arrays.asList(patterns), Arrays.asList(permissionsToCheck));
+    }
+
+    /**
+     * Checks if a user has permissions to access API endpoints associated 
+     * with Grading.
+     * 
+     * Checks when a user tries to access Grading via POST, PUT, DELETE http methods. 
+     * 
+     * @param path The path for the API call
+     * @param permissions The permissions belonging to the user
+     * 
+     * @return True if the user has the required permissions; else false.
+     */
+    public boolean checkGradingPermissions(String path, List<Integer> permissions) {
+        Pattern[] patterns = {
+            // From ExaminationController
+            Pattern.compile("^/api/examination/grading$"),
+            Pattern.compile("^/api/examination/grading/\\d+$"),
+            Pattern.compile("^/api/examination/examinee$"),
+            Pattern.compile("^/api/examination/examinee/\\d+$"),
+            Pattern.compile("^/api/examination/pair$"),
+            Pattern.compile("^/api/examination/pair/\\d+$"),
+            Pattern.compile("^/api/examination/comment$"),
+            Pattern.compile("^/api/examination/comment/\\d+$"),
+            Pattern.compile("^/api/examination/examresult$"),
+            Pattern.compile("^/api/examination/examresult/\\d+$")
+        };
+    
+        Integer[] permissionsToCheck = {
+            permissionList.GRADING_ALL.value,
+            permissionList.GRADING_OWN.value
+        };
+
+        return hasPermission(path, permissions, 
+            Arrays.asList(patterns), Arrays.asList(permissionsToCheck));
+    }
+
+    /**
+     * Checks if a user has permissions to access API endpoints associated 
+     * with Workout.
+     * 
+     * Checks when a user tries to access Workout via POST, PUT, DELETE http methods. 
+     * 
+     * @param path The path for the API call
+     * @param permissions The permissions belonging to the user
+     * 
+     * @return True if the user has the required permissions; else false.
+     */
+    public boolean checkWorkoutPermissions(String path, List<Integer> permissions) {
+        Pattern[] patterns = {
+            // From WorkoutController
+            Pattern.compile("^/api/workouts$"),
+            Pattern.compile("^/api/workouts/update_full_workout$"),
+            Pattern.compile("^/api/workouts/reviews$"),
+            Pattern.compile("^/api/workouts/favorites$"),
+            Pattern.compile("^/api/workouts/add_full_workout$"),
+            Pattern.compile("^/api/workouts/delete_full_workout/\\d+$"),
+            Pattern.compile("^/api/workouts/delete/\\d+$"),
+            // From UserWorkoutController
+            Pattern.compile("^/api/workouts/add/workout/\\d+/user/\\d+$"),
+            Pattern.compile("^/api/workouts/remove/workout/\\d+/user/\\d+$"),
+        };
+    
+        Integer[] permissionsToCheck = {
+            permissionList.WORKOUT_ALL.value,
+            permissionList.WORKOUT_OWN.value
+        };
+
+        return hasPermission(path, permissions, 
+            Arrays.asList(patterns), Arrays.asList(permissionsToCheck));
+    }
+
+    /**
+     * If the path matches ONE pattern, then it checks the if the list
+     * permissions contains at least one of the required permissions in 
+     * "permissionsToCheck".
+     * 
+     * @param path The path for the API call
+     * @param permissions The permissions belonging to the user
+     * @param patterns The patterns of the API endpoints
+     * @param permissionsToCheck A list of required permissions
+     * 
+     * @return True if the users has the required permissions or if no pattern 
+     * matches; else false
+     */
+    private boolean hasPermission(
+		String path, List<Integer> permissions, List<Pattern> patterns, 
+		List<Integer> permissionsToCheck) {
+
+        for (Pattern pattern : patterns) {
+            Matcher matcher = pattern.matcher(path);
+
+            if (matcher.matches()) {
+                if (Collections.disjoint(permissions, permissionsToCheck)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
