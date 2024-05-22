cf0bf0d019dc7266e5e96ca8cb0ea676f7bdf232
Amelia Kjellin <c21akn@cs.umu.se>
2024-05-22 10:00:47 +0200

    Added new files for description popup

---
+++ frontend/src/components/Common/Popup/PopupMini.jsx
@@ -0,0 +1,109 @@
+import { X } from "react-bootstrap-icons"
+import styles from "./PopupSmall.module.css"
+import React, { useEffect } from "react"
+import Divider from "../Divider/Divider"
+import Button from "../Button/Button"
+
+
+/**
+ * Popup is a component that creates a popup window with a title and a close button.
+ * 
+ * 
+ * Props:
+ *     title     @type {string}   - Sets the title of the popup window. Empty title
+ * 							        omits the line under the title
+ *     id        @type {string}   - Sets the id of the popup window
+ * 	   isOpen    @type {boolean}  - Sets if popup should be visible or not
+ *     setIsOpen @type {useState} - Sets the state of the popup window
+ * 	   style     @type {object}   - Additional styles to add to the popup container.
+ * 	   isNested  @type {boolean}  - disables the tinted background and fills the parent container. 
+ * 								    Should be used in nested popups.
+ *
+ * Example usage:
+ *  
+ *	const [showPopup, setShowPopup] = useState(false)
+*
+*	return (
+*		<div>
+*			<PopupSmall id={"test-popup"} title={"Test"} isOpen={showPopup} setIsOpen={setShowPopup} direction={startRedirection}  >
+<>some children</>
+</PopupSmall>
+*			<RoundButton onClick={() => setShowPopup(true)} />
+*		</div>
+* 	)
+*
+* @author Team Medusa, Team Chimera, Team Pomegranate
+* @version 3.0
+* @since 2024-05-16
+*/
+export default function PopupSmall({ title, id, isOpen, setIsOpen, children, isNested, style, onClose, zIndex, direction }) {
+
+	// Synchronize react state with CSS-styling in browser
+	useEffect(() => {
+		// The body scroll should not be disabled when a nested popup is closed.
+		// That should be done when a non-nested popup is closed.
+		if (isNested) return
+		// If it is nested, these things are already applied.
+		document.body.style.overflowY = isOpen ? "hidden" : "visible"
+		document.body.style.touchAction = isOpen ? "none" : "auto"
+		document.body.style.height = isOpen ? "100vh" : "auto"
+	}, [isNested, isOpen])
+
+	if (!isOpen) return
+
+	return (
+		<>
+			<div className={styles.backdrop} style={{ zIndex: zIndex ? zIndex - 10 : 90 }} onClick={() => {
+				setIsOpen(false)
+				if (onClose) onClose()
+			}}
+			/>
+			<div
+				className={styles.container}
+				id={id}
+				style={style}
+			>
+				<div className={styles.topbar}>
+					{title && <Divider title={title} option="h1_center" />}
+					<button
+						className={styles.closebutton}
+						onClick={() => {
+							setIsOpen(false)
+							if (onClose) onClose()
+
+						}}
+					>
+						<X width={44} height={44} color="black" />
+					</button>
+				</div>
+				<div className={styles.mainText}>
+					{children}
+				</div>
+
+				<div className={styles.buttonContainer}>
+					<Button
+						id="continue-button"
+						outlined={false}
+						width="100%"
+						onClick={direction}
+					>
+						<p>Ja</p>
+					</Button>
+
+					<Button
+						id="continue-button"
+						outlined={true}
+						width="100%"
+						onClick={() => {
+							setIsOpen(false)
+							if (onClose) onClose()
+
+						}}
+					>
+						<p>Nej</p>
+					</Button>
+				</div>
+			</div>
+		</>
+	)
+}
---
+++ frontend/src/pages/Activity/Technique/TechniqueDetail/TechniqueDetailMini.jsx
@@ -0,0 +1,132 @@
+import { useState, useEffect, useContext, useCallback, useParams } from "react"
+
+
+import { AccountContext } from "../../../../context"
+
+import Tag from "../../../../components/Common/Tag/Tag"
+import BeltIcon from "../../../../components/Common/BeltIcon/BeltIcon"
+
+
+
+import styles from "./TechniqueDetail.module.css"
+import ErrorState from "../../../../components/Common/ErrorState/ErrorState"
+import Spinner from "../../../../components/Common/Spinner/Spinner"
+import Gallery from "../../../../components/Gallery/Gallery"
+
+
+
+
+
+/**
+ * The detail page for a technique.
+ * 
+ * Props:
+ * 		id: The id used for testing purposes.
+ * 
+ * Example usage:
+ * 	   <TechniqueDetail id="test-id"/>
+ * 
+ * Version 4.1:
+ * 		Fixed navigation from pages outside the website 
+ * 
+ * @author Team Medusa (Grupp 6) & Cyclops (Group 5) & Tomato (Group 6) & Team Durian (Group 3) (2024-04-23), Team Kiwi (Group 2) (2024-05-03)
+ * @version 4.1
+ * @since 2024-04-25
+ */
+function TechniqueDetail({ id }) {
+
+	const { techniqueId } = useParams()
+	const { token } = useContext(AccountContext)
+	const [technique, setTechnique] = useState()
+	const [error, setError] = useState("")
+	const [loading, setLoading] = useState(true)
+
+
+	// Belt sorting order, Adult easiest -> Adult hardest -> Child easiest -> Child hardest
+	const order = [3, 5, 7, 9, 11, 13, 14, 15, 16, 4, 6, 8, 10, 12]
+	const beltSort = (a, b) => {
+		const indexA = order.indexOf(a.id)
+		const indexB = order.indexOf(b.id)
+
+		if (indexA === -1) return 1
+		if (indexB === -1) return -1
+
+		return indexA - indexB
+	}
+
+	const handleGet = useCallback(() => {
+		setLoading(true)
+		setError("")
+		fetch(`/api/techniques/${techniqueId}`, { headers: { token } })
+			.then(async res => {
+				if (!res.ok) {
+					// Quick fix with + " " because old API returns empty body on 404.
+					setError(await res.text() + " ")
+					setLoading(false)
+					return
+				}
+				const technique = await res.json()
+				setTechnique(technique)
+				setLoading(false)
+			})
+			.catch(() => {
+				setError("Ett nätverksfel inträffade. Kontrollera din internetuppkoppling.")
+				setLoading(false)
+			})
+	}, [techniqueId, token])
+
+	useEffect(() => handleGet(), [handleGet, techniqueId, token])
+
+	
+
+	if (error != "") return <ErrorState
+		message={error}
+		onRecover={handleGet}
+	/>
+
+	if (loading) return <div className={styles["technique-detail-center-spinner"]}><Spinner /></div>
+
+	return (
+		<>
+			<div className={styles["technique-detail-container"]} id={id == undefined ? "technique" : id}>
+				<title>Tekniker</title>
+				<h1>{technique.name}</h1>
+
+				<div className={styles["technique-detail-belts-container"]}>
+					{technique.belts ? ( 
+						technique.belts.filter(belt => !belt.child).sort(beltSort).map(belt => <BeltIcon key={belt.name} belt={belt} />)
+					) : (
+						<p>Inga bälten kunde hittas för denna teknik.</p>
+					)}
+				</div>
+				<div className={styles["technique-detail-belts-container"]}>
+					{(
+						technique.belts.filter(belt => belt.child).sort(beltSort).map(belt => <BeltIcon key={belt.name} belt={belt} />)
+					)}
+				</div> 
+				
+				<h2>Beskrivning</h2>
+				{technique.description ? (
+					<p style={{whiteSpace: "pre-wrap", wordBreak: "break-word"}}>{technique.description}</p>
+				) : (
+					<p style={{ fontStyle: "italic", color: "var(--gray)" }}>Beskrivning saknas.</p>
+				)}
+				<h2>Taggar</h2>
+				<div className={styles["technique-detail-tag-container"]}>
+					{technique.tags ? (
+						technique.tags.map(tag => <Tag key={tag.id} text={tag.name} tagType="default" />)
+					) : (
+						<p>Denna teknik saknar taggar.</p>
+					)
+					}
+				</div>
+		
+				<Gallery id={techniqueId} />
+				
+			</div>
+		</>
+	)
+}
+
+
+export default TechniqueDetail
