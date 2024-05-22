935e00bc50f8d83957858b8ccc27a86e7fc78477
Gabriel Morberg <c21gmg@cs.umu.se>
2024-05-22 13:34:19 +0200

    Clicking on a technique card in grading protocol doesnt lead to error now

--- frontend/src/components/Common/Technique/TechniqueCard/TechniqueCard.jsx
+++ frontend/src/components/Common/Technique/TechniqueCard/TechniqueCard.jsx
@@ -30,6 +30,8 @@ function TechniqueCard({ technique, checkBox, id}) {
 
 		if (technique.activity_id && technique.type === "technique") {
 			navigate("/technique/" + technique.activity_id)
+		} else if (technique.id && technique.type === "technique") {
+			navigate("/technique/" + technique.id) 
 		} else if (technique.type === "exercise" && technique.activity_id) {
 			navigate("/exercise/exercise_page/" + technique.activity_id)
 		} else {
