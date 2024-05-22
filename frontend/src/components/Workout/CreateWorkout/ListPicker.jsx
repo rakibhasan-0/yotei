4e4744c6096651c683981ef2ece5005fde70d6e6
Anton Bergmark <dv18abk@cs.umu.se>
2024-05-22 09:18:32 +0200

    Fixed more linting problems

--- frontend/src/components/Workout/CreateWorkout/ListPicker.jsx
+++ frontend/src/components/Workout/CreateWorkout/ListPicker.jsx
@@ -15,49 +15,49 @@ import CheckBox from "../../Common/CheckBox/CheckBox"
 
 
 export default function ListPicker({ onFilterChange }) {
-    const filterOptions = [
-        { label: "Mina listor" },
-        { label: "Delade med mig" },
-        { label: "Publika listor" }
-    ]
-    const [filter, setFilter] = useState([])
+	const filterOptions = [
+		{ label: "Mina listor" },
+		{ label: "Delade med mig" },
+		{ label: "Publika listor" }
+	]
+	const [filter, setFilter] = useState([])
 
 
 
 	useEffect(() => {
-		onFilterChange(filter);
-	}, [filter, onFilterChange]);
+		onFilterChange(filter)
+	}, [filter, onFilterChange])
 
 
 
-    const onSelect = (option) => {
+	const onSelect = (option) => {
 		setFilter(prevFilter => {
 			if (prevFilter.some(opt => opt.label === option.label)) {
 				// If the option is already included, remove it
-				return prevFilter.filter(opt => opt.label !== option.label);
+				return prevFilter.filter(opt => opt.label !== option.label)
 			} else {
 				// If the option is not included, add it
-				return [...prevFilter, option];
+				return [...prevFilter, option]
 			}
 		})
 	}
 
-    return (
-        <div className={styles.listPicker}>
-            <DropDown
-                text={"Listor"}
-                className={styles.filterText}
-                id="list-filter-workout" centered={true}
-                autoClose={false}
-            >
-                <div className={styles.filterBorder}>
+	return (
+		<div className={styles.listPicker}>
+			<DropDown
+				text={"Listor"}
+				className={styles.filterText}
+				id="list-filter-workout" centered={true}
+				autoClose={false}
+			>
+				<div className={styles.filterBorder}>
 					{filterOptions.map((option, index) => (
 						<label className={`${styles.filterItems} filterItems`} key={index}>
 							<CheckBox label={option.label} checked={filter.some(opt => opt.label === option.label)} onClick={() => onSelect(option)} />
 						</label>
 					))}
 				</div>
-            </DropDown>
-        </div>
-    )
+			</DropDown>
+		</div>
+	)
 }
