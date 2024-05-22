9f10bbaace5cdaad39058459190d020803140493
Joakim Gladh <c21jgh@cs.umu.se>
2024-05-22 10:18:20 +0200

    fake commit to retry pipeline after systest removal

--- backend/api/src/main/java/se/umu/cs/pvt/search/builders/SearchActivityListDBBuilder.java
+++ backend/api/src/main/java/se/umu/cs/pvt/search/builders/SearchActivityListDBBuilder.java
@@ -10,7 +10,7 @@ import se.umu.cs.pvt.search.params.SearchActivityListParams;
  * This class builds a {@link DatabaseQuery DatabaseQuery} based on
  * the filtering methods used.
  *
- * @author Team Tomato, updated 2024-05-22
+ * @author Team Tomato, updated 2024-05-22 
  * @date 2024-05-20
  */
 public class SearchActivityListDBBuilder {
