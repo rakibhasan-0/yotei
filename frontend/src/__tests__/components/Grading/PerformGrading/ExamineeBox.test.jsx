0a8b6bc248a0a7bb004eb1568f0adb8c0963534c
Noa <oi22nlg@cs.umu.se>
2024-05-23 11:31:28 +0200

    added three more tests for the examinee box component

--- frontend/src/__tests__/components/Grading/PerformGrading/ExamineeBox.test.jsx
+++ frontend/src/__tests__/components/Grading/PerformGrading/ExamineeBox.test.jsx
@@ -1,4 +1,4 @@
-import {configure, screen, render, fireEvent} from "@testing-library/react"
+import {configure, screen, render, fireEvent, waitFor} from "@testing-library/react"
 import ExamineeBox from "../../../../components/Grading/PerformGrading/ExamineeBox.jsx"
 import "@testing-library/jest-dom"
 import React from "react"
@@ -16,8 +16,36 @@ describe("ExamineeBox name testing", () => {
 describe("ExamineeBox onClick testing", () => {
     test("ExamineeBox: function should run once on click", () => {
         const handleClick = jest.fn()
-        render(<ExamineeBox id={"ExamineeBox"} onClick={handleClick}/>)
-        fireEvent.click(screen.getByTestId("ExamineeBox"))
+        render(<ExamineeBox id={"ExamineeBox"} examineeName={"testname"} onClick={handleClick}/>)
+        fireEvent.click(screen.getByTestId("ExamineeName"))
         expect(handleClick).toHaveBeenCalledTimes(1)
+    })
+
+    test("ExamineeBox: intial color should be white", () => {
+        render(<ExamineeBox id="ExamineeBox" examineeName="testname"/>)
+        const examineeBox = screen.getByTestId("ExamineeBox");
+        expect(examineeBox).toHaveStyle({ backgroundColor: 'white' })
+    })
+
+    test("ExamineeBox: clicked should swap colors to lightgreen", () => {
+        const handleClick = jest.fn()
+        render(<ExamineeBox id="ExamineeBox" examineeName="testname" onClick={handleClick} />)
+        const examineeBox = screen.getByTestId("ExamineeBox");
+        fireEvent.click(screen.getByTestId("ExamineeName"))
+        expect(examineeBox).toHaveStyle({ backgroundColor: 'lightgreen' })
+    })
+
+    test("ExamineeBox: clicked twice should swap colors to lightcoral", async () => {
+        const handleClick = jest.fn()
+        render(<ExamineeBox id="ExamineeBox" examineeName="testname" onClick={handleClick} />)
+        const examineeBox = screen.getByTestId("ExamineeBox");
+        fireEvent.click(screen.getByTestId("ExamineeName"))
+
+        await waitFor(() => {
+            expect(examineeBox).toHaveStyle({ backgroundColor: 'lightgreen' });
+        });
+
+        fireEvent.click(screen.getByTestId("ExamineeName"))
+        expect(examineeBox).toHaveStyle({ backgroundColor: 'lightcoral' })
     })
 })
