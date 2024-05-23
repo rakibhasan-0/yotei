181970f3c1521ecd4b79a6ec5e2770af24343305
Hugo Andersson <c21han@cs.umu.se>
2024-05-23 09:54:12 +0200

    Merge errors, starting to fix it

--- frontend/src/components/Activity/AddToListPopupContent.jsx
+++ frontend/src/components/Activity/AddToListPopupContent.jsx
@@ -10,11 +10,6 @@ import useMap from "../../hooks/useMap"
 import { setError, setSuccess } from "../../utils"
 import { Link } from "react-router-dom"
 
-/**
- * @author Team Tomato
- * @since 2024-05-09, updated 2024-05-23
- * @version 1.0
- * */
 export const AddToListPopupContent = ({ techExerID, setShowMorePopup }) => {
 	const [lists, setLists] = useState([])
 	const [searchListText, setSearchListText] = useState("")
@@ -29,14 +24,17 @@ export const AddToListPopupContent = ({ techExerID, setShowMorePopup }) => {
 	 * @returns {void}
 	 */
 	const handleCheck = (id) => {
-		setSelectedLists((prevIds) => {
+		setSelectedLists(prevIds => {
 			if (prevIds.includes(id)) {
-				return prevIds.filter((itemId) => itemId !== id)
+				return prevIds.filter(itemId => itemId !== id)
 			} else {
 				return [...prevIds, id]
 			}
 		})
 	}
+ 
+
+
 
 	/**
 	 * Fetches lists when the component is mounted or when the
@@ -47,35 +45,40 @@ export const AddToListPopupContent = ({ techExerID, setShowMorePopup }) => {
 		fetchingList()
 	}, [searchListText, lists])
 
+
+
 	/**
 	 * Fetches the lists from the backend, either from cache or by a new API-call.
 	 */
 	function fetchingList() {
+
 		const args = {
 			text: searchListText,
-			isAuthor: isAdminUser(context) ? false : true,
+			isAuthor: true,
 			isShared: false,
-			hidden: false,
+			hidden: false
 		}
 
 		getLists(args, token, map, mapActions, (result) => {
 			if (result.error) return
 
 			// Extract the 'id' and 'name' fields from each item in the result used in displaying the list.
-			const listsToAdd = result.results.map((item) => ({
+			const listsToAdd = result.results.map(item => ({
+
 				id: item.id,
 				name: item.name,
 				author: {
 					userId: item.author.userId,
-					username: item.author.username,
+					username: item.author.username
 				},
 				hidden: item.hidden,
-				date: item.date,
+				date: item.date
 			}))
 
 			setLists(listsToAdd)
 		})
 	}
+
 
 	/**
 	 * POST request to save the activity to the selected lists.
@@ -84,19 +87,19 @@ export const AddToListPopupContent = ({ techExerID, setShowMorePopup }) => {
 		const jsonContent = {
 			listId: selectedLists[0],
 			exerciseId: techExerID.exerciseId,
-			techniqueId: techExerID.techniqueId,
+			techniqueId: techExerID.techniqueId
 		}
-
+	
 		const response = await fetch("/api/activitylistentry/multiAdd", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
-				ids: selectedLists.join(","),
-				token: token,
+				"ids": selectedLists.join(","),
+				"token": token
 			},
-			body: JSON.stringify(jsonContent),
+			body: JSON.stringify(jsonContent)
 		})
-
+		
 		if (!response.ok) {
 			setError("Fel vid sparning av aktivitet till listor")
 		} else {
@@ -104,6 +107,9 @@ export const AddToListPopupContent = ({ techExerID, setShowMorePopup }) => {
 			setShowMorePopup(false)
 		}
 	}
+
+
+
 
 	return (
 		<div className={styles["container"]}>
@@ -114,7 +120,7 @@ export const AddToListPopupContent = ({ techExerID, setShowMorePopup }) => {
 					</Button>
 				</Link>
 			</div>
-			<SearchBar
+			<SearchBar 
 				id="lists-search-bar"
 				placeholder="Sök efter listor"
 				text={searchListText}
