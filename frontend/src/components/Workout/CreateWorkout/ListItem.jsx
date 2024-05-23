ccbaf28f95dc2342d0755b5da84389509bbcc9e9
Melker Henriksson <id20mhn@cs.umu.se>
2024-05-23 11:27:32 +0200

    partial work

--- frontend/src/components/Workout/CreateWorkout/ListItem.jsx
+++ frontend/src/components/Workout/CreateWorkout/ListItem.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom"
 import style from "./ListItem.module.css"
+
 /**
  * This component is used to display the content of a list that is 
  * inside of a drop down menu.
@@ -7,30 +8,38 @@ import style from "./ListItem.module.css"
  * @param {JSX} checkBox 	If a checkbox is wanted, send it as a prop.
  * @param {String} id 		The id of the activity that is in the list.
  * @param {integer} index	The index of the item in the list.
+ * @param {boolean} popUp	Determines whether the list item should redirect or open a popup on click
  *
  * @author	Team Tomato
  * @since	2024-05-20
  */
 
-export default function ListItem({ item, checkBox, id, index}) {
+export default function ListItem({ item, checkBox, id, index, popUp}) {
 	const navigate = useNavigate()
+	const [isOpen, setIsOpen] = useState(false)
 
 	// Determine background color based on index
 	const bgColor = index % 2 === 0 ? "#F8EBEC" : "#FFFFFF"
 
 	// Determines what happens on clicking on the name of different activities in a list.
 	const handleOnClick = () => {
-		if(item.type === "technique") {
-			navigate("/technique/"+ item.path)
-		}
-		if(item.type === "exercise") {
-			navigate("/exercise/exercise_page/" + item.path)
+		if(!popUp){
+			if(item.type === "technique") {
+				navigate("/technique/"+ item.path)
+			}
+			if(item.type === "exercise") {
+				navigate("/exercise/exercise_page/" + item.path)
+			}
+		}	else{
+			setIsOpen(true)
+
 		}
 	}
 
 	
 	return (
 		<div style={{ backgroundColor: bgColor }}>
+			<PopupMini></PopupMini>
 			
 			{item.type === "technique" ? (
 				
