f829cb556c2a2db9dd30d42d37a66524d485fdfc
Anton Björnehall <c21abl@cs.umu.se>
2024-05-27 11:27:44 +0200

    Fixed space to tabs in setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,14 +2,14 @@
 const { createProxyMiddleware } = require("http-proxy-middleware")
 
 module.exports = function(app) {
-		app.use(
-			["/api","/user"],
-			createProxyMiddleware({
-				//Changes the proxy host to target IMP dev server
-				target: "localhost:80",
-				changeOrigin: true,
-			})
-		)
+	app.use(
+		["/api","/user"],
+		createProxyMiddleware({
+			//Changes the proxy host to target IMP dev server
+			target: "localhost:80",
+			changeOrigin: true,
+		})
+	)
 }
 
 
