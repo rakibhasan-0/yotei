fb063e286e02ee404a03da916d739bb2dbf09011
c19ork <c19ork@cs.umu.se>
2024-05-28 10:04:08 +0200

    Fixes DB backup and restore scripts

--- infra/database/backup_db.sh
+++ infra/database/backup_db.sh
@@ -3,12 +3,12 @@
 HOST=localhost
 USERNAME=psql
 DATABASE=yotei
-OUTFILE="$1"                    # Output file = the script argument
+OUTFILE="$1"                    # Output file = the script argument (will save in container)
 
 SCRIPT_PATH=$(dirname "$0")         # Find path of this script
 OLD_PGPASS=$PGPASS                  # Save old path
 export PGPASSFILE=$SCRIPT_PATH/pgpass.conf # Remember to edit this!
 
-docker exec -t html-psql-1 pg_dump -Fc -a -h "$HOST" -U "$USERNAME" "$DATABASE" -f "$OUTFILE"
+docker exec -t yotei-psql-1 pg_dump -Fc -h "$HOST" -U "$USERNAME" "$DATABASE" -f "$OUTFILE"
 
 PGPASSFILE=$OLD_PGPASS          # Restore old path
--- infra/database/restore_db.sh
+++ infra/database/restore_db.sh
@@ -3,20 +3,20 @@
 HOST=localhost
 USERNAME=psql
 DATABASE=yotei
-INFILE="$1"                     # input file = the script argument
+INFILE="$1"                     # Input file = the script argument (will save in container)
 
 SCRIPT_PATH=$(dirname "$0")         # Find path of this script
 OLD_PGPASS=$PGPASSFILE              # Save old path
 export PGPASSFILE=$SCRIPT_PATH/pgpass.conf # Remember to edit this file!
 
-docker exec -t html-psql-1 \
+docker exec -t yotei-psql-1 \
 pg_restore \
-   --clean \
    -d "$DATABASE" \
-   --verbose \
-   --format=c \
    -h "$HOST" \
    -U "$USERNAME" \
+   --clean \
+   --verbose \
+   --format=c \
    "$INFILE"
 
 PGPASSFILE=$OLD_PGPASS          # Restore old path
