fd2289805762a5f048420fe9584b2304b76082bd
id20ehm <id20ehm@cs.umu.se>
2024-05-28 09:58:02 +0200

    add comments

--- frontend/SystemTests/PageObjectModels/groupsPage.ts
+++ frontend/SystemTests/PageObjectModels/groupsPage.ts
@@ -25,7 +25,7 @@ export class GroupsPage {
   }
 
   /**
-   * Create a new group based on the passed properties. 
+   * Create a new group conditionally based on the passed properties.
    * @param group properties (possibly name, start date, end date, days and time) related to the group to be created. 
    */
   async createGroup(group: Group) {
@@ -37,6 +37,7 @@ export class GroupsPage {
     group.startDate && await this.page.locator('#start-date-picker').fill(group.startDate)
     group.endDate && await this.page.locator('#end-date-picker').fill(group.endDate)
 
+    // Fill in all passed days and there time slots. 
     if(group.days) {
       for(const day of group.days) {
         await this.page.locator(`#${day.name}CheckBox-checkbox`).check()
@@ -54,7 +55,6 @@ export class GroupsPage {
   async deleteGroup(name: string) {
     const groupContainer = await this.page.getByText(`${name}`, { exact: true }).locator('..')
     const html = await groupContainer.innerHTML()
-    console.log(html)
     await groupContainer.locator('#edit-group-button').click()
     await this.page.locator('#trashcan_button').click()
     await this.page.getByRole('button', { name: 'Ta bort grupp' }).click()
