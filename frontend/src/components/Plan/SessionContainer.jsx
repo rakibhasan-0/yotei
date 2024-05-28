70dd09f5dd7efdaeda7b8794f04aff89374f6ff0
Victor Østergaard Månsson <c17vsn@cs.umu.se>
2024-05-28 14:07:46 +0200

    Fixed linting

--- frontend/src/components/Plan/SessionContainer.jsx
+++ frontend/src/components/Plan/SessionContainer.jsx
@@ -162,27 +162,19 @@ function SessionContainer ({ id, workout, session, plan}) {
 										<BeltBox id ={styles["sc23-session-container-beltbox"]} style={{borderTopLeftRadius:"4px"}}/>
 								}
 								{
-									checkPlan() && checkColor() ?<ChevronDown id={styles["sc23-dropdown"]}
-										style={{ color: "black", zIndex: 1 }}
+									checkPlan() && checkColor() ?<ChevronDown id={styles["sc23-dropdown"]} style={{ color: "black", zIndex: 1 }}
 										className={[
 											styles["sc23-session-container-chevron-rotation-animation"],
 											styles["sc23-session-container-header-overlap"],
 											toggled ? styles["sc23-chevron-rotate"] : ""
-										].join(" ")}
-										size={20}
-									/>
+										].join(" ")} size={20}/>
 										:
-										<ChevronDown
-											id={styles["sc23-dropdown"]}
-											style={{ zIndex: 1 }}
+										<ChevronDown id={styles["sc23-dropdown"]} style={{ zIndex: 1 }}
 											className={[
 												styles["sc23-session-container-chevron-rotation-animation"],
 												styles["sc23-session-container-header-overlap"],
 												toggled ? styles["sc23-chevron-rotate"] : ""
-											].join(" ")}
-											size={20}
-										/>
-								}
+											].join(" ")}size={20}/>}
 							</div>
 							<div id = {`${id}-content`} className={styles["sc23-session-container-content"]}>
 								
