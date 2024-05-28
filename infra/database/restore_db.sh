6197205bb3f9fef8e50e375591fe6cefbaced798
c19ork <c19ork@cs.umu.se>
2024-05-28 16:53:40 +0200

    De-verbosification and re-verbosification with -v

--- infra/database/restore_db.sh
+++ infra/database/restore_db.sh
@@ -3,22 +3,26 @@
 INFILE=$1
 USELOCALFILE=false
 
-while getopts ':hl:' opt; do
+while getopts ':hvl:' opt; do
     case "$opt" in
 	l) # Local copy filename
 	    USELOCALFILE=true
 	    INFILE=${OPTARG}
 	    ;;
-	h)
+	v) # Verbose
+	    VERBOSE="--verbose "
+	    ;;
+	h) # Help
 	    echo "Backup restore script for Yotei database. Restores from a backup file generated"
 	    echo "by backup_db.sh (by default, the file is assumed to be in the container)."
 	    echo "Can also use a locally stored file."
 	    echo "Note that the yotei-psql-1 container must be running for script to work."
 	    echo
-	    echo "Usage: restore_db.sh [-l] <filename>"
+	    echo "Usage: restore_db.sh [-v] [-l] <filename>"
 	    echo
 	    echo "Options:"
 	    echo "-h, --help    display help message (you are reading it right now!)"
+	    echo "-v            verbose, print all outputs of postgres commands"
 	    echo "-l            use locally stored file. Warning! This will copy it to"
 	    echo "              the root directory in the container, overwriting it."
 	    exit
@@ -53,14 +57,19 @@ else
 fi
 
 
-docker exec -t yotei-psql-1 \
-pg_restore \
-   -d "$DATABASE" \
-   -h "$HOST" \
-   -U "$USERNAME" \
-   --clean \
-   --verbose \
-   --format=c \
-   "$INFILE"
+
+if 
+    (docker exec -t yotei-psql-1 \
+	    pg_restore \
+	    -d "$DATABASE" \
+	    -h "$HOST" \
+	    -U "$USERNAME" \
+	    $VERBOSE\
+	    --clean \
+	    --format=c \
+	    "$INFILE")
+then
+    echo "Successfully restored from backup!"
+fi
 
 PGPASSFILE=$OLD_PGPASS          # Restore old path
