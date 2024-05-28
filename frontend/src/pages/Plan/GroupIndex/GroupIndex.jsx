70b29b6ca7694716a6c362c2d2ab12ad98c6c0ca
id20ehm <id20ehm@cs.umu.se>
2024-05-28 09:14:16 +0200

    change to doublequote

--- frontend/src/pages/Plan/GroupIndex/GroupIndex.jsx
+++ frontend/src/pages/Plan/GroupIndex/GroupIndex.jsx
@@ -81,7 +81,7 @@ export default function GroupIndex() {
 								<div style = {{marginLeft: "5px", display: "flex"}}> { (isAdminUser(context) || canEditGroups(context, group.userId)) && (
 									<>
 										<Link to={`/plan/edit/${group.id}`}>
-											<Pencil id={'edit-group-button'} size={24} color="var(--red-primary)"/>
+											<Pencil id={"edit-group-button"} size={24} color="var(--red-primary)"/>
 										</Link>
 										<div style={{ width: "20px" }}/>
 										<Link to={`./statistics/${group.id}`}>
