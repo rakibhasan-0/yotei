f593c533fa96910c167db40111bcd3cb83d5c032
Tor Larsson <c21tln@cs.umu.se>
2024-05-28 13:44:53 +0200

    Updated documentation of FilterPlan.

--- frontend/src/pages/Plan/FilterPlan.jsx
+++ frontend/src/pages/Plan/FilterPlan.jsx
@@ -27,7 +27,7 @@ import CheckBox from "../../components/Common/CheckBox/CheckBox"
 * Updates: 2024-05-10: Added a checkbox (with a feature toggle, since it does not work currently) for filtering by only my groups or all groups.
 * 		   2024-05-17: Fixed the filtering and refactored code slightly.
 *		   2024-05-22: Added a numFilters counter to the FilterContainer.	 
-*		   2024-05-28: Improved documentation.
+*		   2024-05-28: Improved documentation slightly.
 */
 export default function FilterPlan({ id, chosenGroups, setChosenGroups, dates, onDatesChange, callbackFunctionCheckbox, onlyMyGroups, toggleOnlyMyGroups, numFilters}) {
 	
@@ -35,9 +35,9 @@ export default function FilterPlan({ id, chosenGroups, setChosenGroups, dates, o
 	const onToggle = (checked, chosenGroup) => setChosenGroups(prev => {
 		if(prev) {
 			if(!checked) {
-				//When you uncheck the checkbox (for e.g. a brown belt), you should remove the relevant belts from the filtered list.
-				return prev.filter(g => g !== chosenGroup)
-				//(When all checkboxes are unchecked an empty array is left. This is a special case in PlanIndex.)
+				//When you uncheck the checkbox for a group, that group should be removed from the array of selected groups.
+				return prev.filter(g => g !== chosenGroup) //This is the same as a .delete()-function for an array.
+				//(When all checkboxes are unchecked an empty array is left. This is considered a special case in PlanIndex.)
 				//The "chosenGroups", which is really "selectedPlans" in PlanIndex is just an array of ids of the currently selected groups.
 			}
 			//If the checkbox is checked you should add the chosen group. (e.g. all groups with yellow belts.)
