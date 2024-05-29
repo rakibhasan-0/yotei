74461f7f290003a3784a926471cf622e91f2265e
Nils Nihlen <ens22nnn@cs.umu.se>
2024-05-29 10:22:42 +0200

    Updated testdata to match new database addition

--- frontend/SystemTests/TestFiles/workout.spec.ts
+++ frontend/SystemTests/TestFiles/workout.spec.ts
@@ -68,35 +68,35 @@ test.describe("Workout", () => {
 		// Generates a random string of five characters
 		const name = Math.random().toString(36).slice(2, 7) // Will generate a random string of 5 characters
 
+		const workout = {
+			description: "description description description", 
+			name: name, 
+			techniques: [
+				{ name: "Tsuri ashi" },
+				{ name: "Ayumi ashi" },
+				{ name: "Taisabaki kort" },
+			],
+			exercises: [
+				{ name: "Armhävningar" },
+				{ name: "Armhävningar med bred handposition" },
+				{ name: "Armhävningar med handklapp" },
+			],
+		}
+
 		// Creates a new workout with a randomized name, a sample description,
 		// and adds 6 different activities, 3 exercises and 3 techniques.
-		await workoutPage.createWorkout(
-			{
-				description: "description description description", 
-				name: name, 
-				techniques: [
-					{ name: "Shotei uchi, jodan, rak stöt med främre och bakre handen" },
-					{ name: "Shotei uchi, chudan, rak stöt med främre och bakre handen" },
-					{ name: "Gedan geri, rak spark med främre och bakre benet" },
-				],
-				exercises: [
-					{ name: "Armhävningar" },
-					{ name: "Armhävningar med bred handposition" },
-					{ name: "Armhävningar med handklapp" },
-				],
-			}
-		)
+		await workoutPage.createWorkout(workout)
 
 		// Asserts the existence of the confirmation message and that the workout was created correctly.
 		await expect(page.getByRole("alert")).toContainText("Träningspasset skapades!")
 		await expect(page.locator("#root")).toContainText("description description description")
-		await expect(page.locator("#WorkoutActivityList-1")).toContainText("Shotei uchi, jodan, rak stöt med främre och bakre handen")
-		await expect(page.locator("#WorkoutActivityList-1")).toContainText("Shotei uchi, chudan, rak stöt med främre och bakre handen")
-		await expect(page.locator("#WorkoutActivityList-1")).toContainText("Gedan geri, rak spark med främre och bakre benet")
-		await expect(page.locator("#WorkoutActivityList-1")).toContainText("Armhävningar")
-		await expect(page.locator("#WorkoutActivityList-1")).toContainText("Armhävningar med bred handposition")
-		await expect(page.locator("#WorkoutActivityList-1")).toContainText("Armhävningar med handklapp")
-            
+		await expect(page.locator("#WorkoutActivityList-1")).toContainText(`${workout.techniques[0]}`)
+		await expect(page.locator("#WorkoutActivityList-1")).toContainText(`${workout.techniques[1]}`)
+		await expect(page.locator("#WorkoutActivityList-1")).toContainText(`${workout.techniques[2]}`)
+		await expect(page.locator("#WorkoutActivityList-1")).toContainText(`${workout.exercises[0]}`)
+		await expect(page.locator("#WorkoutActivityList-1")).toContainText(`${workout.exercises[1]}`)
+		await expect(page.locator("#WorkoutActivityList-1")).toContainText(`${workout.exercises[2]}`)
+        
 		await page.waitForSelector("h1")
 
 		// Edits the page
