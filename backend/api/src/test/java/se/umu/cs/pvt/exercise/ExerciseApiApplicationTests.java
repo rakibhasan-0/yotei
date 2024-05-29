6f6a09f6d3d3ae784664df05a6a9ff91cc86a1ec
Tor Larsson <c21tln@cs.umu.se>
2024-05-29 18:53:53 +0200

    Removed unused imports.

--- backend/api/src/test/java/se/umu/cs/pvt/exercise/ExerciseApiApplicationTests.java
+++ backend/api/src/test/java/se/umu/cs/pvt/exercise/ExerciseApiApplicationTests.java
@@ -7,11 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +18,6 @@ import se.umu.cs.pvt.workout.ActivityRepository;
 import se.umu.cs.pvt.workout.WorkoutRepository;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
