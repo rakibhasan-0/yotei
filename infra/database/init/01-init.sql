44d354592eb2929b148a9de2028ae04b592a5796
Isac Norlander <c21inr@cs.umu.se>
2024-05-29 17:24:52 +0200

    added data to sql init so that ther exists a techniquechain and weave at start

--- infra/database/init/01-init.sql
+++ infra/database/init/01-init.sql
@@ -780,7 +780,7 @@ ALTER TABLE in_chain OWNER TO psql;
 \ir defaults/activitylists.sql
 \ir defaults/examination_protocols.sql
 \ir defaults/grading_protocols.sql
---\ir defaults/techniqueChain.sql
+\ir defaults/techniqueChain.sql
 -- Triggers for user
 --
 CREATE OR REPLACE FUNCTION remove_user_references() RETURNS TRIGGER AS $$ 
