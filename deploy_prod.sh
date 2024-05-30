71f5ec7401f091f0938304490f30507185d6ef1d
Emil Engberg <c21eeg@cs.umu.se>
2024-05-30 09:14:27 +0200

    Added missing detach

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 TAG=$1 docker compose -f docker-compose.yml -f docker-compose-domain-release.yml down -v
-TAG=$1 docker compose -f docker-compose.yml -f docker-compose-domain-release.yml up --build
+TAG=$1 docker compose -f docker-compose.yml -f docker-compose-domain-release.yml up --build -d
 TAG=$1 docker compose -f docker-compose.yml -f docker-compose-domain-release.yml push --ignore-push-failures
