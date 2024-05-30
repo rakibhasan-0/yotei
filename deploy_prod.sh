2064d083bb871bd09cb6985df75ee7545d61de0a
Emil Engberg <c21eeg@cs.umu.se>
2024-05-30 14:03:09 +0200

    Fixed deploy script

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -3,5 +3,5 @@
 #TAG=$1 docker compose -f docker-compose.yml -f docker-compose-domain-release.yml down
 #docker volume ls | grep "pgdata" | rev | cut -d' ' -f1 | rev | xargs docker volume rm &> /dev/null
 #docker volume ls | grep "media" | rev | cut -d' ' -f1 | rev | xargs docker volume rm &> /dev/null
-#TAG=$1 docker compose -f docker-compose.yml -f docker-compose-domain-release.yml up --build -d
+TAG=$1 docker compose -f docker-compose.yml -f docker-compose-domain-release.yml build
 TAG=$1 docker compose -f docker-compose.yml -f docker-compose-domain-release.yml push --ignore-push-failures
